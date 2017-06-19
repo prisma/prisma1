0097b0805887591ca73fc839b73b53b54b9cf6ec
Christian Joudrey <cmallette@gmail.com>
2017-06-19 18:02:19 -0400

    Move TestAuthServer to helpers/

--- tests/helpers/test_auth_server.ts
+++ tests/helpers/test_auth_server.ts
@@ -1,5 +1,5 @@
-import {AuthServer} from '../types'
-import {testToken} from '../../tests/mock_data/mockData'
+import {AuthServer} from '../../src/types'
+import {testToken} from '../fixtures/mock_data'
 
 export default class TestAuthServer implements AuthServer {
 
