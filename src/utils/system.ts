4cef0a59409ad814d7ab6fed4b75f14331c38bf4
timsuchanek <tim.suchanek@gmail.com>
2017-09-11 20:02:23 +0200

    cleanup

--- src/utils/system.ts
+++
@@ -1,8 +0,0 @@
-export function exitWithPrintingNewLine(code?: number) {
-  process.stdout.write('\n')
-  process.exit(code)
-}
-
-export function sleep(milliSeconds: number): Promise<void> {
-  return new Promise<void>(resolve => setTimeout(resolve, milliSeconds))
-}
--- src/utils/validation.ts
+++
@@ -1,3 +0,0 @@
-export function isValidProjectName(projectName: string): boolean {
-  return /^[A-Z][a-zA-Z0-9]*$/.test(projectName)
-}
