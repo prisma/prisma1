928897d674a492617acf4e15121177e21ae1d00d
timsuchanek <tim.suchanek@gmail.com>
2017-10-17 15:34:07 +0200

    Adjust default package.json. Closes #814

--- cli/packages/graphcool-cli-core/src/examples.ts
+++ cli/packages/graphcool-cli-core/src/examples.ts
@@ -5,14 +5,7 @@ import { ProjectDefinition } from 'graphcool-cli-engine'
 export const defaultPjson = {
   "name": "",
   "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC"
+  "description": "My Graphcool Service"
 }
 
 
