19e829ab86d525dddc8c9df58166888575ecbc9b
timsuchanek <tim.suchanek@gmail.com>
2017-10-17 15:00:50 +0200

    Adjust example link. Closes #758

--- cli/packages/graphcool-cli-core/src/examples.ts
+++ cli/packages/graphcool-cli-core/src/examples.ts
@@ -29,7 +29,7 @@ export const defaultDefinition: ProjectDefinition = {
 # Just run \`graphcool deploy\` to have the first running Graphcool Service.
 #
 # Check out some examples:
-#    github.com/graphcool/examples
+#    https://github.com/graphcool/graphcool/tree/master/examples
 #
 # Here are the reference docs of this definition format:
 # https://docs-next.graph.cool/reference/service-definition/graphcool.yml-foatho8aip
@@ -130,7 +130,7 @@ export const changedDefaultDefinition: ProjectDefinition = {
 # Just uncomment it and run \`graphcool deploy\`
 #
 # Check out some examples:
-#    github.com/graphcool/examples
+#    https://github.com/graphcool/graphcool/tree/master/examples
 #
 # Happy Coding!
 
