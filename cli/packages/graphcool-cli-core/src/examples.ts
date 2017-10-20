2b95a81d8b26eba4390ef979492477a0f46b4115
timsuchanek <tim.suchanek@gmail.com>
2017-10-20 14:13:55 +0200

    Remove default token. Closes #920

--- cli/packages/graphcool-cli-core/src/examples.ts
+++ cli/packages/graphcool-cli-core/src/examples.ts
@@ -54,8 +54,8 @@ permissions:
 # Your root tokens used for functions to get full access to the API
 # Read more here:
 # https://docs-next.graph.cool/reference/auth/authentication/authentication-tokens-eip7ahqu5o
-rootTokens:
-- mytoken
+# rootTokens:
+# - mytoken
 
 `,
       files: {
