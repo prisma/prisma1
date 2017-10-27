d41a93b2d7dffd7de7fa7bf9f9ae1c7412b89b2c
Nikolas Burk <nikolas.burk@gmail.com>
2017-10-27 19:47:54 +0100

    use promise instead of async/await in node gateway example

--- examples/node-gateway-custom-schema/gateway/index.js
+++ examples/node-gateway-custom-schema/gateway/index.js
@@ -20,64 +20,67 @@ const HttpLink = require('apollo-link-http').HttpLink
 const fetch = require('node-fetch')
 const playground  = require('graphql-playground/middleware').express
 
-async function run() {
-  
-  // Step 1: Create local version of the CRUD API
-  const endpoint = '__SIMPLE_API_ENDPOINT__'  // looks like: https://api.graph.cool/simple/v1/__SERVICE_ID__
-  const link = new HttpLink({ uri: endpoint, fetch })
-  const graphcoolSchema = makeRemoteExecutableSchema({
-    schema: await introspectSchema(link),
-    link,
-  })
+function run() {
 
-  // Step 2: Define schema for the new API
-  const extendTypeDefs = `
-    extend type Query {
-      viewer: Viewer!
-    }
+    // Step 1: Create local version of the CRUD API
+    const endpoint = '__SIMPLE_API_ENDPOINT__'  // looks like: https://api.graph.cool/simple/v1/__SERVICE_ID__
+    const link = new HttpLink({ uri: endpoint, fetch })
+    introspectSchema(link).then(schema => {
+        const graphcoolSchema = makeRemoteExecutableSchema({
+            schema,
+            link
+        })
 
-    type Viewer {
-      me: User
-      topPosts(limit: Int): [Post!]!
-    }
-  `
+        // Step 2: Define schema for the new API
+        const extendTypeDefs = `
+          extend type Query {
+            viewer: Viewer!
+          }
+      
+          type Viewer {
+            me: User
+            topPosts(limit: Int): [Post!]!
+          }
+        `
 
-  // Step 3: Merge remote schema with new schema
-  const mergedSchemas = mergeSchemas({
-    schemas: [graphcoolSchema, extendTypeDefs],
-    resolvers: mergeInfo => ({
-      Query: {
-        viewer: () => ({}),
-      },
-      Viewer: {
-        me: {
-          resolve(parent, args, context, info) {
-            const alias = 'john' // should be determined from context
-            return mergeInfo.delegate('query', 'User', { alias }, context, info)
-          },
-        },
-        topPosts: {
-          resolve(parent, { limit }, context, info) {
-            return mergeInfo.delegate('query', 'allPosts', { first: limit }, context, info)
-          },
-        },
-      },
-    }),
-  })
+        // Step 3: Merge remote schema with new schema
+        const mergedSchemas = mergeSchemas({
+            schemas: [graphcoolSchema, extendTypeDefs],
+            resolvers: mergeInfo => ({
+                Query: {
+                    viewer: () => ({}),
+                },
+                Viewer: {
+                    me: {
+                        resolve(parent, args, context, info) {
+                            const alias = 'john' // should be determined from context
+                            return mergeInfo.delegate('query', 'User', { alias }, context, info)
+                        },
+                    },
+                    topPosts: {
+                        resolve(parent, { limit }, context, info) {
+                            return mergeInfo.delegate('query', 'allPosts', { first: limit }, context, info)
+                        },
+                    },
+                },
+            }),
+        })
 
-  // Step 4: Limit exposed operations from merged schemas 
-  // Hide every root field except `viewer`
-  const schema = transformSchema(mergedSchemas, {
-    '*': false,
-    viewer: true,
-  })
+        // Step 4: Limit exposed operations from merged schemas
+        // Hide every root field except `viewer`
+        const schema = transformSchema(mergedSchemas, {
+            '*': false,
+            viewer: true,
+        })
 
-  const app = express()
+        const app = express()
 
-  app.use('/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }))
-  app.use('/playground', playground({ endpoint: '/graphql' }))
+        app.use('/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }))
+        app.use('/playground', playground({ endpoint: '/graphql' }))
 
-  app.listen(3000, () => console.log('Server running. Open http://localhost:3000/playground to run queries.'))
+        app.listen(3000, () => console.log('Server running. Open http://localhost:3000/playground to run queries.'))
+    })
+
 }
 
 run().catch(console.error.bind(console))
