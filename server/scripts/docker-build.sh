b9efab0d4fed1fb1e3f45946e9dc807cb0274164
Marcus Böhm <wif.mboehm@gmx.de>
2017-11-01 13:57:50 +0100

    fix docker build

--- server/scripts/docker-build.sh
+++ server/scripts/docker-build.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/check-if-tests-must-execute.sh
 if [ $? -eq 0 ]; then
@@ -10,14 +8,14 @@ fi
 
 $DIR/kill-all-docker-containers.sh
 
-docker run -e "BRANCH=${BUILDKITE_BRANCH}" -e "PACKAGECLOUD_PW=${PACKAGECLOUD_PW}" -e "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}" -e "OTHER_REPO_OWNER=${OTHER_REPO_OWNER}" -e "OTHER_REPO=${OTHER_REPO}" -e "OTHER_REPO_FILE=${OTHER_REPO_FILE}" -v $(pwd):/root -v /var/run/docker.sock:/var/run/docker.sock schickling/scala-sbt-docker sbt docker publish propagateVersionToOtherRepo
+docker run -e "BRANCH=${BUILDKITE_BRANCH}" -e "PACKAGECLOUD_PW=${PACKAGECLOUD_PW}" -e "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}" -e "OTHER_REPO_OWNER=${OTHER_REPO_OWNER}" -e "OTHER_REPO=${OTHER_REPO}" -e "OTHER_REPO_FILE=${OTHER_REPO_FILE}" -v $(pwd):/root/build -w /root/build/server -v ~/.ivy2:/root/.ivy2 -v ~/.coursier:/root/.coursier  -v /var/run/docker.sock:/var/run/docker.sock schickling/scala-sbt-docker sbt docker
 
 docker images
 
 TAG=$(echo $BUILDKITE_COMMIT | cut -c1-7)
 
 
-for service in backend-api-relay backend-api-simple backend-api-system backend-api-simple-subscriptions backend-api-subscriptions-websocket backend-api-fileupload backend-api-schema-manager backend-workers graphcool-dev;
+for service in backend-api-relay backend-api-simple backend-api-system backend-api-simple-subscriptions backend-api-subscriptions-websocket backend-api-fileupload backend-api-schema-manager backend-workers graphcool-dev localfaas;
 do
   echo "Tagging graphcool/$service image with $TAG..."
   docker tag graphcool/$service graphcool/$service:$TAG
@@ -26,3 +24,4 @@ do
 done
 
 docker push graphcool/graphcool-dev:latest
+docker push graphcool/localfaas:latest
