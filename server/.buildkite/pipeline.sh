54a60546784412f65d23df3d59c838ecb16a8301
Dominic Petrick <dompetrick@gmail.com>
2017-11-03 17:40:56 +0100

    Buildkite fixes.

--- server/.buildkite/pipeline.sh
+++ server/.buildkite/pipeline.sh
@@ -2,7 +2,7 @@
 
 git diff --exit-code --name-only ${BUILDKITE_COMMIT} ${BUILDKITE_COMMIT}~1 | grep "server/"
 if [ $? -ne 0 ]; then
-  buildkite-agent pipeline upload empty-pipeline.yml
+  buildkite-agent pipeline upload ./server/.buildkite/empty-pipeline.yml
 else
-  buildkite-agent pipeline upload pipeline.yml
+  buildkite-agent pipeline upload ./server/.buildkite/pipeline.yml
 fi
