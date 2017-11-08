84c1a2e9bacdb49a2f542a8dfcd1b31d56472cf8
Dominic Petrick <dompetrick@gmail.com>
2017-11-08 20:52:19 +0100

    WIP custom router

--- server/libs/message-bus/src/main/scala/cool/graph/messagebus/pubsub/inmemory/InMemoryAkkaPubSub.scala
+++ server/libs/message-bus/src/main/scala/cool/graph/messagebus/pubsub/inmemory/InMemoryAkkaPubSub.scala
@@ -1,13 +1,19 @@
 package cool.graph.messagebus.pubsub.inmemory
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import java.util.concurrent.ConcurrentHashMap
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.cluster.pubsub.DistributedPubSub
 import akka.cluster.pubsub.DistributedPubSubMediator.Publish
+import akka.routing._
 import cool.graph.messagebus.Conversions.Converter
 import cool.graph.messagebus.PubSub
 import cool.graph.messagebus.pubsub._
+import cool.graph.messagebus.pubsub.inmemory.PubSubProtocol.Subscribe
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.immutable
 
 /**
   * PubSub implementation solely backed by actors, no external queueing or pubsub stack is utilized.
@@ -46,5 +52,37 @@ case class InMemoryAkkaPubSub[T](implicit val system: ActorSystem) extends PubSu
 
   override def shutdown: Unit = {
     _subscriptions.synchronized { _subscriptions.foreach(_.unsubscribe) }
+  }
+}
+
+object PubSubProtocol {
+  case class Subscribe(topic: String, actorRef: ActorRef)
+  case class Publish(topic: String, message: Any)
+}
+
+case class PubSubRouter() extends Actor {
+  val pubSubLogic = PubSubRoutingLogic()
+  var router      = Router(pubSubLogic, Vector.empty)
+
+  override def receive: Receive = {
+    case Subscribe(topic, ref) =>
+      context watch ref
+      router = router.addRoutee(ActorRefRoutee(ref))
+
+  }
+  // Explicitly don't watch for routee termination for performance?
+}
+
+case class PubSubRoutingLogic() extends RoutingLogic {
+  import scala.collection.JavaConverters._
+
+  val roundRobin    = RoundRobinRoutingLogic()
+  val subscriptions = new ConcurrentHashMap[String, Set[ActorRef]].asScala
+
+  def addSubscriber(topic: String, subscriber: ActorRef) = {}
+
+  def select(message: Any, routees: immutable.IndexedSeq[Routee]): Routee = {
+    val targets = (1 to nbrCopies).map(_ => roundRobin.select(message, routees))
+    SeveralRoutees(targets)
   }
 }
