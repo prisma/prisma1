c9a174c0f8ef38f79d2dc989230dae88a3831bfa
do4gr <matthias.oertel@gmx.net>
2017-11-20 10:11:01 +0100

    formatting

--- server/client-shared/src/main/scala/cool/graph/client/metrics/ApiMetricsMiddleware.scala
+++ server/client-shared/src/main/scala/cool/graph/client/metrics/ApiMetricsMiddleware.scala
@@ -19,11 +19,10 @@ class ApiMetricsMiddleware(
 
   def afterQuery(queryVal: QueryVal, context: MiddlewareQueryContext[RequestContextTrait, _, _]) = {
     (context.ctx.requestIp, context.ctx.projectId, context.ctx.clientId) match {
-      case (requestIp, Some(projectId), clientId) => {
+      case (requestIp, Some(projectId), clientId) =>
         // todo: generate list of features
 
         apiMetricActor ! ApiFeatureMetric(requestIp, testableTime.DateTime, projectId, clientId, context.ctx.listFeatureMetrics, context.ctx.isFromConsole)
-      }
       case _ => println("missing data for FieldMetrics")
     }
   }
