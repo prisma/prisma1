3787bb38eeabcbe6da4d23305c4a7f840f1c80fd
Marcus Böhm <wif.mboehm@gmx.de>
2017-11-24 16:03:17 +0100

    implement RenameInferer

--- server/deploy/src/main/scala/cool/graph/deploy/migration/RenameInferer.scala
+++ server/deploy/src/main/scala/cool/graph/deploy/migration/RenameInferer.scala
@@ -5,3 +5,31 @@ import sangria.ast.Document
 trait RenameInferer {
   def infer(graphQlSdl: Document): Renames
 }
+
+object RenameInferer extends RenameInferer {
+  import DataSchemaAstExtensions._
+
+  override def infer(graphQlSdl: Document): Renames = {
+    val modelNameMapping: Map[String, String] = graphQlSdl.objectTypes.map { objectType =>
+      objectType.oldName -> objectType.name
+    }.toMap
+
+    val enumNameMapping: Map[String, String] = graphQlSdl.enumTypes.map { enumType =>
+      enumType.oldName -> enumType.name
+    }.toMap
+
+    val fieldNameMapping: Map[String, String] = {
+      for {
+        objectType <- graphQlSdl.objectTypes
+        fieldDef   <- objectType.fields
+      } yield s"${objectType.oldName}.${fieldDef.oldName}" -> fieldDef.name
+    }.toMap
+
+    Renames(
+      models = modelNameMapping,
+      enums = enumNameMapping,
+      fields = fieldNameMapping
+    )
+  }
+
+}
