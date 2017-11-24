7008893989a9899af6968b066e2c7cf39a1ed2a8
Marcus Böhm <wif.mboehm@gmx.de>
2017-11-25 00:39:02 +0100

    improve MigrationStepsJsonFormatter

--- server/deploy/src/main/scala/cool/graph/deploy/database/persistence/MigrationStepsJsonFormatter.scala
+++ server/deploy/src/main/scala/cool/graph/deploy/database/persistence/MigrationStepsJsonFormatter.scala
@@ -31,9 +31,9 @@ object MigrationStepsJsonFormatter extends DefaultReads {
         isRequired   <- (json \ isRequiredField).validateOpt[Boolean]
         isList       <- (json \ isListField).validateOpt[Boolean]
         isUnique     <- (json \ isUniqueField).validateOpt[Boolean]
-        relation     <- doubleOptReads[String](relationField).reads(json)
-        defaultValue <- doubleOptReads[String](defaultValueField).reads(json)
-        enum         <- doubleOptReads[String](enumField).reads(json)
+        relation     <- (json \ relationField).validateDoubleOpt[String]
+        defaultValue <- (json \ defaultValueField).validateDoubleOpt[String]
+        enum         <- (json \ enumField).validateDoubleOpt[String]
       } yield {
         UpdateField(
           model = model,
@@ -59,7 +59,7 @@ object MigrationStepsJsonFormatter extends DefaultReads {
         isRequiredField -> x.isRequired,
         isListField     -> x.isList,
         isUniqueField   -> x.isUnique
-      ) ++ doubleOptWrites(relationField, x.relation) ++ doubleOptWrites(defaultValueField, x.defaultValue) ++ doubleOptWrites(enumField, x.enum)
+      ) ++ writeDoubleOpt(relationField, x.relation) ++ writeDoubleOpt(defaultValueField, x.defaultValue) ++ writeDoubleOpt(enumField, x.enum)
     }
   }
 
@@ -68,39 +68,52 @@ object MigrationStepsJsonFormatter extends DefaultReads {
   implicit val updateEnumFormat = Json.format[UpdateEnum]
 
   implicit val migrationStepFormat: Format[MigrationStep] = new Format[MigrationStep] {
-    override def reads(json: JsValue): JsResult[MigrationStep] = ???
+    val discriminatorField = "discriminator"
 
-    override def writes(step: MigrationStep): JsValue = step match {
-      case x: CreateModel => createModelFormat.writes(x)
-      case x: DeleteModel => deleteModelFormat.writes(x)
-      case x: UpdateModel => updateModelFormat.writes(x)
-      case x: CreateField => createFieldFormat.writes(x)
-      case x: DeleteField => deleteFieldFormat.writes(x)
-      case x: UpdateField => updateFieldFormat.writes(x)
-      case x: CreateEnum  => createEnumFormat.writes(x)
-      case x: DeleteEnum  => deleteEnumFormat.writes(x)
-      case x: UpdateEnum  => updateEnumFormat.writes(x)
+    override def reads(json: JsValue): JsResult[MigrationStep] = {
+      (json \ discriminatorField).validate[String].flatMap {
+        case "CreateModel" => createModelFormat.reads(json)
+        case "DeleteModel" => deleteModelFormat.reads(json)
+        case "UpdateModel" => updateModelFormat.reads(json)
+        case "CreateField" => createFieldFormat.reads(json)
+        case "DeleteField" => deleteFieldFormat.reads(json)
+        case "UpdateField" => updateFieldFormat.reads(json)
+        case "CreateEnum"  => createEnumFormat.reads(json)
+        case "DeleteEnum"  => deleteEnumFormat.reads(json)
+        case "UpdateEnum"  => updateEnumFormat.reads(json)
+      }
+    }
+
+    override def writes(step: MigrationStep): JsValue = {
+      val withOutDiscriminator = step match {
+        case x: CreateModel => createModelFormat.writes(x)
+        case x: DeleteModel => deleteModelFormat.writes(x)
+        case x: UpdateModel => updateModelFormat.writes(x)
+        case x: CreateField => createFieldFormat.writes(x)
+        case x: DeleteField => deleteFieldFormat.writes(x)
+        case x: UpdateField => updateFieldFormat.writes(x)
+        case x: CreateEnum  => createEnumFormat.writes(x)
+        case x: DeleteEnum  => deleteEnumFormat.writes(x)
+        case x: UpdateEnum  => updateEnumFormat.writes(x)
+      }
+      withOutDiscriminator ++ Json.obj(discriminatorField -> step.getClass.getSimpleName)
     }
   }
 
   implicit val migrationStepsFormat: Format[MigrationSteps] = Json.format[MigrationSteps]
 
-  def doubleOptReads[T](field: String)(implicit rds: Reads[T]): Reads[Option[Option[T]]] = new Reads[Option[Option[T]]] {
-    override def reads(json: JsValue): JsResult[Option[Option[T]]] = {
-      json.validate[JsObject].flatMap { jsObject =>
-        jsObject.value.get(field) match {
-          case None          => JsSuccess(None)
-          case Some(JsNull)  => JsSuccess(Some(None))
-          case Some(jsValue) => rds.reads(jsValue).map(v => Some(Some(v)))
-        }
-      }
-    }
-  }
-
-  def doubleOptWrites[T](field: String, opt: Option[Option[T]])(implicit writes: Writes[T]): JsObject = {
+  def writeDoubleOpt[T](field: String, opt: Option[Option[T]])(implicit writes: Writes[T]): JsObject = {
     opt match {
       case Some(innerOpt) => JsObject(Vector(field -> Json.toJson(innerOpt)))
       case None           => JsObject.empty
+    }
+  }
+
+  implicit class JsLookupResultExtension(jsLookUp: JsLookupResult) {
+    def validateDoubleOpt[T](implicit rds: Reads[T]): JsResult[Option[Option[T]]] = jsLookUp match {
+      case JsUndefined()     => JsSuccess(None)
+      case JsDefined(JsNull) => JsSuccess(Some(None))
+      case JsDefined(json)   => rds.reads(json).map(v => Some(Some(v)))
     }
   }
 }
