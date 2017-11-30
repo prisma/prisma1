3fbde28a836a331bd91cb05660fd04a06e7b9893
Marcus Böhm <wif.mboehm@gmx.de>
2017-11-30 22:49:34 +0100

    WIP: rolbacks for MigrationApplier

--- server/deploy/src/main/scala/cool/graph/deploy/migration/MigrationApplier.scala
+++ server/deploy/src/main/scala/cool/graph/deploy/migration/MigrationApplier.scala
@@ -6,81 +6,122 @@ import cool.graph.deploy.database.persistence.ProjectPersistence
 import cool.graph.deploy.migration.MigrationApplierJob.ScanForUnappliedMigrations
 import cool.graph.deploy.migration.mutactions._
 import cool.graph.shared.models._
+import slick.dbio.DBIOAction
 import slick.jdbc.MySQLProfile.backend.DatabaseDef
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 trait MigrationApplier {
-  def applyMigration(project: Project, migration: MigrationSteps): Future[Unit]
+  def applyMigration(project: Project, migration: MigrationSteps): Future[MigrationApplierResult]
 }
+case class MigrationApplierResult(succeeded: Boolean)
 
 case class MigrationApplierImpl(
     clientDatabase: DatabaseDef
 )(implicit ec: ExecutionContext)
     extends MigrationApplier {
-  override def applyMigration(project: Project, migration: MigrationSteps): Future[Unit] = {
-    val initialResult = Future.successful(())
 
+  override def applyMigration(project: Project, migration: MigrationSteps): Future[MigrationApplierResult] = {
     if (project.revision == 1) {
-      executeClientMutaction(CreateClientDatabaseForProject(project.id))
+      executeClientMutaction(CreateClientDatabaseForProject(project.id)).map(_ => MigrationApplierResult(succeeded = true))
     } else {
-      migration.steps.foldLeft(initialResult) { (previous, step) =>
-        for {
-          _ <- previous
-          _ <- applyStep(project, step)
-        } yield ()
+      val initialProgress = MigrationProgress(pendingSteps = migration.steps, appliedSteps = Vector.empty, isRollingback = false)
+      recurse(project, initialProgress)
+    }
+  }
+
+  def recurse(project: Project, progress: MigrationProgress): Future[MigrationApplierResult] = {
+    if (!progress.isRollingback) {
+      recurseForward(project, progress)
+    } else {
+      recurseForRollback(project, progress)
+    }
+  }
+
+  def recurseForward(project: Project, progress: MigrationProgress): Future[MigrationApplierResult] = {
+    if (progress.pendingSteps.nonEmpty) {
+      val (step, newProgress) = progress.popPending
+
+      val result = for {
+        _ <- applyStep(project, step)
+        x <- recurse(project, newProgress)
+      } yield x
+
+      result.recoverWith {
+        case exception =>
+          println("encountered exception while applying migration. will roll back.")
+          exception.printStackTrace()
+          recurseForRollback(project, newProgress.markForRollback)
       }
+    } else {
+      Future.successful(MigrationApplierResult(succeeded = true))
+    }
+  }
+
+  def recurseForRollback(project: Project, progress: MigrationProgress): Future[MigrationApplierResult] = {
+    if (progress.appliedSteps.nonEmpty) {
+      val (step, newProgress) = progress.popApplied
+
+      for {
+        _ <- unapplyStep(project, step).recover { case _ => () }
+        x <- recurse(project, newProgress)
+      } yield x
+    } else {
+      Future.successful(MigrationApplierResult(succeeded = false))
     }
   }
 
   def applyStep(project: Project, step: MigrationStep): Future[Unit] = {
-    step match {
-      case x: CreateModel =>
-        executeClientMutaction(CreateModelTable(project.id, x.name))
+    migrationStepToMutaction(project, step).map(executeClientMutaction).getOrElse(Future.successful(()))
+  }
 
-      case x: DeleteModel =>
-        executeClientMutaction(DeleteModelTable(project.id, x.name))
+  def unapplyStep(project: Project, step: MigrationStep): Future[Unit] = {
+    migrationStepToMutaction(project, step).map(executeClientMutactionRollback).getOrElse(Future.successful(()))
+  }
 
-      case x: UpdateModel =>
-        executeClientMutaction(RenameModelTable(projectId = project.id, oldName = x.name, newName = x.newName))
+  def migrationStepToMutaction(project: Project, step: MigrationStep): Option[ClientSqlMutaction] = step match {
+    case x: CreateModel =>
+      Some(CreateModelTable(project.id, x.name))
 
-      case x: CreateField =>
-        val model = project.getModelByName_!(x.model)
-        val field = model.getFieldByName_!(x.name)
-        if (field.isSystemField || !field.isScalar) {
-          Future.successful(())
-        } else {
-          executeClientMutaction(CreateColumn(project.id, model, field))
-        }
+    case x: DeleteModel =>
+      Some(DeleteModelTable(project.id, x.name))
 
-      case x: DeleteField =>
-        val model = project.getModelByName_!(x.name)
-        val field = model.getFieldByName_!(x.name)
-        executeClientMutaction(DeleteColumn(project.id, model, field))
+    case x: UpdateModel =>
+      Some(RenameModelTable(projectId = project.id, oldName = x.name, newName = x.newName))
 
-      case x: UpdateField =>
-        val oldProject = project // TODO: we need the old project here as well
-        val model      = project.getModelByName_!(x.model)
-        val newField   = project.getFieldByName_!(x.model, x.finalName)
-        val oldField   = oldProject.getFieldByName_!(x.model, x.name)
-        executeClientMutaction(UpdateColumn(project.id, model, oldField, newField))
+    case x: CreateField =>
+      val model = project.getModelByName_!(x.model)
+      val field = model.getFieldByName_!(x.name)
+      if (field.isSystemField || !field.isScalar) {
+        None
+      } else {
+        Some(CreateColumn(project.id, model, field))
+      }
 
-      case x: EnumMigrationStep =>
-        println(s"migration step of type ${x.getClass.getSimpleName} does not need to be applied to the client database. Will do nothing.")
-        Future.successful(())
+    case x: DeleteField =>
+      val model = project.getModelByName_!(x.name)
+      val field = model.getFieldByName_!(x.name)
+      Some(DeleteColumn(project.id, model, field))
 
-      case x: CreateRelation =>
-        val relation = project.getRelationByName_!(x.name)
-        executeClientMutaction(CreateRelationTable(project, relation))
+    case x: UpdateField =>
+      val oldProject = project // TODO: we need the old project here as well
+      val model      = project.getModelByName_!(x.model)
+      val newField   = project.getFieldByName_!(x.model, x.finalName)
+      val oldField   = oldProject.getFieldByName_!(x.model, x.name)
+      Some(UpdateColumn(project.id, model, oldField, newField))
 
-      case x: DeleteRelation =>
-        val relation = project.getRelationByName_!(x.name)
-        executeClientMutaction(DeleteRelationTable(project, relation))
-//      case x =>
-//        println(s"migration step of type ${x.getClass.getSimpleName} is not implemented yet. Will ignore it.")
-//        Future.successful(())
-    }
+    case x: EnumMigrationStep =>
+      println(s"migration step of type ${x.getClass.getSimpleName} does not need to be applied to the client database. Will do nothing.")
+      None
+
+    case x: CreateRelation =>
+      val relation = project.getRelationByName_!(x.name)
+      Some(CreateRelationTable(project, relation))
+
+    case x: DeleteRelation =>
+      val relation = project.getRelationByName_!(x.name)
+      Some(DeleteRelationTable(project, relation))
   }
 
   def executeClientMutaction(mutaction: ClientSqlMutaction): Future[Unit] = {
@@ -89,6 +130,40 @@ case class MigrationApplierImpl(
       _          <- clientDatabase.run(statements.sqlAction)
     } yield ()
   }
+
+  def executeClientMutactionRollback(mutaction: ClientSqlMutaction): Future[Unit] = {
+    for {
+      statements <- mutaction.rollback.get
+      _          <- clientDatabase.run(statements.sqlAction)
+    } yield ()
+  }
+
+//  private val emptyMutaction = new ClientSqlMutaction {
+//    val emptyResult = Future(ClientSqlStatementResult[Any](DBIOAction.successful(())))
+//
+//    override def execute: Future[ClientSqlStatementResult[Any]]          = emptyResult
+//    override def rollback: Option[Future[ClientSqlStatementResult[Any]]] = Some(emptyResult)
+//  }
+}
+
+case class MigrationProgress(
+    appliedSteps: Vector[MigrationStep],
+    pendingSteps: Vector[MigrationStep],
+    isRollingback: Boolean
+) {
+  def addAppliedStep(step: MigrationStep) = copy(appliedSteps = appliedSteps :+ step)
+
+  def popPending: (MigrationStep, MigrationProgress) = {
+    val step = pendingSteps.head
+    step -> copy(appliedSteps = appliedSteps :+ step, pendingSteps = pendingSteps.tail)
+  }
+
+  def popApplied: (MigrationStep, MigrationProgress) = {
+    val step = appliedSteps.last
+    step -> copy(appliedSteps = appliedSteps.dropRight(1))
+  }
+
+  def markForRollback = copy(isRollingback = true)
 }
 
 object MigrationApplierJob {
@@ -115,8 +190,12 @@ case class MigrationApplierJob(
     case Some(UnappliedMigration(project, migration)) =>
       println(s"found the unapplied migration in project ${project.id}: $migration")
       val doit = for {
-        _ <- applier.applyMigration(project, migration)
-        _ <- projectPersistence.markMigrationAsApplied(project, migration)
+        result <- applier.applyMigration(project, migration)
+        _ <- if (result.succeeded) {
+              projectPersistence.markMigrationAsApplied(project, migration)
+            } else {
+              Future.successful(())
+            }
       } yield ()
       doit.onComplete {
         case Success(_) =>
