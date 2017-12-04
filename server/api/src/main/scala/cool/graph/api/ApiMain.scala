d8fea37ad7f02a1ef9417425f7b63c6e4dc56931
Dominic Petrick <dompetrick@gmail.com>
2017-12-04 16:46:30 +0100

    Fix server heckmeck.

--- server/api/src/main/scala/cool/graph/api/ApiMain.scala
+++ server/api/src/main/scala/cool/graph/api/ApiMain.scala
@@ -1,21 +1,18 @@
 package cool.graph.api
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
+import cool.graph.akkautil.http.ServerExecutor
 import cool.graph.api.schema.SchemaBuilder
 import cool.graph.api.server.ApiServer
 
-import scala.concurrent.ExecutionContext.Implicits.global
-
 object ApiMain extends App with LazyLogging {
-  implicit val system          = ActorSystem("deploy-main")
+  implicit val system          = ActorSystem("api-main")
   implicit val materializer    = ActorMaterializer()
   implicit val apiDependencies = new ApiDependenciesImpl
-  val schemaBuilder            = SchemaBuilder()
-  val server                   = ApiServer(schemaBuilder = schemaBuilder)
 
-  Http().bindAndHandle(server.innerRoutes, "0.0.0.0", 9000).onSuccess {
-    case _ => logger.info("Server running on: 9000")
-  }
+  val schemaBuilder = SchemaBuilder()
+  val server        = ApiServer(schemaBuilder = schemaBuilder, "api")
+
+  ServerExecutor(9000, server).startBlocking()
 }
