de29740ede827319cadf485912869062c7da9aab
Dominic Petrick <dompetrick@gmail.com>
2017-12-16 20:13:39 +0100

    Debug output

--- server/scripts/beta_deploy.sh
+++ server/scripts/beta_deploy.sh
@@ -5,7 +5,7 @@ TOKEN=${GITHUB_TOKEN}
 echo "Fetching cb binary..."
 curl --header "Authorization: token ${TOKEN}" \
      --header 'Accept: application/vnd.github.v3.raw' \
-     --location https://api.github.com/repos/graphcool/coolbelt/releases/latest -sSL | \
+     --location "https://api.github.com/repos/graphcool/coolbelt/releases/latest" -sSL | \
      jq '.assets[] | select(.name == "coolbelt_linux") | .url' | \
      xargs -I "{}" \
          curl -sSL --header 'Accept: application/octet-stream' -o cb \
@@ -13,7 +13,9 @@ curl --header "Authorization: token ${TOKEN}" \
 
 chmod +x cb
 
-echo "Replacing images..."
+echo $(pwd)
+ls -lisah
 
+echo "Replacing images..."
 export CB_MODE=env
 cb service replace-all --customer graphcool --cluster database-beta-eu-west-1-dev
