67557f7c80ca6d9392eba01fbd1ff0015a9fea5d
Dominic Petrick <dompetrick@gmail.com>
2017-12-16 19:35:55 +0100

    Use linux binary for deployment.

--- server/scripts/beta_deploy.sh
+++ server/scripts/beta_deploy.sh
@@ -6,7 +6,7 @@ echo "Fetching cb binary..."
 curl --header "Authorization: token ${TOKEN}" \
      --header 'Accept: application/vnd.github.v3.raw' \
      --location https://api.github.com/repos/graphcool/coolbelt/releases/latest -sSL | \
-     jq '.assets[] | select(.name == "coolbelt") | .url' | \
+     jq '.assets[] | select(.name == "coolbelt_linux") | .url' | \
      xargs -I "{}" \
          curl -sSL --header 'Accept: application/octet-stream' -o cb \
          --location "{}?access_token=${TOKEN}"
