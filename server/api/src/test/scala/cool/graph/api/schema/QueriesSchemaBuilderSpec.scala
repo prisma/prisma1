1b4c3d114d093204a017a8eca1f75b420ed1fb97
Dominic Petrick <dompetrick@gmail.com>
2017-12-18 14:53:54 +0100

    merge

--- server/api/src/test/scala/cool/graph/api/schema/QueriesSchemaBuilderSpec.scala
+++ server/api/src/test/scala/cool/graph/api/schema/QueriesSchemaBuilderSpec.scala
@@ -3,20 +3,52 @@ package cool.graph.api.schema
 import cool.graph.api.ApiBaseSpec
 import cool.graph.shared.project_dsl.SchemaDsl
 import cool.graph.util.GraphQLSchemaAssertions
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 import sangria.renderer.SchemaRenderer
 
-class QueriesSchemaBuilderSpec extends FlatSpec with Matchers with ApiBaseSpec with GraphQLSchemaAssertions {
+import scala.collection.mutable
+
+class QueriesSchemaBuilderSpec extends WordSpec with Matchers with ApiBaseSpec with GraphQLSchemaAssertions {
   val schemaBuilder = testDependencies.apiSchemaBuilder
 
-  "the single item query for a model" should "be generated correctly" in {
-    val project = SchemaDsl() { schema =>
-      schema.model("Todo")
+  "the single item query for a model" must {
+    "be generated correctly" in {
+      val project = SchemaDsl() { schema =>
+        schema.model("Todo")
+      }
+
+      val schema = SchemaRenderer.renderSchema(schemaBuilder(project))
+
+      val query = schema.mustContainQuery("todo")
+      query should be("todo(where: TodoWhereUniqueInput!): Todo")
+    }
+  }
+
+  "the multi item query for a model" must {
+    "be generated correctly" in {
+      val project = SchemaDsl() { schema =>
+        schema.model("Todo")
+      }
+
+      val schema = SchemaRenderer.renderSchema(schemaBuilder(project))
+
+      val query = schema.mustContainQuery("todoes")
+      println(query)
+      query should be("todoes(where: TodoWhereInput, orderBy: TodoOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [Todo]!")
     }
 
-    val schema = SchemaRenderer.renderSchema(schemaBuilder(project))
+    "not include a *WhereUniqueInput if there is no unique field" in {
+      val project = SchemaDsl() { schema =>
+        schema.model("Todo").copy(fields = mutable.Buffer.empty).field("test", _.String)
+      }
 
-    val query = schema.mustContainQuery("todo")
-    query should be("todo(where: TodoWhereUniqueInput!): Todo")
+      println(project)
+
+      val schema = SchemaRenderer.renderSchema(schemaBuilder(project))
+
+      //      val query = schema.mustContainQuery("todoes")
+      println(schema)
+      //      query should be("todoes(where: TodoWhereInput, orderBy: TodoOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [Todo]!")
+    }
   }
 }
