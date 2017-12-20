4bc33c5aa3b919dac5b001f8f8272dbfc8a73a67
Søren Bramer Schmidt <sorenbs@gmail.com>
2017-12-20 16:01:00 +0100

    combine cluster and schema server again

--- server/deploy/src/main/scala/cool/graph/deploy/DeployMain.scala
+++ server/deploy/src/main/scala/cool/graph/deploy/DeployMain.scala
@@ -2,14 +2,13 @@ package cool.graph.deploy
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import cool.graph.akkautil.http.ServerExecutor
-import cool.graph.deploy.server.{ClusterServer, SchemaServer}
+import cool.graph.deploy.server.{ClusterServer}
 
 object DeployMain extends App {
   implicit val system       = ActorSystem("deploy-main")
   implicit val materializer = ActorMaterializer()
 
   val dependencies  = DeployDependenciesImpl()
-  val clusterServer = ClusterServer(dependencies.clusterSchemaBuilder, "cluster")
-  val schemaServer  = SchemaServer(dependencies.projectPersistence, "cluster")
-  ServerExecutor(8081, clusterServer, schemaServer).startBlocking()
+  val clusterServer = ClusterServer(dependencies.clusterSchemaBuilder, dependencies.projectPersistence, "cluster")
+  ServerExecutor(8081, clusterServer).startBlocking()
 }
--- server/single-server/src/main/scala/cool/graph/singleserver/SingleServerMain.scala
+++ server/single-server/src/main/scala/cool/graph/singleserver/SingleServerMain.scala
@@ -5,7 +5,7 @@ import akka.stream.ActorMaterializer
 import cool.graph.akkautil.http.ServerExecutor
 import cool.graph.api.ApiDependenciesImpl
 import cool.graph.api.server.ApiServer
-import cool.graph.deploy.server.{ClusterServer, SchemaServer}
+import cool.graph.deploy.server.{ClusterServer}
 
 object SingleServerMain extends App {
   implicit val system          = ActorSystem("single-server")
@@ -19,9 +19,7 @@ object SingleServerMain extends App {
 
   ServerExecutor(
     port = port,
-    ClusterServer(singleServerDependencies.clusterSchemaBuilder, "cluster"),
-    SchemaServer(singleServerDependencies.projectPersistence, "cluster"),
-    ApiServer(singleServerDependencies.apiSchemaBuilder),
-    SchemaServer(singleServerDependencies.projectPersistence)
+    ClusterServer(singleServerDependencies.clusterSchemaBuilder, singleServerDependencies.projectPersistence, "cluster"),
+    ApiServer(singleServerDependencies.apiSchemaBuilder)
   ).startBlocking()
 }
