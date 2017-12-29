f354e28d6484b718730e8becca21f483fba0ede3
do4gr <matthias.oertel@gmx.net>
2017-12-29 10:47:32 +0100

    introduce transactionally catch sql exception in the correct mutaction

--- server/api/src/main/scala/cool/graph/api/database/mutactions/GetFieldFromSQLUniqueException.scala
+++ server/api/src/main/scala/cool/graph/api/database/mutactions/GetFieldFromSQLUniqueException.scala
@@ -7,13 +7,6 @@ import cool.graph.api.mutations.MutationTypes.ArgumentValue
 
 object GetFieldFromSQLUniqueException {
 
-  def getFieldFromArgumentValueList(values: List[ArgumentValue], e: SQLIntegrityConstraintViolationException): String = {
-    values.filter(x => e.getCause.getMessage.contains("\'" + x.name + "_")) match {
-      case x if x.nonEmpty => "Field name = " + x.head.name
-      case _               => "Sorry, no more details available."
-    }
-  }
-
   def getFieldOptionFromArgumentValueList(values: List[ArgumentValue], e: SQLIntegrityConstraintViolationException): Option[String] = {
     values.filter(x => e.getCause.getMessage.contains("\'" + x.name + "_")) match {
       case x if x.nonEmpty => Some("Field name = " + x.head.name)
@@ -21,11 +14,11 @@ object GetFieldFromSQLUniqueException {
     }
   }
 
-  def getFieldFromCoolArgs(values: List[CoolArgs], e: SQLIntegrityConstraintViolationException): String = {
+  def getFieldOptionFromCoolArgs(values: List[CoolArgs], e: SQLIntegrityConstraintViolationException): Option[String] = {
     val combinedValues: List[(String, Any)] = values.flatMap(_.raw)
     combinedValues.filter(x => e.getCause.getMessage.contains("\'" + x._1 + "_")) match {
-      case x if x.nonEmpty => "Field name = " + x.head._1
-      case _               => "Sorry, no more details available."
+      case x if x.nonEmpty => Some("Field name = " + x.head._1)
+      case _               => None
     }
   }
 }
