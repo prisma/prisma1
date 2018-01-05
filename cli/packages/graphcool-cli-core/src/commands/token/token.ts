5a29022513de54dfba1a6f63c4553c5f54d1fd34
timsuchanek <tim.suchanek@gmail.com>
2018-01-05 13:32:56 +0100

    feat(token): add --copy option

--- cli/packages/graphcool-cli-core/src/commands/token/token.ts
+++ cli/packages/graphcool-cli-core/src/commands/token/token.ts
@@ -1,11 +1,18 @@
 import { Command, flags, Flags } from 'graphcool-cli-engine'
 import * as fs from 'fs-extra'
+import * as ncp from 'copy-paste'
 
 export default class Token extends Command {
   static topic = 'token'
   static description = 'Create a new service token'
+  static flags: Flags = {
+    copy: flags.boolean({
+      char: 'c',
+      description: 'Copy token to clipboard',
+    }),
+  }
   async run() {
-    const { reset } = this.flags
+    const { copy } = this.flags
     await this.definition.load(this.flags)
     const serviceName = this.definition.definition!.service
     const stage = this.definition.definition!.stage
@@ -17,7 +24,14 @@ export default class Token extends Command {
     if (!token) {
       this.out.log(`There is no secret set in the graphcool.yml`)
     } else {
-      this.out.log(token)
+      if (copy) {
+        await new Promise(r => {
+          ncp.copy(token, () => r())
+        })
+        this.out.log(`Token copied to clipboard`)
+      } else {
+        this.out.log(token)
+      }
     }
   }
 }
