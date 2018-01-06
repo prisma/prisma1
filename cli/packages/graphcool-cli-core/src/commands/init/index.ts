9dfb5180c662a123e5fde54ffc3ffe2d5c90c086
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2018-01-06 18:55:04 +0100

    local up -> local start

--- cli/packages/graphcool-cli-core/src/commands/init/index.ts
+++ cli/packages/graphcool-cli-core/src/commands/init/index.ts
@@ -67,7 +67,7 @@ To deploy your Graphcool service:
   ${chalk.green('graphcool deploy')}
 
 To start your local Graphcool cluster:
-  ${chalk.green('graphcool local up')}
+  ${chalk.green('graphcool local start')}
 
 You can find further instructions in the ${chalk.green('graphcool.yml')} file,
 which is the central service configuration.
