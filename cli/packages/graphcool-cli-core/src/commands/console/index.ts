3f9b6c10601adcb61da59cde3d09af508f7baf2a
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2018-01-06 18:20:53 +0100

    remove flags

--- cli/packages/graphcool-cli-core/src/commands/console/index.ts
+++ cli/packages/graphcool-cli-core/src/commands/console/index.ts
@@ -6,10 +6,7 @@ import * as childProcess from 'child_process'
 export default class ConsoleCommand extends Command {
   static topic = 'console'
   static description = 'Open Graphcool Console in browser'
-  static flags: Flags = {}
   async run() {
-    const { web } = this.flags
-    await this.definition.load(this.flags)
     const serviceName = this.definition.definition!.service!
     const stage = this.definition.definition!.stage
 
