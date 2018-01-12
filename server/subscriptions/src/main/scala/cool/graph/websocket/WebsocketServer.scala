1d54112d2b4cca1c0af2cf9eaf603732969e9b61
Marcus Böhm <wif.mboehm@gmx.de>
2018-01-12 11:09:27 +0100

    cleanup

--- server/subscriptions/src/main/scala/cool/graph/websocket/WebsocketServer.scala
+++ server/subscriptions/src/main/scala/cool/graph/websocket/WebsocketServer.scala
@@ -53,37 +53,12 @@ case class WebsocketServer(dependencies: SubscriptionDependencies, prefix: Strin
   }
 
   def newSession(projectId: String, v7protocol: Boolean): Flow[Message, Message, NotUsed] = {
-
-    val sessionId = Cuid.createCuid()
-
-//    val flow: Flow[Message, IncomingWebsocketMessage, Any] = ActorFlow
-//      .actorRef[Message, Message] { out =>
-//        Props(WebsocketSession(projectId, sessionId, out, services.requestsQueuePublisher, bugsnag))
-//      }(system, materializer)
-//      .collect {
-//        case TextMessage.Strict(text) ⇒ Future.successful(text)
-//        case TextMessage.Streamed(textStream) ⇒
-//          textStream
-//            .limit(100)
-//            .completionTimeout(5.seconds)
-//            .runFold("")(_ + _)
-//      }
-//      .mapAsync(3)(identity)
-//      .map(TextMessage.Strict)
-//      .collect {
-//        case TextMessage.Strict(text) =>
-//          incomingWebsocketMessageRate.inc()
-//          IncomingWebsocketMessage(projectId = projectId, sessionId = sessionId, body = text)
-//      }
-//
-//    val x: Sink[Message, Any] = flow.to(Sink.actorRef[IncomingWebsocketMessage](manager, CloseWebsocketSession(sessionId)))
-
     ActorFlow
       .actorRef[Message, Message] { out =>
         Props {
           WebsocketSession(
             projectId = projectId,
-            sessionId = sessionId,
+            sessionId = Cuid.createCuid(),
             outgoing = out,
             manager = manager,
             requestsPublisher = dependencies.requestsQueuePublisher,
@@ -93,50 +68,5 @@ case class WebsocketServer(dependencies: SubscriptionDependencies, prefix: Strin
         }
       }(system, materializer)
       .mapMaterializedValue(_ => akka.NotUsed)
-
-//    val incomingMessages =
-//      Flow[Message]
-//        .collect {
-//          case TextMessage.Strict(text) ⇒ Future.successful(text)
-//          case TextMessage.Streamed(textStream) ⇒
-//            textStream
-//              .limit(100)
-//              .completionTimeout(5.seconds)
-//              .runFold("")(_ + _)
-//        }
-//        .mapAsync(3)(identity)
-//        .map(TextMessage.Strict)
-//        .collect {
-//          case TextMessage.Strict(text) =>
-//            incomingWebsocketMessageRate.inc()
-//            IncomingWebsocketMessage(projectId = projectId, sessionId = sessionId, body = text)
-//        }
-//        .to(Sink.actorRef[IncomingWebsocketMessage](manager, CloseWebsocketSession(sessionId)))
-//
-//    val outgoingMessage: Source[Message, NotUsed] =
-//      Source
-//        .actorRef[OutgoingMessage](5, OverflowStrategy.fail)
-//        .mapMaterializedValue { outActor =>
-//          manager ! OpenWebsocketSession(projectId = projectId, sessionId = sessionId, outActor)
-//          NotUsed
-//        }
-//        .map(
-//          (outMsg: OutgoingMessage) => {
-//            outgoingWebsocketMessageRate.inc()
-//            TextMessage(outMsg.text)
-//          }
-//        )
-//        .via(OnCompleteStage(() => {
-//          manager ! CloseWebsocketSession(sessionId)
-//        }))
-//        .keepAlive(FiniteDuration(10, TimeUnit.SECONDS), () => {
-//          if (v7protocol) {
-//            TextMessage.Strict("""{"type":"ka"}""")
-//          } else {
-//            TextMessage.Strict("""{"type":"keepalive"}""")
-//          }
-//        })
-//
-//    Flow.fromSinkAndSource(incomingMessages, outgoingMessage)
   }
 }
