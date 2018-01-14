0b115af08a3614b99353d7ae7badbdc17995a719
Marcus Böhm <wif.mboehm@gmx.de>
2018-01-14 14:50:45 +0100

    cleanup

--- server/api/src/main/scala/cool/graph/api/schema/ObjectTypeBuilder.scala
+++ server/api/src/main/scala/cool/graph/api/schema/ObjectTypeBuilder.scala
@@ -53,30 +53,12 @@ class ObjectTypeBuilder(
             "aggregate",
             aggregateTypeForModel(model),
             resolve = (ctx: Context[ApiUserContext, IdBasedConnection[DataItem]]) => {
-//              val countArgs =
-//                ctx.value.parent.args.map(args => SangriaQueryArguments.createSimpleQueryArguments(None, None, None, None, None, args.filter, None))
-//              CountManyModelDeferred(model, ???)
               val emptyQueryArguments = QueryArguments(None, None, None, None, None, None, None)
               ctx.value.parent.args.getOrElse(emptyQueryArguments)
             }
           )
         )
       }
-//        sangria.schema.Field(
-//          "count",
-//          IntType,
-//          Some("Count of filtered result set without considering pagination arguments"),
-//          resolve = ctx => {
-//            val countArgs = ctx.value.parent.args.map(args => SangriaQueryArguments.createSimpleQueryArguments(None, None, None, None, None, args.filter, None))
-//
-//            ctx.value.parent match {
-//              case ConnectionParentElement(Some(nodeId), Some(field), _) =>
-//                CountToManyDeferred(field, nodeId, countArgs)
-//              case _ =>
-//                CountManyModelDeferred(model, countArgs)
-//            }
-//          }
-//        )
     )
   }
 
