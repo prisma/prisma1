89c01f7e8f69ce5e27a8e0224cc7361456caa772
timsuchanek <tim.suchanek@gmail.com>
2018-01-16 19:50:10 +0100

    fix test

--- cli/test.sh
+++ cli/test.sh
@@ -3,9 +3,10 @@
 set -e
 
 cd packages/prisma-cli-engine
-npm run build
-npm test
+yarn install
+yarn build
+yarn test
 cd ../../packages/prisma-cli-core
-npm test
+yarn test
 cd ../../packages/prisma-yml
-npm test
+yarn test
