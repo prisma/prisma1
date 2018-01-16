ed81cda9c420fcbf800f80bd2e11053481a5504a
Marcus Böhm <wif.mboehm@gmx.de>
2018-01-16 11:04:39 +0100

    introduce dedicated ProjectFetcher for single server

--- server/single-server/src/main/scala/cool/graph/singleserver/SingleServerDependencies.scala
+++ server/single-server/src/main/scala/cool/graph/singleserver/SingleServerDependencies.scala
@@ -38,9 +38,7 @@ case class SingleServerDependencies()(implicit val system: ActorSystem, val mate
   override val databases        = Databases.initialize(config)
   override val apiSchemaBuilder = CachedSchemaBuilder(SchemaBuilder(), invalidationPubSub)
   override val projectFetcher: ProjectFetcher = {
-    val schemaManagerEndpoint = config.getString("schemaManagerEndpoint")
-    val schemaManagerSecret   = config.getString("schemaManagerSecret")
-    val fetcher               = ProjectFetcherImpl(Vector.empty, config, schemaManagerEndpoint = schemaManagerEndpoint, schemaManagerSecret = schemaManagerSecret)
+    val fetcher = SingleServerProjectFetcher(projectPersistence)
     CachedProjectFetcherImpl(fetcher, invalidationPubSub)
   }
 
