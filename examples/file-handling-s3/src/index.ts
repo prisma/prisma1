cd03768734e0d167568cbfd7f5215ddad1ebc782
Nikolas <nikolas.burk@gmail.com>
2018-01-24 15:12:28 +0100

    Update index.ts

--- examples/file-handling-s3/src/index.ts
+++ examples/file-handling-s3/src/index.ts
@@ -13,9 +13,9 @@ const s3client = new S3({
 })
 
 const prisma = new Prisma({
-  endpoint: process.env.PRISMA_ENDPOINT,
-  secret: process.env.PRISMA_SECRET,
-  debug: true,
+  endpoint: process.env.PRISMA_ENDPOINT,  // Prisma service endpoint (see `~/.prisma/config.yml`)
+  secret: process.env.PRISMA_SECRET,      // `secret` taken from `prisma.yml`
+  debug: true                             // log all requests to the Prisma API to console
 })
 
 const server = new GraphQLServer({
