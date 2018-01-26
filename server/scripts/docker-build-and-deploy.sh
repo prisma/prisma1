e7c1939d511ffc2ab86c5e385ef74adeb9720699
Dominic Petrick <dompetrick@gmail.com>
2018-01-26 17:41:02 +0100

    Additionally tag major.minor

--- server/scripts/docker-build-and-deploy.sh
+++ server/scripts/docker-build-and-deploy.sh
@@ -13,7 +13,7 @@ if [ -z ${BUILDKITE_TAG} ]; then
     LAST_GIT_TAG=$(git describe --abbrev=0) # Get closest git tag
     TAG_ELEMENTS=(${LAST_GIT_TAG//./ })
     NEXT_VERSION="${TAG_ELEMENTS[0]}.$((${TAG_ELEMENTS[1]} + 1))"
-    LAST_DOCKER_TAG=$(curl -sS 'https://registry.hub.docker.com/v2/repositories/prismagraphql/prisma/tags/' | jq '."results"[]["name"]' --raw-output | grep -v latest | grep ${NEXT_VERSION}-beta | head -n 1)
+    LAST_DOCKER_TAG=$(curl -sS 'https://registry.hub.docker.com/v2/repositories/prismagraphql/prisma/tags/' | jq '."results"[]["name"]' --raw-output | grep -v latest | grep ${NEXT_VERSION}-beta- | head -n 1)
 
     echo "Last git tag: $LAST_GIT_TAG"
     echo "Next version: $NEXT_VERSION"
@@ -28,11 +28,25 @@ if [ -z ${BUILDKITE_TAG} ]; then
         echo "Rolling version: $rollingVersion"
         echo "Next docker tag: $NEXT_DOCKER_TAG"
     fi
+
+    # Always release -beta as well
+    ADDITIONALLY_RELEASE="$NEXT_VERSION-beta"
 else
-    # Stable release through tag
+    # Stable release through tag. Tag both x.x and x.x.x.
     NEXT_VERSION=${BUILDKITE_TAG}
     NEXT_DOCKER_TAG=${BUILDKITE_TAG}
+    IFS=. read major minor patch <<< ${BUILDKITE_TAG}
+
+    # Check which image we additionally have to tag. Either x.x or x.x.x, depending which tag we pushed
+    if [ -z $patch ]; then
+        # We are releaseing a x.x image, so tag x.x.x as well.
+        ADDITIONALLY_RELEASE="$BUILDKITE_TAG.0"
+    else
+        # We already have x.x.x, so we need to retag the x.x image.
+        ADDITIONALLY_RELEASE="$major.$minor"
+    fi
 fi
+
 
 docker run -e "BRANCH=${BUILDKITE_BRANCH}" -e "COMMIT_SHA=${BUILDKIT_COMMIT}" -e "CLUSTER_VERSION=${NEXT_DOCKER_TAG}" -v $(pwd):/root/build -w /root/build/server -v ~/.ivy2:/root/.ivy2 -v ~/.coursier:/root/.coursier  -v /var/run/docker.sock:/var/run/docker.sock graphcool/scala-sbt-docker sbt docker
 docker images
@@ -45,10 +59,10 @@ do
   echo "Pushing prismagraphql/$service:$NEXT_DOCKER_TAG..."
   docker push prismagraphql/$service:$NEXT_DOCKER_TAG
 
-  if [ -z ${BUILDKITE_TAG} ]; then
-    echo "Additionally tagging and pushing prismagraphql/$service:latest image with $NEXT_VERSION-beta..."
-    docker tag prismagraphql/$service:latest prismagraphql/$service:$NEXT_VERSION-beta
-    docker push prismagraphql/$service:$NEXT_VERSION-beta
+  if [ ! -z "$ADDITIONALLY_RELEASE" ]; then
+    echo "Additionally tagging and pushing prismagraphql/$service:latest image with $ADDITIONALLY_RELEASE..."
+    docker tag prismagraphql/$service:latest prismagraphql/$service:$ADDITIONALLY_RELEASE
+    docker push prismagraphql/$service:$ADDITIONALLY_RELEASE
   fi
 done
 
