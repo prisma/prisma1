55d7db6f13242894f1beb0736ae9cfb4888e7d2b
timsuchanek <tim.suchanek@gmail.com>
2018-02-01 09:15:09 +0100

    chore(publish): fix publish script

--- cli/scripts/publish.sh
+++ cli/scripts/publish.sh
@@ -138,14 +138,14 @@ if [ -z "$CIRCLE_TAG" ]; then
 
   # calc next minor
   step=1
-  nextMinor=$((minor + step))
+  nextMinor=$((latestMinor + step))
 
   nextLastNumber=0
 
-  echo "beta minor $betaMinor latest minor $latestMinor"
+  echo "beta minor $betaMinor latest minor $latestMinor next minor ${nextMinor}"
 
   # calc next last number
-  if [ $betaMinor == $latestMinor ]; then
+  if [ $betaMinor > $latestMinor ] && [ $betaMinor != $latestMinor ]; then
     echo "$betaMinor is greater than $latestMinor"
     nextLastNumber=$((betaLastNumber + step))
   fi
