a4f8bef20f283bfcc134dec15292b2fbb6b67a1d
super goat <p.nicolaou.13@gmail.com>
2018-02-05 18:04:23 +0000

    ✅ Init command tests [WIP]
    Added getTmpDir function to prisma-cli-core, and added failing tests for the init command

--- cli/packages/prisma-cli-core/src/commands/init/index.test.ts
+++ cli/packages/prisma-cli-core/src/commands/init/index.test.ts
@@ -1,18 +1,34 @@
 import * as nock from 'nock'
+import * as fs from 'fs-extra'
+import * as path from 'path'
 import { Config } from 'prisma-cli-engine'
+import { getTmpDir } from '../../test/getTmpDir'
 import Init from './'
 
 afterAll(() => {
   nock.cleanAll()
 })
 
-describe.skip('init', () => {
+describe('init', () => {
   test('test project', async () => {
-    const result = await Init
-      .mock
-      //'-t',
-      //'blank',
-      ()
-    expect(result.out.stdout.output).toContain('Written files:')
+
+    // I am not really sure what I need to do with this?
+    const tmpDir = getTmpDir()
+
+    const testFolder = './'
+
+    // This doesn't see to be the correct test.
+    fs.readdir(tmpDir, (err, files) => {
+      files.map(file => expect(file).toMatchSnapshot())
+    })
+
+    // console.log(tmpDir)
+    // const result = await Init
+    //   .mock
+    //   //'-t',
+    //   //'blank',
+    //   ()
+    // expect(result.out.stdout.output).toContain('Written files:')
+    // expect(true).toBe(true)
   })
 })
