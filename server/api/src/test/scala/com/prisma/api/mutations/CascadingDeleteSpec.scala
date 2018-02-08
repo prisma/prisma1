9a8534dc387f604bdd2ff93ba1e50d9c6c16a15c
do4gr <matthias.oertel@gmx.net>
2018-02-08 18:19:13 +0100

    more test cases

--- server/api/src/test/scala/com/prisma/api/mutations/CascadingDeleteSpec.scala
+++ server/api/src/test/scala/com/prisma/api/mutations/CascadingDeleteSpec.scala
@@ -192,6 +192,28 @@ class CascadingDeleteSpec extends FlatSpec with Matchers with ApiBaseSpec {
     database.runDbActionOnClientDb(DatabaseQueryBuilder.itemCountForTable(project.id, "_RelayId").as[Int]) should be(Vector(6))
   }
 
+  "If the parent is not cascading nothing on the path" should "be deleted except for the parent" in {
+    //         P-C-GC
+    val project = SchemaDsl() { schema =>
+      val parent     = schema.model("P").field_!("p", _.String, isUnique = true)
+      val child      = schema.model("C").field_!("c", _.String, isUnique = true)
+      val grandChild = schema.model("GC").field_!("gc", _.String, isUnique = true)
+
+      child.oneToOneRelation("p", "c", parent, modelAOnDelete = OnDelete.Cascade)
+      grandChild.oneToOneRelation("c", "gc", child)
+    }
+    database.setup(project)
+
+    server.executeQuerySimple("""mutation{createP(data:{p:"p", c: {create:{c: "c", gc :{create:{gc: "gc"}}}}}){p, c {c, gc{gc}}}}""", project)
+
+    server.executeQuerySimple("""mutation{deleteP(where: {p:"p"}){id}}""", project)
+    server.executeQuerySimple("""query{ps{p, c {c}}}""", project).toString should be("""{"data":{"ps":[]}}""")
+    server.executeQuerySimple("""query{cs{c, p {p}}}""", project).toString should be("""{"data":{"cs":[{"c":"c","p":null}]}}""")
+    server.executeQuerySimple("""query{gCs{gc, c {c}}}""", project).toString should be("""{"data":{"gCs":[{"gc":"gc","c":{"c":"c"}}]}}""")
+
+    database.runDbActionOnClientDb(DatabaseQueryBuilder.itemCountForTable(project.id, "_RelayId").as[Int]) should be(Vector(2))
+  }
+
   "P1!-C1! PM-SC1! relation deleting the parent marked cascading" should "work" in {
     //         P
     //       /   \
@@ -320,13 +342,54 @@ class CascadingDeleteSpec extends FlatSpec with Matchers with ApiBaseSpec {
     database.runDbActionOnClientDb(DatabaseQueryBuilder.itemCountForTable(project.id, "_RelayId").as[Int]) should be(Vector(3))
   }
 
+  "A deep uninterrupted path" should "error on a required relation violation at the end" in {
+    //         P-C-GC-D-E-F!
+    val project = SchemaDsl() { schema =>
+      val parent     = schema.model("P").field_!("p", _.String, isUnique = true)
+      val child      = schema.model("C").field_!("c", _.String, isUnique = true)
+      val grandChild = schema.model("GC").field_!("gc", _.String, isUnique = true)
+      val levelD     = schema.model("D").field_!("d", _.String, isUnique = true)
+      val levelE     = schema.model("E").field_!("e", _.String, isUnique = true)
+      val levelF     = schema.model("F").field_!("f", _.String, isUnique = true)
+
+      child.oneToOneRelation_!("p", "c", parent, modelBOnDelete = OnDelete.Cascade)
+      grandChild.oneToOneRelation_!("c", "gc", child, modelBOnDelete = OnDelete.Cascade)
+      levelD.manyToManyRelation("gc", "d", grandChild, modelBOnDelete = OnDelete.Cascade)
+      levelE.manyToManyRelation("d", "e", levelD, modelBOnDelete = OnDelete.Cascade)
+      levelF.oneToOneRelation_!("e", "f", levelE)
+    }
+    database.setup(project)
+
+    server.executeQuerySimple("""mutation{createP(data:{p:"p", c: {create:{c: "c", gc :{create:{gc: "gc"}}}}}){p, c {c, gc{gc}}}}""", project)
+    server.executeQuerySimple("""mutation{createP(data:{p:"p2", c: {create:{c: "c2", gc :{create:{gc: "gc2"}}}}}){p, c {c,gc{gc}}}}""", project)
+    server.executeQuerySimple("""mutation{createD(data:{d:"d", e: {create:[{e: "e", f: {create :{f:"f"}}}]}, gc: {connect:{gc: "gc"}}}){d}}""", project)
+
+    server.executeQuerySimpleThatMustFail(
+      """mutation{deleteP(where: {p:"p"}){id}}""",
+      project,
+      errorCode = 3042,
+      errorContains = """The change you are trying to make would violate the required relation '_FToE' between F and E"""
+    )
+
+    server.executeQuerySimple("""query{ps{p, c {c}}}""", project).toString should be("""{"data":{"ps":[{"p":"p","c":{"c":"c"}},{"p":"p2","c":{"c":"c2"}}]}}""")
+    server.executeQuerySimple("""query{cs{c, p {p}, gc {gc}}}""", project).toString should be(
+      """{"data":{"cs":[{"c":"c","p":{"p":"p"},"gc":{"gc":"gc"}},{"c":"c2","p":{"p":"p2"},"gc":{"gc":"gc2"}}]}}""")
+    server.executeQuerySimple("""query{gCs{gc, c {c}, d {d}}}""", project).toString should be(
+      """{"data":{"gCs":[{"gc":"gc","c":{"c":"c"},"d":[{"d":"d"}]},{"gc":"gc2","c":{"c":"c2"},"d":[]}]}}""")
+    server.executeQuerySimple("""query{ds{d, gc {gc},e {e}}}""", project).toString should be(
+      """{"data":{"ds":[{"d":"d","gc":[{"gc":"gc"}],"e":[{"e":"e"}]}]}}""")
+    server.executeQuerySimple("""query{fs{f, e {e}}}""", project).toString should be("""{"data":{"fs":[{"f":"f","e":{"e":"e"}}]}}""")
+
+    database.runDbActionOnClientDb(DatabaseQueryBuilder.itemCountForTable(project.id, "_RelayId").as[Int]) should be(Vector(9))
+  }
+
   "A required relation violation anywhere on the path" should "error and roll back all of the changes" in {
 
     /**           A           If cascading all the way down to D from A is fine, but deleting C would
-      *          /            violate a required relation on E then this should error and not delete
-      *         B             anything.
+      *          /            violate a required relation on E that is not cascading then this should
+      *         B             error and not delete anything.
       *          \
-      *          C - E
+      *          C . E
       *          /
       *         D
       */
@@ -358,6 +421,44 @@ class CascadingDeleteSpec extends FlatSpec with Matchers with ApiBaseSpec {
     )
   }
 
+  "Several relations between the same model" should "be handled correctly" in {
+
+    /**           A           If there are two relations between B and C and only one of them is marked
+      *          /            cascading, then only the nodes connected to C's which are connected to B
+      *         B             by this relations should be deleted.
+      *        /  :
+      *       Cs   C
+      *        \ /
+      *         D
+      */
+    val project = SchemaDsl() { schema =>
+      val a = schema.model("A").field_!("a", _.Float, isUnique = true)
+      val b = schema.model("B").field_!("b", _.Float, isUnique = true)
+      val c = schema.model("C").field_!("c", _.Float, isUnique = true)
+      val d = schema.model("D").field_!("d", _.Float, isUnique = true)
+
+      a.oneToOneRelation("b", "a", b, modelAOnDelete = OnDelete.Cascade)
+      b.manyToManyRelation("cs", "bs", c, modelAOnDelete = OnDelete.Cascade, relationName = Some("Relation1"))
+      c.manyToManyRelation("d", "c", d, modelAOnDelete = OnDelete.Cascade, relationName = Some("Relation2"))
+      b.oneToOneRelation("c", "b", c)
+    }
+    database.setup(project)
+
+    server.executeQuerySimple("""mutation{createA(data:{a: 10.10, b: {create:{b: 11.11}}}){a}}""", project)
+
+    server.executeQuerySimple("""mutation{updateB(where: {b: 11.11}, data:{cs: {create:[{c: 12.12},{c: 12.13}]}}){b}}""", project)
+    server.executeQuerySimple("""mutation{updateB(where: {b: 11.11}, data:{c: {create:{c: 12.99}}}){b}}""", project)
+
+    server.executeQuerySimple("""mutation{updateC(where: {c: 12.12}, data:{d: {create:{d: 13.13}}}){c}}""", project)
+    server.executeQuerySimple("""mutation{updateC(where: {c: 12.99}, data:{d: {create:{d: 13.99}}}){c}}""", project)
+
+    server.executeQuerySimple("""mutation{deleteA(where: {a:10.10}){a}}""", project)
+
+    server.executeQuerySimple("""query{as{a, b {b}}}""", project).toString should be("""{"data":{"as":[]}}""")
+    server.executeQuerySimple("""query{bs{b, c {c}, cs {c}}}""", project).toString should be("""{"data":{"bs":[]}}""")
+    server.executeQuerySimple("""query{cs{c, d {d}}}""", project).toString should be("""{"data":{"cs":[{"c":12.99,"d":[{"d":13.99}]}]}}""")
+    server.executeQuerySimple("""query{ds{d}}""", project).toString should be("""{"data":{"ds":[{"d":13.99}]}}""")
+  }
   //endregion
 
   //region  NESTED DELETE
