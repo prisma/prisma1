81519341b6b37d5665b1fc382bee2de539150df7
Marcus Böhm <wif.mboehm@gmx.de>
2018-02-12 19:12:40 +0100

    define metrics that will be displayed in the console

--- server/api/src/main/scala/com/prisma/api/ApiMetrics.scala
+++ server/api/src/main/scala/com/prisma/api/ApiMetrics.scala
@@ -2,7 +2,7 @@ package com.prisma.api
 
 import com.prisma.errors.BugsnagErrorReporter
 import com.prisma.metrics.{CustomTag, MetricsManager}
-import com.prisma.profiling.{JvmProfiler, MemoryProfiler}
+import com.prisma.profiling.JvmProfiler
 
 object ApiMetrics extends MetricsManager(BugsnagErrorReporter(sys.env.getOrElse("BUGSNAG_API_KEY", ""))) {
   // this is intentionally empty. Since we don't define metrics here, we need to load the object once so the profiler kicks in.
@@ -21,12 +21,18 @@ object ApiMetrics extends MetricsManager(BugsnagErrorReporter(sys.env.getOrElse(
 
   JvmProfiler.schedule(this)
 
+  val projectIdTag = CustomTag("projectId")
+
   val projectCacheGetCount          = defineCounter("projectCacheGetCount")
   val projectCacheMissCount         = defineCounter("projectCacheMissCount")
   val schemaBuilderBuildTimerMetric = defineTimer("schemaBuilderBuildTimer", CustomTag("projectId", recordingThreshold = 600))
   val sqlQueryTimer                 = defineTimer("sqlQueryTimer", CustomTag("projectId", recordingThreshold = 1000), CustomTag("queryName", recordingThreshold = 1000))
   val sqlDataChangeMutactionTimer   = defineTimer("sqlDataChangeMutactionTimer", CustomTag("projectId", recordingThreshold = 1000))
-  val requestDuration               = defineTimer("requestDuration", CustomTag("projectId", recordingThreshold = 1500))
   val mutactionCount                = defineCounter("mutactionCount", CustomTag("projectId", recordingThreshold = 100))
 
+  // these Metrics are consumed by the console to power the dashboard. Only change them with extreme caution!
+  val requestDuration    = defineTimer("requestDuration", projectIdTag)
+  val requestCounter     = defineCounter("requestCount", projectIdTag)
+  val databaseSize       = defineGauge("databaseSize") // ???
+  val subscriptionEvents = defineCounter("subscriptionEvents", projectIdTag)
 }
