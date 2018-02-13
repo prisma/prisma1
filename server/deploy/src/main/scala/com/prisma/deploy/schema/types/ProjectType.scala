9e237718469ade5023e6d0e17d0957f4f2f20612
Marcus Böhm <wif.mboehm@gmx.de>
2018-02-13 18:07:30 +0100

    expose sanitized metric key in project

--- server/deploy/src/main/scala/com/prisma/deploy/schema/types/ProjectType.scala
+++ server/deploy/src/main/scala/com/prisma/deploy/schema/types/ProjectType.scala
@@ -2,6 +2,7 @@ package com.prisma.deploy.schema.types
 
 import com.prisma.deploy.schema.SystemUserContext
 import com.prisma.shared.models
+import com.prisma.shared.models.Project
 import sangria.schema._
 
 object ProjectType {
@@ -9,9 +10,11 @@ object ProjectType {
     "Project",
     "This is a project",
     fields[SystemUserContext, models.Project](
-      Field("id", StringType, resolve = _.value.id),
+      Field("metricKey", StringType, resolve = ctx => metricKey(ctx.value)),
       Field("name", StringType, resolve = _.value.projectId.name),
       Field("stage", StringType, resolve = _.value.projectId.stage)
     )
   )
+
+  def metricKey(project: Project): String = project.id.replace('@', '-').replace('~', '-')
 }
