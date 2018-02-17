4a41720b5780ce007030293e8231d721b817d877
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2018-02-17 19:26:48 +0100

    Update index.ts
    create new `Prisma` instance for every request

--- examples/file-handling-s3/src/index.ts
+++ examples/file-handling-s3/src/index.ts
@@ -12,18 +12,16 @@ const s3client = new S3({
   },
 })
 
-const prisma = new Prisma({
-  endpoint: process.env.PRISMA_ENDPOINT,  // Prisma service endpoint (see `~/.prisma/config.yml`)
-  secret: process.env.PRISMA_SECRET,      // `secret` taken from `prisma.yml`
-  debug: true                             // log all requests to the Prisma API to console
-})
-
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
   context: req => ({
     ...req,
-    db: prisma,
+    db: new Prisma({
+      endpoint: process.env.PRISMA_ENDPOINT,  // Prisma service endpoint (see `~/.prisma/config.yml`)
+      secret: process.env.PRISMA_SECRET,      // `secret` taken from `prisma.yml`
+      debug: true                             // log all requests to the Prisma API to console
+    }),
   }),
 })
 
