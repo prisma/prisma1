5edfaa19b851ee186b62508d2624a89b04749248
Nilan Marktanner <nilan.marktanner@student.kit.edu>
2018-02-17 20:48:02 +0100

    Update index.ts (#1931)
    * Update index.ts
    
    create new `Prisma` instance for every request
    
    * fix second prisma usage
    
    * Update index.ts
    
    I am bad and I should feel bad :D
    
    * Update index.ts

--- examples/file-handling-s3/src/index.ts
+++ examples/file-handling-s3/src/index.ts
@@ -12,27 +12,30 @@ const s3client = new S3({
   },
 })
 
-const prisma = new Prisma({
-  endpoint: process.env.PRISMA_ENDPOINT,  // Prisma service endpoint (see `~/.prisma/config.yml`)
-  secret: process.env.PRISMA_SECRET,      // `secret` taken from `prisma.yml`
-  debug: true                             // log all requests to the Prisma API to console
-})
+const getPrismaInstance = () => {
+  return new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,  // Prisma service endpoint (see `~/.prisma/config.yml`)
+    secret: process.env.PRISMA_SECRET,      // `secret` taken from `prisma.yml`
+    debug: true                             // log all requests to the Prisma API to console
+  })
+}
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
   context: req => ({
     ...req,
-    db: prisma,
-  }),
+    db: getPrismaInstance(),
+  })
 })
 
 server.express.post(
   '/upload',
   fileApi({
     s3: s3client,
-    prisma,
-  }),
+    prisma: getPrismaInstance()
+  })
 )
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
