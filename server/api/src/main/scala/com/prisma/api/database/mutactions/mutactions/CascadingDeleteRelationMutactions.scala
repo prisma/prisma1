cbb13aa9dc9d257f8d728391e71eeab225ebb263
do4gr <matthias.oertel@gmx.net>
2018-03-05 17:24:59 +0100

    also fix cascading deletes required checks

--- server/api/src/main/scala/com/prisma/api/database/mutactions/mutactions/CascadingDeleteRelationMutactions.scala
+++ server/api/src/main/scala/com/prisma/api/database/mutactions/mutactions/CascadingDeleteRelationMutactions.scala
@@ -4,9 +4,9 @@ import java.sql.SQLException
 
 import com.prisma.api.database.DatabaseMutationBuilder._
 import com.prisma.api.database.mutactions.{ClientSqlDataChangeMutaction, ClientSqlStatementResult}
-import com.prisma.api.mutations.mutations.CascadingDeletes.{ModelEdge, NodeEdge, Path}
+import com.prisma.api.mutations.mutations.CascadingDeletes.{NodeEdge, Path}
 import com.prisma.api.schema.APIErrors.RequiredRelationWouldBeViolated
-import com.prisma.shared.models.{Project, Relation}
+import com.prisma.shared.models.{Field, Project, Relation}
 import com.prisma.util.gc_value.OtherGCStuff.parameterString
 import slick.dbio.DBIOAction
 
@@ -14,12 +14,20 @@ import scala.concurrent.Future
 
 case class CascadingDeleteRelationMutactions(project: Project, path: Path) extends ClientSqlDataChangeMutaction {
 
-  val relationFieldsNotOnPath         = path.lastModel.relationFields.filter(f => !path.edges.map(_.relation).contains(f.relation.get))
-  val requiredRelationFieldsNotOnPath = relationFieldsNotOnPath.filter(_.otherSideIsRequired(project))
+//  val relationFieldsNotOnPath         = path.lastModel.relationFields.filter(f => !path.edges.map(_.relation).contains(f.relation.get))
+//  val requiredRelationFieldsNotOnPath = relationFieldsNotOnPath.filter(_.otherSideIsRequired(project))
+//  val extendedPaths = requiredRelationFieldsNotOnPath.map(path.appendEdge(project, _))
+//  val requiredCheck = extendedPaths.map(oldParentFailureTrigger(project, _))
 
-  val extendedPaths = requiredRelationFieldsNotOnPath.map(path.appendEdge(project, _))
+  val fieldsWhereThisModelIsRequired =
+    project.schema.allFields.filter(f => f.isRequired && !f.isList && f.relatedModel(project.schema).contains(path.lastModel))
 
-  val requiredCheck = extendedPaths.map(oldParentFailureTrigger(project, _))
+  val otherFieldsWhereThisModelIsRequired = path.lastEdge match {
+    case Some(edge) => fieldsWhereThisModelIsRequired.filter(f => f != edge.parentField)
+    case None       => fieldsWhereThisModelIsRequired
+  }
+
+  val requiredCheck = otherFieldsWhereThisModelIsRequired.map(oldParentFailureTriggerByField(project, path, _))
 
   val deleteAction = List(cascadingDeleteChildActions(project.id, path))
 
@@ -35,16 +43,15 @@ case class CascadingDeleteRelationMutactions(project: Project, path: Path) exten
     })
   }
 
-  private def otherFailingRequiredRelationOnChild(cause: String): Option[Relation] = {
-    extendedPaths.collectFirst { case x: Path if causedByThisMutactionChildOnly(x, cause) => x.lastRelation_! }
-  }
+  private def otherFailingRequiredRelationOnChild(cause: String): Option[Relation] =
+    otherFieldsWhereThisModelIsRequired.collectFirst { case f if causedByThisMutactionChildOnly(f, cause) => f.relation.get }
 
-  private def causedByThisMutactionChildOnly(path: Path, cause: String) = {
-    val parentCheckString = s"`${path.lastRelation_!.id}` OLDPARENTPATHFAILURETRIGGER WHERE `${path.childSideOfLastEdge}`"
+  private def causedByThisMutactionChildOnly(field: Field, cause: String) = {
+    val parentCheckString = s"`${field.relation.get.id}` OLDPARENTPATHFAILURETRIGGERBYFIELD WHERE `${field.oppositeRelationSide.get}`"
 
-    path.lastEdge_! match {
-      case edge: NodeEdge => cause.contains(parentCheckString) && cause.contains(parameterString(edge.childWhere))
-      case _: ModelEdge   => cause.contains(parentCheckString)
+    path.lastEdge match {
+      case Some(edge: NodeEdge) => cause.contains(parentCheckString) && cause.contains(parameterString(edge.childWhere))
+      case _                    => cause.contains(parentCheckString)
     }
   }
 }
