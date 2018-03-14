97bee5109f6f3c9108be70bc7b7d0647b9ef01cd
Marcus Böhm <wif.mboehm@gmx.de>
2018-03-14 22:51:28 +0100

    optimize imports

--- server/servers/api/src/main/scala/com/prisma/api/server/RequestHandler.scala
+++ server/servers/api/src/main/scala/com/prisma/api/server/RequestHandler.scala
@@ -2,7 +2,6 @@ package com.prisma.api.server
 
 import akka.http.scaladsl.model.StatusCodes.OK
 import akka.http.scaladsl.model._
-import com.prisma.errors.{ErrorReporter, ProjectMetadata}
 import com.prisma.api.ApiDependencies
 import com.prisma.api.database.DataResolver
 import com.prisma.api.import_export.{BulkExport, BulkImport}
@@ -11,11 +10,11 @@ import com.prisma.api.schema.APIErrors.InvalidToken
 import com.prisma.api.schema._
 import com.prisma.auth.Auth
 import com.prisma.client.server.GraphQlRequestHandler
+import com.prisma.errors.{ErrorReporter, ProjectMetadata}
 import com.prisma.shared.models.{Project, ProjectWithClientId}
 import com.prisma.utils.`try`.TryExtensions._
-import io.netty.channel.unix.Errors
 import sangria.schema.Schema
-import spray.json.{JsArray, JsNumber, JsObject, JsString, JsValue}
+import spray.json.JsValue
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Failure
