c1434e1486e55f1dc2a6d21eb97e9c26fb8084d9
Marcus Böhm <wif.mboehm@gmx.de>
2018-03-19 19:19:08 +0100

    add extension method for prepared statement to set GcValues

---
+++ server/connectors/api-connector-mysql/src/main/scala/com/prisma/api/connector/mysql/database/JdbcExtensions.scala
@@ -0,0 +1,25 @@
+package com.prisma.api.connector.mysql.database
+
+import java.sql.PreparedStatement
+
+import com.prisma.gc_values._
+import org.joda.time.format.DateTimeFormat
+
+object JdbcExtensions {
+  val dateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS").withZoneUTC()
+
+  implicit class PreparedStatementExtensions(val ps: PreparedStatement) extends AnyVal {
+    def setGcValue(index: Int, value: GCValue): Unit = value match {
+      case gcValue: StringGCValue    => ps.setString(index, gcValue.value)
+      case gcValue: BooleanGCValue   => ps.setBoolean(index, gcValue.value)
+      case gcValue: IntGCValue       => ps.setInt(index, gcValue.value)
+      case gcValue: FloatGCValue     => ps.setDouble(index, gcValue.value)
+      case gcValue: GraphQLIdGCValue => ps.setString(index, gcValue.value)
+      case gcValue: DateTimeGCValue  => ps.setDate(index, new java.sql.Date(gcValue.value.getMillis))
+      case gcValue: EnumGCValue      => ps.setString(index, gcValue.value)
+      case gcValue: JsonGCValue      => ps.setString(index, gcValue.value.toString)
+      case NullGCValue               => ps.setNull(index, java.sql.Types.NULL)
+      case x                         => sys.error(s"This method must only be called with LeafGCValues. Was called with: ${x.getClass}")
+    }
+  }
+}
