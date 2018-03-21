af4ce493d6104f949ff5fb4da0670fb3560691f7
Marcus Böhm <wif.mboehm@gmx.de>
2018-03-21 14:47:24 +0100

    extract sql result set to gcvalue conversion into JdbcExtensions

--- server/connectors/api-connector-mysql/src/main/scala/com/prisma/api/connector/mysql/database/JdbcExtensions.scala
+++ server/connectors/api-connector-mysql/src/main/scala/com/prisma/api/connector/mysql/database/JdbcExtensions.scala
@@ -1,9 +1,13 @@
 package com.prisma.api.connector.mysql.database
 
-import java.sql.{PreparedStatement, Timestamp}
+import java.sql.{PreparedStatement, ResultSet, Timestamp}
 
 import com.prisma.gc_values._
+import com.prisma.shared.models.{Field, TypeIdentifier}
+import com.prisma.shared.models.TypeIdentifier.TypeIdentifier
+import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import play.api.libs.json.Json
 
 object JdbcExtensions {
   val dateTimeFormat = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS").withZoneUTC()
@@ -20,6 +24,40 @@ object JdbcExtensions {
       case gcValue: JsonGCValue      => ps.setString(index, gcValue.value.toString)
       case NullGCValue               => ps.setNull(index, java.sql.Types.NULL)
       case x                         => sys.error(s"This method must only be called with LeafGCValues. Was called with: ${x.getClass}")
+    }
+  }
+
+  implicit class ResultSetExtensions(val resultSet: ResultSet) extends AnyVal {
+
+    def getGcValue(name: String, typeIdentifier: TypeIdentifier.Value): GCValue = {
+      val gcValue: GCValue = typeIdentifier match {
+        case TypeIdentifier.String    => StringGCValue(resultSet.getString(name))
+        case TypeIdentifier.GraphQLID => GraphQLIdGCValue(resultSet.getString(name))
+        case TypeIdentifier.Enum      => EnumGCValue(resultSet.getString(name))
+        case TypeIdentifier.Int       => IntGCValue(resultSet.getInt(name))
+        case TypeIdentifier.Float     => FloatGCValue(resultSet.getDouble(name))
+        case TypeIdentifier.Boolean   => BooleanGCValue(resultSet.getBoolean(name))
+        case TypeIdentifier.DateTime =>
+          val sqlType = resultSet.getTimestamp(name)
+          if (sqlType != null) {
+            DateTimeGCValue(new DateTime(sqlType.getTime))
+          } else {
+            NullGCValue
+          }
+        case TypeIdentifier.Json =>
+          val sqlType = resultSet.getString(name)
+          if (sqlType != null) {
+            JsonGCValue(Json.parse(sqlType))
+          } else {
+            NullGCValue
+          }
+        case TypeIdentifier.Relation => sys.error("TypeIdentifier.Relation is not supported here")
+      }
+      if (resultSet.wasNull) { // todo: should we throw here if the field is required but it is null?
+        NullGCValue
+      } else {
+        gcValue
+      }
     }
   }
 }
