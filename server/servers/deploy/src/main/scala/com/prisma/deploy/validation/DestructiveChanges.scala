a1463cef1fda3f24ed5a811615c4d7814f26c6e5
do4gr <matthias.oertel@gmx.net>
2018-04-17 11:37:23 +0200

    add model name to error

--- server/servers/deploy/src/main/scala/com/prisma/deploy/validation/DestructiveChanges.scala
+++ server/servers/deploy/src/main/scala/com/prisma/deploy/validation/DestructiveChanges.scala
@@ -46,7 +46,9 @@ case class DestructiveChanges(persistencePlugin: DeployConnector, project: Proje
           case true =>
             clientDataResolver.existsByModel(existingModel.name).map {
               case true =>
-                Vector(SchemaError.global("You are creating a required field but there are already nodes present that would violate that constraint."))
+                Vector(
+                  SchemaError.global(
+                    s"You are creating a required field on $existingModel but there are already nodes present that would violate that constraint."))
               case false => Vector.empty
             }
 
