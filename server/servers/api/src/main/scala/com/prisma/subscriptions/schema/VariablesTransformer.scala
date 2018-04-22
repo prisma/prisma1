0f3720e1136e377150c5d10397900c62d27c5d42
do4gr <matthias.oertel@gmx.net>
2018-04-22 14:06:12 +0200

    clean up variables transformer

--- server/servers/api/src/main/scala/com/prisma/subscriptions/schema/VariablesTransformer.scala
+++ server/servers/api/src/main/scala/com/prisma/subscriptions/schema/VariablesTransformer.scala
@@ -5,45 +5,25 @@ import play.api.libs.json._
 
 object VariablesTransformer {
   def transformVariables(variables: JsValue, mutation: ModelMutationType, updatedFields: Option[List[String]]): JsValue = {
-    val mutationName = mutation.toString
+    val mutationName  = mutation.toString
+    val isUpdate      = mutationName == "UPDATED"
+    lazy val fieldSet = updatedFields.get.toSet
 
-    def mutationIn(input: JsObject): JsObject = {
+    def removeUpdated(input: JsObject): JsObject = {
       JsObject(input.fields.map {
-        case ("mutation_in", JsString(action))                                                 => ("boolean", JsBoolean(action == mutationName))
-        case ("mutation_in", JsArray(values))                                                  => ("boolean", JsBoolean(values.map(_.as[JsString].value).contains(mutationName)))
-        case (key: String, obj: JsObject)                                                      => (key, mutationIn(obj))
-        case (key: String, JsArray(eles)) if eles.nonEmpty && eles.head.isInstanceOf[JsObject] => (key, JsArray(eles.map(v => mutationIn(v.as[JsObject]))))
-        case (key: String, arr: JsArray)                                                       => (key, arr)
-        case (key: String, leafJsValue)                                                        => (key, leafJsValue)
+        case ("mutation_in", JsString(action))                                         => ("boolean", JsBoolean(action == mutationName))
+        case ("mutation_in", JsArray(values))                                          => ("boolean", JsBoolean(values.map(_.as[JsString].value).contains(mutationName)))
+        case ("updatedFields_contains", JsString(field)) if isUpdate                   => ("boolean", JsBoolean(fieldSet.contains(field)))
+        case ("updatedFields_contains_every", JsArray(values)) if isUpdate             => ("boolean", JsBoolean(values.map(_.as[JsString].value).toSet.subsetOf(fieldSet)))
+        case ("updatedFields_contains_some", JsArray(values)) if isUpdate              => ("boolean", JsBoolean(values.map(_.as[JsString].value).exists(fieldSet.contains)))
+        case (key, obj: JsObject)                                                      => (key, removeUpdated(obj))
+        case (key, JsArray(eles)) if eles.nonEmpty && eles.head.isInstanceOf[JsObject] => (key, JsArray(eles.map(v => removeUpdated(v.as[JsObject]))))
+        case (key, arr: JsArray)                                                       => (key, arr)
+        case (key, leafJsValue)                                                        => (key, leafJsValue)
       })
     }
 
-    mutationName == "UPDATED" match {
-      case false =>
-        mutationIn(variables.as[JsObject])
-
-      case true =>
-        val removedMutationIn = mutationIn(variables.as[JsObject])
-        val fieldSet          = updatedFields.get.toSet
-
-        def removedUpdatedFields(input: JsObject): JsObject = {
-          JsObject(input.fields.map {
-            case ("updatedFields_contains", JsString(field))       => ("boolean", JsBoolean(fieldSet.contains(field)))
-            case ("updatedFields_contains_every", JsArray(values)) => ("boolean", JsBoolean(values.map(_.as[JsString].value).toSet.subsetOf(fieldSet)))
-            case ("updatedFields_contains_some", JsArray(values))  => ("boolean", JsBoolean(values.map(_.as[JsString].value).exists(fieldSet.contains)))
-            case ("mutation_in", JsString(action))                 => ("boolean", JsBoolean(action == mutationName))
-            case ("mutation_in", JsArray(values))                  => ("boolean", JsBoolean(values.map(_.as[JsString].value).contains(mutationName)))
-            case (key: String, obj: JsObject)                      => (key, removedUpdatedFields(obj))
-            case (key: String, JsArray(eles)) if eles.nonEmpty && eles.head.isInstanceOf[JsObject] =>
-              (key, JsArray(eles.map(v => removedUpdatedFields(v.as[JsObject]))))
-            case (key: String, arr: JsArray) => (key, arr)
-            case (key: String, leafJsValue)  => (key, leafJsValue)
-          })
-        }
-
-        removedUpdatedFields(removedMutationIn)
-    }
-
+    removeUpdated(variables.as[JsObject])
   }
 
 }
