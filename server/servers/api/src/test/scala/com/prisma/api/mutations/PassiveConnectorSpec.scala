0d89e35d35225d8d9803d475184e8fadd0c5ab5f
Marcus Böhm <wif.mboehm@gmx.de>
2018-05-01 18:33:09 +0200

    restructure spec

--- server/servers/api/src/test/scala/com/prisma/api/mutations/PassiveConnectorSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/PassiveConnectorSpec.scala
@@ -5,9 +5,19 @@ import com.prisma.deploy.connector.postgresql.PostgresDeployConnector
 import com.prisma.shared.schema_dsl.SchemaDsl
 import org.scalatest.{FlatSpec, Matchers}
 
-class PassiveConnectorSpec extends FlatSpec with Matchers with ApiSpecBase {
-  //
+trait PassiveConnectorSpec extends FlatSpec with Matchers with ApiSpecBase {
   val projectId = "passive_test"
+
+  def executeOnInternalDatabase(sql: String) = {
+    val connector = testDependencies.deployConnector.asInstanceOf[PostgresDeployConnector]
+    val session   = connector.internalDatabase.createSession()
+    val statement = session.createStatement()
+    statement.execute(sql)
+    session.close()
+  }
+}
+
+class PassiveConnectorSpecForInlineRelations extends PassiveConnectorSpec {
 
   val inlineRelationSchema = s"""
     |DROP SCHEMA IF EXISTS $projectId CASCADE;
@@ -40,14 +50,6 @@ class PassiveConnectorSpec extends FlatSpec with Matchers with ApiSpecBase {
     """.stripMargin
   }
 
-  def executeOnInternalDatabase(sql: String) = {
-    val connector = testDependencies.deployConnector.asInstanceOf[PostgresDeployConnector]
-    val session   = connector.internalDatabase.createSession()
-    val statement = session.createStatement()
-    statement.execute(sql)
-    session.close()
-  }
-
   "A Create Mutation" should "create and return item" in {
     executeOnInternalDatabase(inlineRelationSchema)
     val res = server.query(
@@ -59,11 +61,6 @@ class PassiveConnectorSpec extends FlatSpec with Matchers with ApiSpecBase {
       project = inlineRelationProject
     )
     res.toString should be(s"""{"data":{"createList":{"name":"the list name"}}}""")
-//
-//    val queryRes = server.query("""{ scalarModels{optString, optInt, optFloat, optBoolean, optEnum, optDateTime, optJson}}""", project = project)
-//
-//    queryRes.toString should be(
-//      s"""{"data":{"scalarModels":[{"optJson":[1,2,3],"optInt":1337,"optBoolean":true,"optDateTime":"2016-07-31T23:59:01.000Z","optString":"lala${TroubleCharacters.value}","optEnum":"A","optFloat":1.234}]}}""")
   }
 
   "A Create Mutation" should "created nested items" in {
@@ -118,5 +115,73 @@ class PassiveConnectorSpec extends FlatSpec with Matchers with ApiSpecBase {
       project = project
     )
     res.toString should be(s"""{"data":{"createList":{"theName":"the list"}}}""")
+  }
+}
+
+class PassiveConnectorSpecForTableRelations extends FlatSpec with PassiveConnectorSpec with Matchers with ApiSpecBase {
+  val relationTableSchema =
+    s"""
+       |DROP SCHEMA IF EXISTS $projectId CASCADE;
+       |CREATE SCHEMA $projectId;
+       |CREATE TABLE $projectId.list (
+       |  id      varchar PRIMARY KEY  -- implicit primary key constraint
+       |, name    text NOT NULL
+       |);
+       |
+       |CREATE TABLE $projectId.todo (
+       |  id       varchar PRIMARY KEY
+       |, title     text NOT NULL
+       |);
+       |
+       |CREATE TABLE $projectId.list_to_todo (
+       |  list_id varchar REFERENCES $projectId.list (id) ON UPDATE CASCADE ON DELETE CASCADE
+       |, todo_id varchar REFERENCES $projectId.todo (id) ON UPDATE CASCADE ON DELETE CASCADE
+       |);
+     """.stripMargin
+
+  val relationTableProject = SchemaDsl.fromString(id = projectId, withReservedFields = false) {
+    """
+      | type List @model(table: "list"){
+      |   id: String!
+      |   name: String!
+      |   todos: [Todo] @relation(table: "list_to_todo")
+      | }
+      |
+      | type Todo @model(table: "todo"){
+      |   id: String!
+      |   title: String!
+      |   list: List
+      | }
+    """.stripMargin
+  }
+
+  "A Create Mutation" should "create and return item" in {
+    executeOnInternalDatabase(relationTableSchema)
+    val res = server.query(
+      s"""mutation {
+         |  createList(data: {
+         |    name: "the list name"
+         |  }){ name }
+         |}""".stripMargin,
+      project = relationTableProject
+    )
+    res.toString should be(s"""{"data":{"createList":{"name":"the list name"}}}""")
+  }
+
+  "A Create Mutation" should "created nested items" in {
+    executeOnInternalDatabase(relationTableSchema)
+    // how do we implement this? We would have to reorder in this case?
+    val res = server.query(
+      s"""mutation {
+         |  createTodo(data: {
+         |    title: "the todo"
+         |    list: {
+         |      create: { name: "the list" }
+         |    }
+         |  }){ title }
+         |}""".stripMargin,
+      project = relationTableProject
+    )
+    res.toString should be(s"""{"data":{"createTodo":{"title":"the todo"}}}""")
   }
 }
