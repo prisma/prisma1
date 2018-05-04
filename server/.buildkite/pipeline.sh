0773b293ae701a18327ade1367776a934c1a20b6
do4gr <matthias.oertel@gmx.net>
2018-05-04 18:09:32 +0200

    add Postgres integration tests

--- server/.buildkite/pipeline.sh
+++ server/.buildkite/pipeline.sh
@@ -9,11 +9,17 @@ static=$(printf "    - label: \":mysql: MySql API connector\"
     - label: \":mysql: MySql deploy connector\"
       command: cd server && ./.buildkite/scripts/test.sh deploy-connector-mysql mysql
 
+    - label: \":scala: integration-tests-mysql\"
+      command: cd server && ./.buildkite/scripts/test.sh integration-tests mysql
+
     - label: \":postgres: Postgres API connector\"
       command: cd server && ./.buildkite/scripts/test.sh api-connector-postgresql postgres
 
     - label: \":postgres: Postgres deploy connector\"
       command: cd server && ./.buildkite/scripts/test.sh deploy-connector-postgresql postgres
+
+    - label: \":scala: integration-tests-postgres\"
+      command: cd server && ./.buildkite/scripts/test.sh integration-tests postgres
 
     # Libs are not specific to a connector, simply run with mysql
     - label: \":scala: libs\"
@@ -21,9 +27,7 @@ static=$(printf "    - label: \":mysql: MySql API connector\"
 
     - label: \":scala: subscriptions\"
       command: cd server && ./.buildkite/scripts/test.sh subscriptions mysql
-
-    - label: \":scala: integration-tests-mysql\"
-      command: cd server && ./.buildkite/scripts/test.sh integration-tests-mysql mysql
+      
 ")
 
 optional=""
