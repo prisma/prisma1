d74689776ad8213308d789d6c97cfa0a2e557a3e
Marcus Böhm <wif.mboehm@gmx.de>
2018-05-07 15:28:11 +0200

    ignore flaky test in mysql

--- server/servers/api/src/test/scala/com/prisma/api/mutations/UpdateMutationSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/UpdateMutationSpec.scala
@@ -1,5 +1,6 @@
 package com.prisma.api.mutations
 
+import com.prisma.IgnoreMySql
 import com.prisma.api.ApiSpecBase
 import com.prisma.api.util.TroubleCharacters
 import com.prisma.shared.schema_dsl.SchemaDsl
@@ -155,7 +156,7 @@ class UpdateMutationSpec extends FlatSpec with Matchers with ApiSpecBase {
     )
   }
 
-  "Updating" should "change the updatedAt datetime" in {
+  "Updating" should "change the updatedAt datetime" taggedAs (IgnoreMySql) in {
     val project = SchemaDsl() { schema =>
       schema
         .model("Todo")
