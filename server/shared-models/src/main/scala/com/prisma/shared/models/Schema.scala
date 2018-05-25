1e9f5c99e8727a69b88d4d13697d12503b4430d3
Marcus Böhm <wif.mboehm@gmx.de>
2018-05-25 11:44:50 +0200

    remove obsolete toLowerCase calls

--- server/shared-models/src/main/scala/com/prisma/shared/models/Schema.scala
+++ server/shared-models/src/main/scala/com/prisma/shared/models/Schema.scala
@@ -11,11 +11,10 @@ case class Schema(
     relationTemplates: List[RelationTemplate] = List.empty,
     enums: List[Enum] = List.empty
 ) {
-  val models    = modelTemplates.map(_.build(this))
-  val relations = relationTemplates.map(_.build(this))
-
-  def allFields: Seq[Field]                 = models.flatMap(_.fields)
-  def allRelationFields: Seq[RelationField] = models.flatMap(_.relationFields)
+  val models                                = modelTemplates.map(_.build(this))
+  val relations                             = relationTemplates.map(_.build(this))
+  val allFields: Seq[Field]                 = models.flatMap(_.fields)
+  val allRelationFields: Seq[RelationField] = models.flatMap(_.relationFields)
 
   def fieldsWhereThisModelIsRequired(model: Model) = allRelationFields.filter(f => f.isRequired && !f.isList && f.relatedModel_! == model)
 
@@ -23,19 +22,13 @@ case class Schema(
     models.find(_.stableIdentifier == stableId).getOrElse(throw SharedErrors.InvalidModel(s"Could not find a model for the stable identifier: $stableId"))
   }
 
-  // note: mysql columns are case insensitive, so we have to be as well. But we could make them case sensitive https://dev.mysql.com/doc/refman/5.6/en/case-sensitivity.html
-  def getModelByName(name: String): Option[Model] = models.find(_.name.toLowerCase() == name.toLowerCase())
-  def getModelByName_!(name: String): Model       = getModelByName(name).getOrElse(throw SharedErrors.InvalidModel(s"No model with name: $name found."))
-
-  def getFieldByName(model: String, name: String): Option[Field] = getModelByName(model).flatMap(_.getFieldByName(name))
-  def getFieldByName_!(model: String, name: String): Field       = getModelByName_!(model).getFieldByName_!(name)
-
-  // note: mysql columns are case insensitive, so we have to be as well
-  def getEnumByName(name: String): Option[Enum] = enums.find(_.name.toLowerCase == name.toLowerCase)
-
-  def getRelationByName(name: String): Option[Relation] = relations.find(_.name == name)
-  def getRelationByName_!(name: String): Relation       = getRelationByName(name).get
-
+  def getModelByName_!(name: String): Model                                         = getModelByName(name).getOrElse(throw SharedErrors.InvalidModel(s"No model with name: $name found."))
+  def getModelByName(name: String): Option[Model]                                   = models.find(_.name == name)
+  def getFieldByName_!(model: String, name: String): Field                          = getModelByName_!(model).getFieldByName_!(name)
+  def getFieldByName(model: String, name: String): Option[Field]                    = getModelByName(model).flatMap(_.getFieldByName(name))
+  def getEnumByName(name: String): Option[Enum]                                     = enums.find(_.name == name)
+  def getRelationByName_!(name: String): Relation                                   = getRelationByName(name).get
+  def getRelationByName(name: String): Option[Relation]                             = relations.find(_.name == name)
   def getRelationsThatConnectModels(modelA: String, modelB: String): List[Relation] = relations.filter(_.connectsTheModels(modelA, modelB))
 
 }
