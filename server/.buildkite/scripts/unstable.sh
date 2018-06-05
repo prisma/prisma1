f7239db65e42a838cecf2463e1efc342dc0c3782
Dominic Petrick <dompetrick@gmail.com>
2018-06-05 20:14:23 +0200

    Scope unstable script to channel branches.

--- server/.buildkite/scripts/unstable.sh
+++ server/.buildkite/scripts/unstable.sh
@@ -2,18 +2,21 @@
 
 set -e
 
+CHANNEL="${1:?Provide the channel this script is run on (e.g. alpha, beta)}"
+VERSION_OFFSET="${2:?Offset in minor version to build against}"
+
 if [ -n "$BUILDKITE_TAG" ]; then
-    # Triggered by a tag - has to be beta to have any effect
-    if [[ ! $BUILDKITE_TAG =~ "-beta" ]]; then
-      exit 0
-    fi
+  # Triggered by a tag - has to be beta to have any effect
+  if [[ ! $BUILDKITE_TAG =~ "-beta" ]]; then
+    exit 0
+  fi
+elif [ "$BUILDKITE_BRANCH" != "$CHANNEL" ]
+  # Not triggered by tag - has to be on the branch of the corresponding channel to have any effect
+  exit 0
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/kill-all-docker-containers.sh
-
-CHANNEL="${1:?Provide the channel this script is run on (e.g. alpha, beta)}"
-VERSION_OFFSET="${2:?Offset in minor version to build against}"
 
 # Rolling number versioning for unstable channels
 LAST_GIT_TAG=$(git tag --sort=-version:refname | grep -v -e v -e beta | head -n 1)
@@ -28,13 +31,13 @@ echo "Last docker tag: $LAST_DOCKER_TAG"
 NEXT_DOCKER_TAG=""
 
 if [ -z LAST_DOCKER_TAG ]; then
-    NEXT_DOCKER_TAG="$NEXT_VERSION-$CHANNEL-1"
+  NEXT_DOCKER_TAG="$NEXT_VERSION-$CHANNEL-1"
 else
-    IFS=- read version channelStr rollingVersion <<< ${LAST_DOCKER_TAG}
-    NEXT_DOCKER_TAG="$NEXT_VERSION-$CHANNEL-$(($rollingVersion + 1))"
+  IFS=- read version channelStr rollingVersion <<< ${LAST_DOCKER_TAG}
+  NEXT_DOCKER_TAG="$NEXT_VERSION-$CHANNEL-$(($rollingVersion + 1))"
 
-    echo "Rolling version: $rollingVersion"
-    echo "Next docker tag: $NEXT_DOCKER_TAG"
+  echo "Rolling version: $rollingVersion"
+  echo "Next docker tag: $NEXT_DOCKER_TAG"
 fi
 
 # Always release -CHANNEL as well
