4c1eeb61fa51854fb98c2eb26bb9ff9be78d1e91
Divyendu Singh <divyendu.z@gmail.com>
2018-06-19 13:24:50 +0200

    tests: integration - deploy, delete for eu, us

---
+++ cli/packages/prisma-test/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "roots": [
+      "<rootDir>/src"
+    ],
+    "transform": {
+      "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+      "ts",
+      "tsx",
+      "js",
+      "jsx",
+      "json",
+      "node"
+    ],
+  }
---
+++ cli/packages/prisma-test/src/TestHelper.ts
@@ -0,0 +1,45 @@
+import { getTmpDir } from '../../prisma-cli-core/src/test/getTmpDir'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import * as cuid from 'scuid'
+
+export enum REGION {
+  EU = 'eu',
+  US = 'us',
+}
+
+export type MockEnvironment = {
+  region: REGION | undefined
+  cwd?: string | undefined
+  endpoint?: string | undefined
+}
+
+export function setupMockEnvironment(environment: MockEnvironment): MockEnvironment {
+  const { region } = environment
+  const home = getTmpDir()
+  const cwd = environment.cwd ? environment.cwd : path.join(home, cuid())
+  fs.mkdirpSync(cwd)
+  fs.writeFileSync(
+    `${cwd}/datamodel.graphql`,
+    `
+      type User {
+        id: ID! @unique
+        name: String!
+      }
+    `,
+  )
+  const endpoint = `https://${region}1.prisma.sh/public-${cuid()}/default/default`
+  fs.writeFileSync(
+    `${cwd}/prisma.yml`,
+    `
+      endpoint: ${endpoint}
+      datamodel: datamodel.graphql
+    `,
+  )
+
+  return {
+    region,
+    cwd,
+    endpoint,
+  }
+}
