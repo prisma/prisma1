69c7e9f38987bdc3e5fe58cf8c671ae87fdd5569
Marcus Böhm <wif.mboehm@gmx.de>
2018-06-25 14:57:35 +0200

    add additional result classes for mutaction results

--- server/connectors/api-connector/src/main/scala/com/prisma/api/connector/ApiMutactionResult.scala
+++ server/connectors/api-connector/src/main/scala/com/prisma/api/connector/ApiMutactionResult.scala
@@ -8,8 +8,12 @@ sealed trait FurtherNestedMutactionResult extends DatabaseMutactionResult {
   def id: IdGCValue
 }
 
-case class CreateDataItemResult(id: IdGCValue) extends FurtherNestedMutactionResult
-case class UpdateItemResult(id: IdGCValue)     extends FurtherNestedMutactionResult
+case class CreateDataItemResult(id: IdGCValue)                             extends FurtherNestedMutactionResult
+case class UpdateItemResult(id: IdGCValue)                                 extends FurtherNestedMutactionResult
+case class DeleteDataItemResult(id: IdGCValue, previousValues: PrismaNode) extends FurtherNestedMutactionResult
+sealed trait UpsertDataItemResult                                          extends FurtherNestedMutactionResult
+case class UpsertNodeCreated(result: CreateDataItemResult)                 extends UpsertDataItemResult { def id = result.id }
+case class UpsertNodeUpdated(result: UpdateItemResult)                     extends UpsertDataItemResult { def id = result.id }
 
 object UnitDatabaseMutactionResult extends DatabaseMutactionResult
 /**
