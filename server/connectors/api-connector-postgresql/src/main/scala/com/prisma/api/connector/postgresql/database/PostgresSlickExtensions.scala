cc6ee30aa5da9907832c946c97af1002cb43c0d4
Marcus Böhm <wif.mboehm@gmx.de>
2018-06-28 11:02:06 +0200

    remove unused code

--- server/connectors/api-connector-postgresql/src/main/scala/com/prisma/api/connector/postgresql/database/PostgresSlickExtensions.scala
+++ server/connectors/api-connector-postgresql/src/main/scala/com/prisma/api/connector/postgresql/database/PostgresSlickExtensions.scala
@@ -2,7 +2,7 @@ package com.prisma.api.connector.postgresql.database
 
 import com.prisma.api.connector.postgresql.database.JdbcExtensions._
 import com.prisma.gc_values._
-import slick.jdbc.{PositionedParameters, SQLActionBuilder, SetParameter}
+import slick.jdbc.PositionedParameters
 
 object PostgresSlickExtensions {
 
@@ -10,14 +10,6 @@ object PostgresSlickExtensions {
     def setGcValue(value: GCValue): Unit = {
       val npos = pp.pos + 1
       pp.ps.setGcValue(npos, value)
-      pp.pos = npos
-    }
-  }
-
-  implicit object SetGcValueParam extends SetParameter[GCValue] {
-    override def apply(gcValue: GCValue, pp: PositionedParameters): Unit = {
-      val npos = pp.pos + 1
-      pp.ps.setGcValue(npos, gcValue)
       pp.pos = npos
     }
   }
