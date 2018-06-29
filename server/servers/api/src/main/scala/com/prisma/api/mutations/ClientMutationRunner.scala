f31e1dc5245e83df1cb65b8ff2cd653575187e40
Marcus Böhm <wif.mboehm@gmx.de>
2018-06-29 11:49:07 +0200

    bring back publishing of subscription events

--- server/servers/api/src/main/scala/com/prisma/api/mutations/ClientMutationRunner.scala
+++ server/servers/api/src/main/scala/com/prisma/api/mutations/ClientMutationRunner.scala
@@ -1,7 +1,7 @@
 package com.prisma.api.mutations
 
-import com.prisma.api.connector.{DatabaseMutactionExecutor, DatabaseMutactionResult, ExecuteServerSideSubscription}
-import com.prisma.api.mutactions.{DatabaseMutactionVerifier, ServerSideSubscriptions, SideEffectMutactionExecutor}
+import com.prisma.api.connector.DatabaseMutactionExecutor
+import com.prisma.api.mutactions.{DatabaseMutactionVerifier, ServerSideSubscriptions, SideEffectMutactionExecutor, SubscriptionEvents}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -14,14 +14,14 @@ object ClientMutationRunner {
       databaseMutactionVerifier: DatabaseMutactionVerifier
   )(implicit ec: ExecutionContext): Future[T] = {
     for {
-      mutaction               <- clientMutation.prepareMutactions()
-      errors                  = databaseMutactionVerifier.verify(mutaction +: mutaction.allNestedMutactions)
-      _                       = if (errors.nonEmpty) throw errors.head
-      databaseResults         <- databaseMutactionExecutor.executeTransactionally(mutaction)
-      serverSideSubscriptions = ServerSideSubscriptions.extractFromMutactions(clientMutation.project, databaseResults, requestId = "")
-      // fixme: also add PublishSubscriptionEvents
-      _          <- sideEffectMutactionExecutor.execute(serverSideSubscriptions)
-      prismaNode <- clientMutation.getReturnValue(databaseResults)
+      mutaction                 <- clientMutation.prepareMutactions()
+      errors                    = databaseMutactionVerifier.verify(mutaction +: mutaction.allNestedMutactions)
+      _                         = if (errors.nonEmpty) throw errors.head
+      databaseResults           <- databaseMutactionExecutor.executeTransactionally(mutaction)
+      serverSideSubscriptions   = ServerSideSubscriptions.extractFromMutactions(clientMutation.project, databaseResults, requestId = "")
+      publishSubscriptionEvents = SubscriptionEvents.extractFromMutactionResults(clientMutation.project, clientMutation.mutationId, databaseResults)
+      _                         <- sideEffectMutactionExecutor.execute(publishSubscriptionEvents ++ serverSideSubscriptions)
+      prismaNode                <- clientMutation.getReturnValue(databaseResults)
     } yield prismaNode
   }
 }
