ab70e4e56d30bf5d0cf86ab59c9ee9715d17095d
Marcus Böhm <wif.mboehm@gmx.de>
2018-06-29 15:10:52 +0200

    cleanup relay id actions

--- server/connectors/api-connector-jdbc/src/main/scala/com/prisma/api/connector/jdbc/database/RelayIdActions.scala
+++ server/connectors/api-connector-jdbc/src/main/scala/com/prisma/api/connector/jdbc/database/RelayIdActions.scala
@@ -1,7 +1,5 @@
 package com.prisma.api.connector.jdbc.database
 
-import java.sql.{PreparedStatement, Statement}
-
 import com.prisma.gc_values.IdGCValue
 import com.prisma.shared.models.Model
 
@@ -9,21 +7,17 @@ trait RelayIdActions extends BuilderBase {
   import slickDatabase.profile.api._
 
   def createRelayId(model: Model, id: IdGCValue): DBIO[_] = {
-    SimpleDBIO[Boolean] { x =>
-      lazy val queryString: String = {
-        sql
-          .insertInto(relayTable)
-          .columns(relayIdColumn, relayStableIdentifierColumn)
-          .values(placeHolder, placeHolder)
-          .getSQL
+    lazy val query = sql
+      .insertInto(relayTable)
+      .columns(relayIdColumn, relayStableIdentifierColumn)
+      .values(placeHolder, placeHolder)
+
+    insertToDBIO(query)(
+      setParams = pp => {
+        pp.setGcValue(id)
+        pp.setString(model.stableIdentifier)
       }
-
-      val statement: PreparedStatement = x.connection.prepareStatement(queryString, Statement.RETURN_GENERATED_KEYS)
-      statement.setGcValue(1, id)
-      statement.setString(2, model.stableIdentifier)
-
-      statement.execute()
-    }
+    )
   }
 
   def deleteRelayIds(ids: Vector[IdGCValue]): DBIO[Unit] = {
