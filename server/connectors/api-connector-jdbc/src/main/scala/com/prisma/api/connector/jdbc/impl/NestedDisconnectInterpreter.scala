d6a60e3b00c9037e924bfd68055762cee754207b
do4gr <matthias.oertel@gmx.net>
2018-07-29 15:08:12 +0200

    adjust validation, adjust test

--- server/connectors/api-connector-jdbc/src/main/scala/com/prisma/api/connector/jdbc/impl/NestedDisconnectInterpreter.scala
+++ server/connectors/api-connector-jdbc/src/main/scala/com/prisma/api/connector/jdbc/impl/NestedDisconnectInterpreter.scala
@@ -48,7 +48,7 @@ case class NestedDisconnectInterpreter(mutaction: NestedDisconnect)(implicit val
                 case None => throw APIErrors.NodeNotFoundForWhereError(where)
                 case Some(childId) =>
                   for {
-                    _ <- mutationBuilder.ensureThatNodeIsConnected(mutaction.relationField, childId)
+                    _ <- mutationBuilder.ensureThatNodesAreConnected(mutaction.relationField, childId, parentId)
                     _ <- mutationBuilder.deleteRelationRowByChildIdAndParentId(mutaction.relationField, childId, parentId)
                   } yield ()
               }
