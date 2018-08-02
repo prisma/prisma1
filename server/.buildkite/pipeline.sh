048732102aa77121721de962053c3dc884e4fb4a
Marcus Böhm <wif.mboehm@gmx.de>
2018-08-02 12:58:16 +0200

    add testing against MySQL8 to CI

--- server/.buildkite/pipeline.sh
+++ server/.buildkite/pipeline.sh
@@ -12,14 +12,20 @@ fi
 declare -a connectors=(mysql postgres postgres-passive)
 
 # Projects with a locked connector
-static=$(printf "    - label: \":mysql: MySql API connector\"
+static=$(printf "    - label: \":mysql: MySql 5.7 API connector\"
       command: cd server && ./.buildkite/scripts/test.sh api-connector-mysql mysql
 
-    - label: \":mysql: MySql deploy connector\"
+    - label: \":mysql: MySql 5.7 deploy connector\"
       command: cd server && ./.buildkite/scripts/test.sh deploy-connector-mysql mysql
 
     - label: \":scala: integration-tests-mysql\"
       command: cd server && ./.buildkite/scripts/test.sh integration-tests-mysql mysql
+
+    - label: \":mysql: MySql 8 API connector\"
+      command: cd server && ./.buildkite/scripts/test.sh api-connector-mysql mysql8
+
+    - label: \":mysql: MySql 8 deploy connector\"
+      command: cd server && ./.buildkite/scripts/test.sh deploy-connector-mysql mysql8
 
     - label: \":postgres: Postgres API connector\"
       command: cd server && ./.buildkite/scripts/test.sh api-connector-postgres postgres
