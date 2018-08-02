67dc43363b643504d1600ebeb7ea68fb53d60fc9
Marcus Böhm <wif.mboehm@gmx.de>
2018-08-02 10:57:05 +0200

    add retries to flaky DeadlockSpec

--- server/servers/api/src/test/scala/com/prisma/api/mutations/DeadlockSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/DeadlockSpec.scala
@@ -3,13 +3,18 @@ package com.prisma.api.mutations
 import com.prisma.api.ApiSpecBase
 import com.prisma.shared.schema_dsl.SchemaDsl
 import com.prisma.utils.await.AwaitUtils
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{FlatSpec, Matchers, Retries}
 
 import scala.concurrent.Future
 
-class DeadlockSpec extends FlatSpec with Matchers with ApiSpecBase with AwaitUtils {
-
+class DeadlockSpec extends FlatSpec with Matchers with Retries with ApiSpecBase with AwaitUtils {
   import testDependencies.system.dispatcher
+
+  override def withFixture(test: NoArgTest) = {
+    val delay = Span(5, Seconds) // we assume that the process gets overwhelmed sometimes by the concurrent requests. Give it a bit of time to recover before retrying.
+    withRetry(delay) { super.withFixture(test) }
+  }
 
   "creating many items" should "not cause deadlocks" in {
     val project = SchemaDsl.fromBuilder { schema =>
