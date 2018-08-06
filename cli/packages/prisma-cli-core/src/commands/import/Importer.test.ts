5d09d4dcc3002846131e9fc1d48183484c009092
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-06 19:34:57 +1000

    Reproduce error with a test

--- cli/packages/prisma-cli-core/src/commands/import/Importer.test.ts
+++ cli/packages/prisma-cli-core/src/commands/import/Importer.test.ts
@@ -1,36 +1,36 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
-import { Importer } from './Importer';
+import { Importer } from './Importer'
 import { Environment } from 'prisma-yml'
-import { Client, Config, Output } from 'prisma-cli-engine';
-import { getTmpDir } from '../../../../prisma-yml/src/test/getTmpDir';
-import * as cuid from 'scuid'
-
-const tmpDir = getTmpDir()
+import { Client, Config, Output } from 'prisma-cli-engine'
+import { getTmpDir } from '../../../../prisma-yml/src/test/getTmpDir'
 
 function initImporter() {
-    const config = new Config()
-    const output = new Output(config)
-    const env = new Environment(getTmpDir(), output)
-    const client = new Client(config, env, output)
-    const types = `
-    type User {
-      id: ID! @unique
-      name: String!
-    }`
-    return new Importer(tmpDir, types, client, output, config);
+  const config = new Config()
+  const output = new Output(config)
+  const env = new Environment(getTmpDir(), output)
+  const client = new Client(config, env, output)
+  const types = `
+    type Post {
+      title: String
+      description: String
+      state: String
+    }
+  `
+  return new Importer(
+    path.join(__dirname, 'fixtures', 'basic', 'import-missing-ids'),
+    types,
+    client,
+    output,
+    config,
+  )
 }
 
 describe('Importer', () => {
-    const importer = initImporter()
-    const nodesDir = path.join(tmpDir, 'nodes')
-    fs.mkdirpSync(nodesDir)
-    fs.copyFileSync(
-        path.join(__dirname, 'boilerplate', '1.json'),
-        `${nodesDir}/${cuid()}.json`
-    )
+  const importer = initImporter()
 
-    it('throws when ids are missing', async () => {
-        expect(async () => importer.upload('serviceName', 'stage')).toThrow()
-    });
+  it('Should throw an error when ids are missing', async () => {
+    await importer.upload('serviceName', 'stage')
+    expect(importer.out.stdout.output).toMatchSnapshot()
+  })
 })
