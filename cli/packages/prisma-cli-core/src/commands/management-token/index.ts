f67639511777f10d5e4a253dae8a194e19fbabc6
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-08-18 10:07:36 +1000

    Implement management token generation

--- cli/packages/prisma-cli-core/src/commands/management-token/index.ts
+++ cli/packages/prisma-cli-core/src/commands/management-token/index.ts
@@ -1,6 +1,9 @@
 import { Command, flags, Flags } from 'prisma-cli-engine'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import * as dotenv from 'dotenv'
+import * as jwt from 'jsonwebtoken'
 import * as ncp from 'copy-paste'
-
 export default class ManagementToken extends Command {
   static topic = 'management-token'
   static description = 'Create a new management API token'
@@ -15,6 +18,7 @@ export default class ManagementToken extends Command {
     }),
     ['expiry-duration']: flags.number({
       description: 'An expiry duration in hours',
+      defaultValue: 1,
       char: 'd',
     }),
     copy: flags.boolean({
@@ -33,6 +37,38 @@ export default class ManagementToken extends Command {
           action: '*',
         },
       ],
+    }
+    if (!secret && !envFile) {
+      throw new Error('You must specify either --env-file or --secret')
+    } else {
+      if (envFile) {
+        this.loadEnvFile(envFile)
+      }
+      const token = jwt.sign(
+        tokenPayload,
+        secret ? secret : process.env.PRISMA_MANAGEMENT_API_SECRET,
+        { expiresIn: `${expDuration}h`}
+      )
+      if (!copy) {
+        this.out.log(token)
+      } else {
+        ncp.copy(token, () => {
+          this.out.log('Management api token was copied to the clipboard')
+        })
+      }
+    }
+  }
+  loadEnvFile(envFilePath) {
+    if (!fs.pathExistsSync(envFilePath)) {
+      envFilePath = path.join(process.cwd(), envFilePath)
+    }
+    if (!fs.pathExistsSync(envFilePath)) {
+      throw new Error(`--env-file path '${envFilePath}' does not exist`)
+      
+    }
+    dotenv.config({ path: envFilePath })
+    if(!process.env.PRISMA_MANAGEMENT_API_SECRET) {
+      throw new Error('Cannot find variable PRISMA_MANAGEMENT_API_SECRET in environment file')
     }
   }
 }
