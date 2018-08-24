4ee70713075ddd8b0ea539c1d8c66870354a53e5
Divyendu Singh <divyendu.z@gmail.com>
2018-08-24 16:54:14 +0530

    orm: fix Id name

--- prisma-lib/src/codegen/GoGenerator.ts
+++ prisma-lib/src/codegen/GoGenerator.ts
@@ -30,7 +30,7 @@ import * as upperCamelCase from 'uppercamelcase'
 
 const upperCamelCasePatched = (s: string) => {
   const cased = upperCamelCase(s)
-  return cased === 'Id' ? 'ID' : cased
+  return cased.startsWith('Id') === 'Id' ? `ID${cased.slice(2)}` : cased
 }
 
 export class GoGenerator extends Generator {
@@ -90,7 +90,30 @@ export class GoGenerator extends Generator {
         | GraphQLObjectTypeRef
         | GraphQLInputObjectType
         | GraphQLInterfaceType,
-    ): string => ``,
+    ): string => {
+      const fieldMap = type.getFields()
+      return `// ${type.name} docs
+      type ${type.name} struct {
+                ${Object.keys(fieldMap)
+                  .map(key => {
+                    const field = fieldMap[key]
+
+                    // TODO: Ok this can't go to production - hacky - need to find proper field definition and field name with Null + List properties.
+                    // TODO: Add Nullability and array to fieldType later.
+                    const fieldType = field.type
+                      .toString()
+                      .replace('!', '')
+                      .replace('[', '')
+                      .replace(']', '')
+                      .trim()
+
+                    return `${upperCamelCasePatched(field.name)} ${this
+                      .scalarMapping[fieldType] || fieldType}`
+                  })
+                  .join('\n')}
+            }
+        `
+    },
 
     GraphQLScalarType: (type: GraphQLScalarType): string => ``,
 
@@ -265,7 +288,9 @@ ${typeNames
         let type = typeMap[key]
         return this.graphqlRenderers[type.constructor.name]
           ? this.graphqlRenderers[type.constructor.name](type)
-          : null
+          : `// TODO: No GraphQL Renderer for Type ${type.name} of type ${
+              type.constructor.name
+            }`
       })
       .join('\n')}
 
