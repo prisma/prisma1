323ab3a3ce25edb0774176cc4e623194de384fd4
timsuchanek <tim.suchanek@gmail.com>
2018-09-09 13:51:37 +0200

    add formatting

--- prisma-lib/src/codegen/javascript-client.ts
+++ prisma-lib/src/codegen/javascript-client.ts
@@ -1,12 +1,28 @@
 import { TypescriptGenerator, RenderOptions } from './typescript-client'
+import * as prettier from 'prettier'
 
 export class JavascriptGenerator extends TypescriptGenerator {
   constructor(options) {
     super(options)
   }
+  format(code: string, options: prettier.Options = {}) {
+    try {
+      return prettier.format(code, {
+        ...options,
+        parser: 'javascript',
+      })
+    } catch (e) {
+      console.log(
+        `There is a syntax error in generated code, unformatted code printed, error: ${JSON.stringify(
+          e,
+        )}`,
+      )
+      return code
+    }
+  }
   renderJavascript(options?: RenderOptions) {
     const args = this.renderPrismaClassArgs(options)
-    return `\
+    return this.format(`\
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var prisma_lib_1 = require("prisma-lib");
@@ -14,6 +30,6 @@ var typeDefs = require("./graphql").typeDefs
 
 exports.Prisma = prisma_lib_1.makePrismaBindingClass(${args});
 exports.prisma = new exports.Prisma();
-`
+`)
   }
 }
