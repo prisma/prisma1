d2156ddb33d8818df9b21fc77fce37a550314792
Divyendu Singh <divyendu.z@gmail.com>
2018-09-18 18:18:57 +0200

    fix(client): re-use RenderOptions

--- client/src/codegen/typescript-definitions.ts
+++ client/src/codegen/typescript-definitions.ts
@@ -1,9 +1,4 @@
-import { TypescriptGenerator } from './typescript-client'
-
-export interface RenderOptions {
-  endpoint?: string
-  secret?: string
-}
+import { TypescriptGenerator, RenderOptions } from './typescript-client'
 
 export class TypescriptDefinitionsGenerator extends TypescriptGenerator {
   renderExports(options?: RenderOptions) {
