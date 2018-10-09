790c51380f0159204f23a2f22e34a1f8a324e740
do4gr <matthias.oertel@gmx.net>
2018-10-09 17:57:19 +0200

    test updateMany, remove println

--- server/servers/api/src/test/scala/com/prisma/api/mutations/nonEmbedded/VeryManyMutationsSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/nonEmbedded/VeryManyMutationsSpec.scala
@@ -26,15 +26,21 @@ class VeryManyMutationsSpec extends FlatSpec with Matchers with ApiSpecBase {
   override protected def beforeAll(): Unit = {
     super.beforeAll()
     database.setup(project)
-    createVeryManyTops
   }
+
+  //Postgres has a limit of 32678 parameters to a query
 
   "The delete many Mutation" should "delete the items matching the where clause" in {
+    createVeryManyTops
+
+    val update = server.query("""mutation {updateManyMiddles(where: { int_gt: 100 } data:{int: 500}){count}}""", project)
+    update.pathAsLong("data.updateManyMiddles.count") should equal(36291)
 
     val result = server.query("""mutation {deleteManyMiddles(where: { int_gt: 100 }){count}}""", project)
-
     result.pathAsLong("data.deleteManyMiddles.count") should equal(36291)
   }
+
+  "A cascading delete" should "not hit the parameter limit" in {}
 
   def createVeryManyTops = for (int <- 1 to 1000) {
     createTop(int)
