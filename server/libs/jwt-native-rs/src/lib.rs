24d784a2c58672c1da8c02ed7a05c278fdd2201b
Dominic Petrick <dompetrick@gmail.com>
2018-10-12 18:54:45 +0200

    More experiments with byte protocol. Implemented verify.

--- server/libs/jwt-native-rs/src/lib.rs
+++ server/libs/jwt-native-rs/src/lib.rs
@@ -1,14 +1,15 @@
+#![allow(unused_variables, dead_code)]
+
 #[macro_use]
 extern crate serde_derive;
 extern crate jsonwebtoken as jwt;
 extern crate chrono;
 
-//use std::ffi::{CStr, CString};
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::mem;
 
-use jwt::{encode, Header};
+use jwt::{decode, encode, Header, Validation};
 use chrono::prelude::*;
 
 #[repr(C)]
@@ -16,28 +17,32 @@ use chrono::prelude::*;
 pub struct ProtocolBuffer {
     success: u8,
     len: usize,
-    data: *const u8,
+    data: *const c_char,
 }
 
-#[derive(Serialize)]
+impl Drop for ProtocolBuffer {
+    fn drop(&mut self) {
+        println!("[Rust] Dropping ProtocolBuffer");
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Claims {
     iat: i64, // Issued at
     nbf: i64, // Not before
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     exp: Option<i64>, // Expiration
 }
 
 #[no_mangle]
 pub extern "C" fn create_token(secret: *const c_char, expiration_in_seconds: i64) -> *mut ProtocolBuffer {
-    println!("[Rust] Received: {:?}, {:?}", secret, expiration_in_seconds);
-
     let secret_str = to_string(secret);
     let expiration = if expiration_in_seconds < 0 {
         None
     } else {
         Some(expiration_in_seconds)
     };
-
-    println!("[Rust] Unpacked: {}, {:?}", secret_str, expiration);
 
     let now = Utc::now().timestamp();
     let claims = Claims {
@@ -48,29 +53,89 @@ pub extern "C" fn create_token(secret: *const c_char, expiration_in_seconds: i64
 
     // String is always heap allocated, no need to box it
     let token = encode(&Header::default(), &claims, secret_str.as_ref()).unwrap();
-    let len = token.len();
-    let ptr = token.as_ptr();
 
-    mem::forget(token);
-    let res = Box::into_raw(Box::new(ProtocolBuffer { success: 1, len: len, data: ptr }));
-    res
+    Box::into_raw(write_string_buffer(true, token))
 }
 
-// todo free memory
+#[no_mangle]
+pub extern "C" fn verify_token(token: *const c_char, secrets: *const c_char, num_secrets: i64) -> *mut ProtocolBuffer {
+    let parsed_token = to_string(token);
+    let parsed_secrets = to_string_vector(secrets, num_secrets);
 
-/// Convert a native string to a Rust string
+    let mut last_error: String = String::from("");
+
+    for secret in parsed_secrets {
+        let t = decode::<Claims>(&parsed_token, secret.as_ref(), &Validation { validate_exp: false, ..Validation::default()});
+        match t {
+            Ok(x) => {
+                if is_expired(x.claims.exp) {
+                    return Box::into_raw(write_string_buffer(false, String::from("token is expired")))
+                }
+                return Box::into_raw(write_empty_buffer(true))
+            },
+            Err(e) => last_error.push_str(&format!("{} ", e)),
+        }
+    }
+
+    println!("[Rust] Failed");
+    Box::into_raw(write_string_buffer(false, last_error))
+}
+
+#[no_mangle]
+pub extern "C" fn destroy_buffer(buffer: *mut ProtocolBuffer) {
+    unsafe { Box::from_raw(buffer) };
+}
+
+fn is_expired(exp_claim: Option<i64>) -> bool {
+    match exp_claim {
+        Some(exp) => {
+            println!("{}", exp);
+            exp >= Utc::now().timestamp()
+        },
+        None => false,
+    }
+}
+
+// Rust -> JVM
+
+fn write_empty_buffer(success: bool) -> Box<ProtocolBuffer> {
+    Box::new(ProtocolBuffer { success: success as u8, len: 0, data: std::ptr::null() })
+}
+
+fn write_string_buffer(success: bool, data: String) -> Box<ProtocolBuffer> {
+    let len = data.len();
+    let ptr = string_to_ptr(data);
+
+    Box::new(ProtocolBuffer { success: success as u8, len: len, data: ptr })
+}
+
+fn string_to_ptr(s: String) -> *const c_char {
+    let cs = CString::new(s.as_bytes()).unwrap();
+    let ptr = cs.as_ptr();
+
+    mem::forget(cs);
+    ptr
+}
+
+// JVM -> Rust
+
+// Todo verify that this does 0 copy
 fn to_string(pointer: *const c_char) -> String {
-    let slice = unsafe { CStr::from_ptr(pointer).to_bytes() };
-    std::str::from_utf8(slice).unwrap().to_string()
+    unsafe {
+        String::from(std::str::from_utf8_unchecked(CStr::from_ptr(pointer).to_bytes()))
+    }
 }
 
-// Convert a Rust string to a native string
-//fn to_ptr(string: String) -> *const c_char {
-//    let cs = CString::new(string.as_bytes()).unwrap();
-//    let ptr = cs.as_ptr();
-//
-//    // Tell Rust not to clean up the string while we still have a pointer to it.
-//    // Otherwise, we'll get a segfault.
-//    mem::forget(cs);
-//    ptr
-//}
+// Todo verify that this does 0 copy
+fn to_string_vector(raw: *const c_char, num_elements: i64) -> Vec<String> {
+    let mut vec = Vec::new();
+    unsafe {
+        for offset in 0..num_elements {
+            let ptr = { raw.offset(offset as isize) };
+            vec.push(to_string(ptr))
+        }
+    }
+
+    vec
+}
+
--- server/libs/jwt-native/src/main/scala/com/prisma/jwt/Main.scala
+++ server/libs/jwt-native/src/main/scala/com/prisma/jwt/Main.scala
@@ -1,20 +1,69 @@
-import com.prisma.jwt.JnaRustBridge
-import com.sun.jna.Native
-import com.sun.jna.ptr.IntByReference
+package com.prisma.jwt
+
+import com.sun.jna.{Memory, Native, Pointer}
+import org.joda.time.DateTime
 
 object Main extends App {
+  def copyToNativeStringArray(elements: Vector[String]): Pointer = {
+    var offset        = 0
+    val memorySize    = elements.foldLeft(0)((prev, next) => prev + next.getBytes().size + 1) // +1 for \0 termination
+    val memoryPointer = new Memory(memorySize * Native.getNativeSize(Character.TYPE))
+
+    for (s <- elements) {
+      memoryPointer.setString(offset, s)
+
+      // null-terminator
+      memoryPointer.setMemory(offset + s.length, 1, 0.toByte)
+      offset += s.length + 1
+    }
+
+    memoryPointer // todo when is this released / GC'ed?
+  }
+
   val currentDir = System.getProperty("user.dir")
 
   System.setProperty("jna.debug_load.jna", "true")
   System.setProperty("jna.debug_load", "true")
 
+  println("---------------------- Creation ----------------------")
+  val secrets = Vector("some_secret", "some_other_secret", "another_one")
   val library = Native.loadLibrary("jwt_native", classOf[JnaRustBridge])
-
-  val withExp = library.create_token("some_secret", 2000)
+  val withExp = library.create_token("some_secret", DateTime.now().plusHours(1).getMillis / 1000)
   val noExp   = library.create_token("some_secret", -1)
 
   println(withExp)
   println(noExp)
 
-  println(withExp.len)
+  // Possibility #1
+  println("1: " + withExp.data.getString(0))
+  println("2: " + noExp.data.getString(0))
+
+  // Possibility #2
+//  val bytes = withExp.data.getByteArray(0, withExp.len.intValue())
+
+  val tokenWithExp    = withExp.data.getString(0)
+  val tokenWithoutExp = noExp.data.getString(0)
+
+  library.destroy_buffer(withExp)
+  library.destroy_buffer(noExp)
+
+  println("---------------------- Validation ----------------------")
+
+  val mem           = copyToNativeStringArray(secrets)
+  val verify1Result = library.verify_token(tokenWithExp, mem, secrets.length)
+
+  println(verify1Result)
+  assert(verify1Result.success)
+
+  library.destroy_buffer(verify1Result)
+
+  val verify2Result = library.verify_token(tokenWithoutExp, mem, secrets.length)
+
+  println(verify2Result)
+  if (!verify2Result.success) {
+    println(verify2Result.data.getString(0))
+  }
+  assert(verify2Result.success)
+
+  library.destroy_buffer(verify2Result)
 }
