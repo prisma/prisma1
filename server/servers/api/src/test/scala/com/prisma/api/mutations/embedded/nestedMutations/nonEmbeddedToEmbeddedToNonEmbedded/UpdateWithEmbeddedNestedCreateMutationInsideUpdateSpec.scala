816d9c0aa5d7f0d46e59d28db5b1b316f6bb4d0b
do4gr <matthias.oertel@gmx.net>
2018-10-19 17:54:29 +0200

    another tests

--- server/servers/api/src/test/scala/com/prisma/api/mutations/embedded/nestedMutations/nonEmbeddedToEmbeddedToNonEmbedded/UpdateWithEmbeddedNestedCreateMutationInsideUpdateSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/embedded/nestedMutations/nonEmbeddedToEmbeddedToNonEmbedded/UpdateWithEmbeddedNestedCreateMutationInsideUpdateSpec.scala
@@ -189,4 +189,63 @@ class UpdateWithEmbeddedNestedCreateMutationInsideUpdateSpec extends FlatSpec wi
     update.toString should be("""{"data":{"updateParent":{"p":"p1","children":[{"c":"c1","friendOpt":{"f":"f1"}}]}}}""")
   }
 
+  "a FriendsOpt relation" should "be possible" in {
+
+    val project = SchemaDsl.fromString() { embedddedToJoinFriendsOpt }
+
+    database.setup(project)
+
+    val create = server
+      .query(
+        """mutation {
+          |  createParent(data: {
+          |    p: "p1"
+          |    children: {create:{
+          |       c: "c1"
+          |    }}
+          |  }){
+          |    p
+          |    children{
+          |       c
+          |       friendsOpt{
+          |         f
+          |       }
+          |
+          |    }
+          |  }
+          |}""",
+        project
+      )
+
+    create.toString should be("""{"data":{"createParent":{"p":"p1","children":[{"c":"c1","friendsOpt":[]}]}}}""")
+
+    val update = server
+      .query(
+        """mutation {
+          |  updateParent(
+          |  where:{p:"p1"}
+          |  data: {
+          |    children: {update:{
+          |       where:{c: "c1"}
+          |       data:{
+          |       friendsOpt:{create:[{f: "f1"}, {f: "f2"}]}
+          |       }
+          |    }}
+          |  }){
+          |    p
+          |    children{
+          |       c
+          |       friendsOpt{
+          |         f
+          |       }
+          |
+          |    }
+          |  }
+          |}""",
+        project
+      )
+
+    update.toString should be("""{"data":{"updateParent":{"p":"p1","children":[{"c":"c1","friendsOpt":[{"f":"f1"},{"f":"f2"}]}]}}}""")
+  }
+
 }
