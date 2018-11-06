b372979d32924d6c96223bee7eda4fb9f160fba7
timsuchanek <tim.suchanek@gmail.com>
2018-11-06 14:51:27 +0100

    add test for nested embedded type

--- client/src/Client.test.ts
+++ client/src/Client.test.ts
@@ -90,3 +90,56 @@ test('embedded type', t => {
 
   t.snapshot(print(document))
 })
+
+test('nested mbedded type', t => {
+  const typeDefs = `
+    type Query {
+      user: User
+    }
+
+    type User {
+      id: ID!
+      posts: [Post!]!
+    }
+
+    type Post {
+      content: String!
+      meta: PostMeta
+    }
+
+    type PostMeta {
+      meta: String!
+    }
+  `
+
+  const models: Model[] = [
+    {
+      embedded: false,
+      name: 'User',
+    },
+    {
+      embedded: true,
+      name: 'Post',
+    },
+    {
+      embedded: true,
+      name: 'PostMeta',
+    },
+  ]
+
+  const endpoint = 'http://localhost;4466'
+
+  const client: any = new Client({
+    typeDefs,
+    endpoint,
+    models,
+  })
+
+  client.user()
+
+  const document = client.getDocumentForInstructions(
+    Object.keys(client.currentInstructions)[0],
+  )
+
+  t.snapshot(print(document))
+})
