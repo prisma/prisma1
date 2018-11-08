1b3f738b76c367018e8a37b067d492a3bac39b10
do4gr <matthias.oertel@gmx.net>
2018-11-08 15:04:06 +0100

    rename legacy schemainferrerembeddedspec since legacy does not have embeddeds

--- server/servers/deploy/src/test/scala/com/prisma/deploy/migration/inference/SchemaInfererEmbeddedSpec.scala
+++ server/servers/deploy/src/test/scala/com/prisma/deploy/migration/inference/SchemaInfererEmbeddedSpec.scala
@@ -1,14 +1,14 @@
 package com.prisma.deploy.migration.inference
 
 import com.prisma.deploy.connector.InferredTables
-import com.prisma.deploy.migration.validation.LegacyDataModelValidator
+import com.prisma.deploy.migration.validation.{DataModelValidatorImpl, LegacyDataModelValidator}
 import com.prisma.deploy.specutils.DeploySpecBase
 import com.prisma.shared.models.ApiConnectorCapability.LegacyDataModelCapability
 import com.prisma.shared.models.{ConnectorCapability, OnDelete, Schema}
 import com.prisma.shared.schema_dsl.TestProject
 import org.scalatest.{Matchers, WordSpec}
 
-class LegacySchemaInfererEmbeddedSpec extends WordSpec with Matchers with DeploySpecBase {
+class SchemaInfererEmbeddedSpec extends WordSpec with Matchers with DeploySpecBase {
   val emptyProject = TestProject.empty
 
   "Inferring embedded typeDirectives" should {
@@ -44,17 +44,8 @@ class LegacySchemaInfererEmbeddedSpec extends WordSpec with Matchers with Deploy
 //  self relations between embedded types
 //  embedded types on connector without embedded types capability
 
-  def infer(schema: Schema, types: String, mapping: SchemaMapping = SchemaMapping.empty): Schema = {
-    val actualCapabilities = Set[ConnectorCapability](LegacyDataModelCapability)
-    val validator = LegacyDataModelValidator(
-      types,
-      LegacyDataModelValidator.directiveRequirements,
-      deployConnector.fieldRequirements,
-      actualCapabilities
-    )
-
-    val prismaSdl = validator.generateSDL
-
-    SchemaInferrer(actualCapabilities).infer(schema, SchemaMapping.empty, prismaSdl, InferredTables.empty)
+  def infer(schema: Schema, types: String, mapping: SchemaMapping = SchemaMapping.empty, capabilities: Set[ConnectorCapability]): Schema = {
+    val prismaSdl = DataModelValidatorImpl.validate(types, deployConnector.fieldRequirements, capabilities).get
+    SchemaInferrer(capabilities).infer(schema, mapping, prismaSdl, InferredTables.empty)
   }
 }
