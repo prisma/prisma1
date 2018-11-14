dafd07bdb86ffc83cdc2c02a59174af75ae5a588
Marcus Böhm <wif.mboehm@gmx.de>
2018-11-14 14:03:54 +0100

    use sangria-server also in PrismaProdMain

--- server/images/prisma-local/src/main/scala/com/prisma/local/PrismaLocalMain.scala
+++ server/images/prisma-local/src/main/scala/com/prisma/local/PrismaLocalMain.scala
@@ -2,13 +2,8 @@ package com.prisma.local
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import com.prisma.akkautil.http.ServerExecutor
-import com.prisma.api.server.ApiServer
-import com.prisma.deploy.server.ManagementServer
 import com.prisma.image.SangriaHandlerImpl
-import com.prisma.sangria_server.{AkkaHttpSangriaServer, BlazeSangriaServer}
-import com.prisma.websocket.WebsocketServer
-import com.prisma.workers.WorkerServer
+import com.prisma.sangria_server.AkkaHttpSangriaServer
 
 object PrismaLocalMain extends App {
   implicit val system       = ActorSystem("single-server")
@@ -20,19 +15,9 @@ object PrismaLocalMain extends App {
 
   Version.check()
 
-  // FIXME: still need to start the worker server
-  val sangriaHandler = SangriaHandlerImpl()
+  val sangriaHandler = SangriaHandlerImpl(managementApiEnabled = true)
   val executor       = AkkaHttpSangriaServer
 //  val executor = BlazeSangriaServer
   val server = executor.create(handler = sangriaHandler, port = dependencies.config.port.getOrElse(4466), requestPrefix = sys.env.getOrElse("ENV", "local"))
   server.startBlocking()
-
-//  ServerExecutor(
-//    port = dependencies.config.port.getOrElse(4466),
-//    ManagementServer("management"),
-//    ManagementServer("cluster"), // Deprecated, will be removed soon
-//    WebsocketServer(dependencies),
-//    ApiServer(dependencies.apiSchemaBuilder),
-//    WorkerServer(dependencies)
-//  ).startBlocking()
 }
