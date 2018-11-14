c1c9b5481d21bd823faa9f80f65d1a82cfddefb5
do4gr <matthias.oertel@gmx.net>
2018-11-14 17:22:13 +0100

    enable ssl setting, enable mongo+srv connection string for mongo atlas

--- server/connectors/api-connector-mongo/src/main/scala/com/prisma/api/connector/mongo/MongoDatabasesFactory.scala
+++ server/connectors/api-connector-mongo/src/main/scala/com/prisma/api/connector/mongo/MongoDatabasesFactory.scala
@@ -5,7 +5,15 @@ import org.mongodb.scala._
 object MongoDatabasesFactory {
 
   def initialize(config: DatabaseConfig) = {
-    val uri: String = s"mongodb://${config.user}:${config.password.getOrElse("")}@${config.host}:${config.port}/?authSource=admin"
+    val uri: String = (config.database, config.ssl) match {
+      case (None, false)    => s"mongodb://${config.user}:${config.password.getOrElse("")}@${config.host}:${config.port}/?authSource=admin"
+      case (None, true)     => s"mongodb://${config.user}:${config.password.getOrElse("")}@${config.host}:${config.port}/?authSource=admin&ssl=true"
+      case (Some(db), true) => s"mongodb+srv://${config.user}:${config.password.getOrElse("")}@${config.host}/$db"
+      case (_, _)           => sys.error("Database provided, but ssl set to true.")
+    }
+
+    System.setProperty("org.mongodb.async.type", "netty")
+
     println(s"mongoUri: $uri")
 
     MongoClient(uri)
--- server/connectors/deploy-connector-mongo/src/main/scala/com/prisma/deploy/connector/mongo/MongoInternalDatabaseDefs.scala
+++ server/connectors/deploy-connector-mongo/src/main/scala/com/prisma/deploy/connector/mongo/MongoInternalDatabaseDefs.scala
@@ -4,6 +4,14 @@ import com.prisma.config.DatabaseConfig
 import org.mongodb.scala.MongoClient
 
 case class MongoInternalDatabaseDefs(dbConfig: DatabaseConfig) {
-  val uri: String = s"mongodb://${dbConfig.user}:${dbConfig.password.getOrElse("")}@${dbConfig.host}:${dbConfig.port}/?authSource=admin"
-  val client      = MongoClient(uri)
+  val uri: String = (dbConfig.database, dbConfig.ssl) match {
+    case (None, false)    => s"mongodb://${dbConfig.user}:${dbConfig.password.getOrElse("")}@${dbConfig.host}:${dbConfig.port}/?authSource=admin"
+    case (None, true)     => s"mongodb://${dbConfig.user}:${dbConfig.password.getOrElse("")}@${dbConfig.host}:${dbConfig.port}/?authSource=admin&ssl=true"
+    case (Some(db), true) => s"mongodb+srv://${dbConfig.user}:${dbConfig.password.getOrElse("")}@${dbConfig.host}/$db"
+    case (_, _)           => sys.error("Database provided, but ssl set to true.")
+  }
+
+  System.setProperty("org.mongodb.async.type", "netty")
+
+  val client = MongoClient(uri)
 }
