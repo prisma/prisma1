217800b8052c3d5107884af8aad8595ac31c9b13
Marcus Böhm <wif.mboehm@gmx.de>
2018-11-22 16:07:42 +0100

    WIP

---
+++ server/connectors/deploy-connector-jdbc/src/main/scala/com/prisma/deploy/connector/jdbc/DatabaseInspectorImpl.scala
@@ -0,0 +1,36 @@
+package com.prisma.deploy.connector.jdbc
+
+import com.prisma.deploy.connector._
+import slick.dbio.DBIO
+import slick.jdbc.JdbcProfile
+import slick.jdbc.meta.MTable
+
+import scala.concurrent.{ExecutionContext, Future}
+
+case class DatabaseInspectorImpl(db: JdbcProfile#Backend#Database)(implicit ec: ExecutionContext) extends DatabaseInspector {
+
+  override def inspect(schema: String): Future[Tables] = db.run(action(schema))
+
+  def action(schema: String): DBIO[Tables] = {
+    for {
+      // the line below does not work perfectly on postgres. E.g. it will return tables for schemas "passive_test" and "passive$test" when param is "passive_test"
+      // we therefore have one additional filter step
+      potentialTables <- MTable.getTables(cat = None, schemaPattern = Some(schema), namePattern = None, types = None)
+      mTables         = potentialTables.filter(table => table.name.schema.contains(schema))
+      tables          <- DBIO.sequence(mTables.map(mTableToModel))
+    } yield {
+      Tables(tables)
+    }
+  }
+
+  def mTableToModel(mTable: MTable): DBIO[Table] = {
+    for {
+      mColumns <- mTable.getColumns
+//      importedKeys <- mTable.getImportedKeys
+    } yield {
+      val columns = mColumns.map(mc => Column(mc.name, mc.typeName, foreignKey = None))
+      val indexes = Vector.empty[Index]
+      Table(mTable.name.name, columns, indexes)
+    }
+  }
+}
--- server/servers/deploy/src/test/scala/com/prisma/deploy/migration/MigrationsSpec.scala
+++ server/servers/deploy/src/test/scala/com/prisma/deploy/migration/MigrationsSpec.scala
@@ -1,7 +1,11 @@
 package com.prisma.deploy.migration
 
-import com.prisma.deploy.connector.Tables
+import com.prisma.deploy.connector.{EmptyDatabaseIntrospectionInferrer, FieldRequirementsInterface, Tables}
+import com.prisma.deploy.connector.postgres.database.DatabaseIntrospectionInferrerImpl
+import com.prisma.deploy.migration.inference.{MigrationStepsInferrer, SchemaInferrer}
+import com.prisma.deploy.schema.mutations.{DeployMutation, DeployMutationInput, MutationError, MutationSuccess}
 import com.prisma.deploy.specutils.DeploySpecBase
+import com.prisma.shared.models.{ConnectorCapabilities, Project, Schema}
 import com.prisma.shared.schema_dsl.SchemaDsl
 import org.scalatest.{Matchers, WordSpecLike}
 
@@ -9,21 +13,24 @@ class MigrationsSpec extends WordSpecLike with Matchers with DeploySpecBase {
 
   val name      = this.getClass.getSimpleName
   val stage     = "default"
-  val serviceId = s"$name@$stage"
+  val serviceId = testDependencies.projectIdEncoder.toEncodedString(name, stage)
   val initialDataModel =
     """
       |type A {
       |  id: ID! @id
       |}
     """.stripMargin
-  val inspector = deployConnector.testFacilities.inspector
+  val inspector        = deployConnector.testFacilities.inspector
+  var project: Project = Project(id = serviceId, schema = Schema.empty)
+
+  import system.dispatcher
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
-    val (project, _) = setupProject(initialDataModel)
+    setup()
   }
 
-  "adding a scalar field should work" ignore {
+  "adding a scalar field should work" in {
     val dataModel =
       """
         |type A {
@@ -32,11 +39,59 @@ class MigrationsSpec extends WordSpecLike with Matchers with DeploySpecBase {
         |}
       """.stripMargin
 
-    server.deploySchema(serviceId, dataModel)
+    deploy(dataModel, ConnectorCapabilities.empty)
 
     val result = inspect
     val column = result.table_!("A").column_!("field")
     column.tpe should be("text")
+  }
+
+  def setup() = {
+    val idAsString = testDependencies.projectIdEncoder.toEncodedString(name, stage)
+    deployConnector.deleteProjectDatabase(idAsString).await()
+    server.addProject(name, stage)
+    deploy(initialDataModel, ConnectorCapabilities.empty)
+    project = testDependencies.projectPersistence.load(serviceId).await.get
+  }
+
+  def deploy(dataModel: String, capabilities: ConnectorCapabilities): Unit = {
+    val input = DeployMutationInput(
+      clientMutationId = None,
+      name = name,
+      stage = stage,
+      types = dataModel,
+      dryRun = None,
+      force = None,
+      secrets = Vector.empty,
+      functions = Vector.empty
+    )
+    val mutation = DeployMutation(
+      args = input,
+      project = project,
+      schemaInferrer = SchemaInferrer(capabilities),
+      migrationStepsInferrer = MigrationStepsInferrer(),
+      schemaMapper = SchemaMapper,
+      migrationPersistence = testDependencies.migrationPersistence,
+      projectPersistence = testDependencies.projectPersistence,
+      migrator = testDependencies.migrator,
+      functionValidator = testDependencies.functionValidator,
+      invalidationPublisher = testDependencies.invalidationPublisher,
+      capabilities = capabilities,
+      clientDbQueries = deployConnector.clientDBQueries(project),
+      databaseIntrospectionInferrer = EmptyDatabaseIntrospectionInferrer,
+      fieldRequirements = FieldRequirementsInterface.empty,
+      isActive = true
+    )
+
+    val result = mutation.execute.await
+    result match {
+      case MutationSuccess(result) =>
+        if (result.errors.nonEmpty) {
+          sys.error(s"Deploy returned unexpected errors: ${result.errors}")
+        }
+      case MutationError =>
+        sys.error("Deploy returned an unexpected error")
+    }
   }
 
   def inspect: Tables = {
