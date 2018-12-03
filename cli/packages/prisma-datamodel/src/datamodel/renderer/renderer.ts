631e7be78fb631f0ba3608d418eba309e4bf647c
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2018-12-03 18:37:44 +0100

    Fixed @id not being rendered by parser.

--- cli/packages/prisma-datamodel/src/datamodel/renderer/renderer.ts
+++ cli/packages/prisma-datamodel/src/datamodel/renderer/renderer.ts
@@ -74,6 +74,7 @@ export default class Renderer {
     if(field.defaultValue !== null) { fieldDirectives.push({ name: DirectiveKeys.default, arguments: { value: this.renderValue(field.type, field.defaultValue) }}) }
     if(field.isUnique) { fieldDirectives.push({ name: DirectiveKeys.isUnique, arguments: {} }) }
     if(field.relationName !== null) { fieldDirectives.push({ name: DirectiveKeys.relation, arguments: { name: field.relationName } }) }
+    if(field.isId) { fieldDirectives.push({ name: DirectiveKeys.isId, arguments: { } }) } 
 
     const renderedDirectives = this.renderDirectives(fieldDirectives)
     
