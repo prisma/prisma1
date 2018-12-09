c855d6333743e326758c3d9dbbf6723daffb5ce6
Harshit Pant <pantharshit00@gmail.com>
2018-12-09 18:17:38 +0530

    fix(client): double model output in js generator

--- client/src/codegen/javascript-client.ts
+++ client/src/codegen/javascript-client.ts
@@ -22,7 +22,6 @@ var typeDefs = require("./prisma-schema").typeDefs
 ${this.renderModels()}
 exports.Prisma = prisma_lib_1.makePrismaClientClass(${args});
 exports.prisma = new exports.Prisma();
-${this.renderModels()}
 `)
   }
   renderModels() {
