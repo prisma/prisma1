8295404d0629e4ec2619c68893dc78b2ba451470
Dominic Petrick <dompetrick@gmail.com>
2018-12-10 11:51:30 +0100

    Fix native build script.

--- server/.buildkite/scripts/native-image.sh
+++ server/.buildkite/scripts/native-image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-docker run -e "BRANCH=$BUILDKITE_BRANCH" -e "COMMIT_SHA=$BUILDKITE_COMMIT" -e "CLUSTER_VERSION=$DOCKER_TAG" -v $(pwd):/root/build -w /root/build/server -v ~/.ivy2:/root/.ivy2 -v ~/.coursier:/root/.coursier -v /var/run/docker.sock:/var/run/docker.sock prismagraphql/build-image sbt "project prisma-native" prisma-native-image:packageBin
-
+docker run -e "BRANCH=$BUILDKITE_BRANCH" -e "COMMIT_SHA=$BUILDKITE_COMMIT" -e "CLUSTER_VERSION=$DOCKER_TAG" -v $(pwd):/root/build -w /root/build -v ~/.ivy2:/root/.ivy2 -v ~/.coursier:/root/.coursier -v /var/run/docker.sock:/var/run/docker.sock prismagraphql/build-image sbt "project prisma-native" prisma-native-image:packageBin
 
 ## todo: Build & push image with binary inside
