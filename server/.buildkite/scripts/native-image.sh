7a508538d786565986a492dd2d4359f811bfe22d
Dominic Petrick <dompetrick@gmail.com>
2018-12-11 15:07:39 +0100

    Upload artifacts to S3

--- server/.buildkite/scripts/native-image.sh
+++ server/.buildkite/scripts/native-image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## Note: Execute in context of server sub folder of git repo/
+
 set -e
 
 docker run -e "BRANCH=$BUILDKITE_BRANCH" -e "COMMIT_SHA=$BUILDKITE_COMMIT" -e "CLUSTER_VERSION=$DOCKER_TAG" \
@@ -11,4 +13,9 @@ docker run -e "BRANCH=$BUILDKITE_BRANCH" -e "COMMIT_SHA=$BUILDKITE_COMMIT" -e "C
   prismagraphql/build-image:debian sbt "project prisma-native" prisma-native-image:packageBin
 #  -v /.cargo/:~/root/.cargo \
 
+export BUILDKITE_ARTIFACT_UPLOAD_DESTINATION="s3://$ARTIFACT_BUCKET/$BUILDKITE_JOB_ID"
+buildkite-agent artifact upload images/prisma-native/target/prisma-native-image/prisma-native
+
 ## todo: Build & push image with binary inside
+
+## todo: Upload to github release on tag
