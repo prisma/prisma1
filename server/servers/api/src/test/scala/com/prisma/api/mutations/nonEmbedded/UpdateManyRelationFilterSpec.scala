113d4dee4ac5db4552a431f8c3d2856e5dcdde37
do4gr <matthias.oertel@gmx.net>
2018-12-12 12:54:13 +0100

    enable more filter based tests

--- server/servers/api/src/test/scala/com/prisma/api/mutations/nonEmbedded/UpdateManyRelationFilterSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/nonEmbedded/UpdateManyRelationFilterSpec.scala
@@ -1,13 +1,13 @@
 package com.prisma.api.mutations.nonEmbedded
 
 import com.prisma.api.ApiSpecBase
-import com.prisma.shared.models.ConnectorCapability.{JoinRelationLinksCapability, JoinRelationsFilterCapability}
+import com.prisma.shared.models.ConnectorCapability.JoinRelationLinksCapability
 import com.prisma.shared.models.Project
 import com.prisma.shared.schema_dsl.SchemaDsl
 import org.scalatest.{FlatSpec, Matchers}
 
 class UpdateManyRelationFilterSpec extends FlatSpec with Matchers with ApiSpecBase {
-  override def runOnlyForCapabilities = Set(JoinRelationLinksCapability, JoinRelationsFilterCapability)
+  override def runOnlyForCapabilities = Set(JoinRelationLinksCapability)
 
   val schema =
     """type Top{
