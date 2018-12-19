71956aad2951a50807289c1553f7f546868817cd
Marcus Böhm <wif.mboehm@gmx.de>
2018-12-19 16:32:29 +0100

    expose the datamodel in the management schema

--- server/servers/deploy/src/main/scala/com/prisma/deploy/schema/types/ProjectType.scala
+++ server/servers/deploy/src/main/scala/com/prisma/deploy/schema/types/ProjectType.scala
@@ -1,18 +1,28 @@
 package com.prisma.deploy.schema.types
 
+import com.prisma.deploy.connector.persistence.MigrationPersistence
 import com.prisma.deploy.schema.SystemUserContext
 import com.prisma.shared.models
-import com.prisma.shared.models.ProjectIdEncoder
+import com.prisma.shared.models.{MigrationId, ProjectIdEncoder}
 import sangria.schema._
+
+import scala.concurrent.ExecutionContext
 
 object ProjectType {
 
-  def Type(encoder: ProjectIdEncoder): ObjectType[SystemUserContext, models.Project] = ObjectType(
+  def Type(
+      encoder: ProjectIdEncoder,
+      migrationPersistence: MigrationPersistence
+  )(implicit ec: ExecutionContext): ObjectType[SystemUserContext, models.Project] = ObjectType(
     "Project",
     "This is a project",
     fields[SystemUserContext, models.Project](
       Field("name", StringType, resolve = x => encoder.fromEncodedString(x.value.id).name),
-      Field("stage", StringType, resolve = x => encoder.fromEncodedString(x.value.id).stage)
+      Field("stage", StringType, resolve = x => encoder.fromEncodedString(x.value.id).stage),
+      Field("datamodel", StringType, resolve = { ctx =>
+        val project = ctx.value
+        migrationPersistence.byId(MigrationId(project.id, project.revision)).map(_.get.rawDataModel)
+      })
     )
   )
 }
