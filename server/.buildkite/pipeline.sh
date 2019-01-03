347967568693863ae21fce7df7af2e48131cfe70
Dominic Petrick <dompetrick@gmail.com>
2019-01-03 14:20:30 +0100

    Default command

--- server/.buildkite/pipeline.sh
+++ server/.buildkite/pipeline.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-$(which ruby) $(dirname $0)/build-cli/cli.rb "$@"
+
+CMD=$1
+
+if [ -n "$CMD" ]; then
+  $(which ruby) $(dirname $0)/build-cli/cli.rb "$@"
+else
+  $(which ruby) $(dirname $0)/build-cli/cli.rb pipeline
+fi
