ae253dc7d53835795c6bd362674a3312fa88a6ef
do4gr <matthias.oertel@gmx.net>
2019-01-04 16:02:42 +0100

    document differing behavior during testing and prod

--- server/connectors/deploy-connector-mongo/src/main/scala/com/prisma/deploy/connector/mongo/impl/mutactions/RelationMutactionInterpreters.scala
+++ server/connectors/deploy-connector-mongo/src/main/scala/com/prisma/deploy/connector/mongo/impl/mutactions/RelationMutactionInterpreters.scala
@@ -8,9 +8,11 @@ import com.prisma.shared.models.Relation
 
 import scala.concurrent.Future
 
+//Fixme: this should never be triggered since Mongo does not use RelationTables, but it is  used in testing Mode
+//once we switch over the tests to datamodel v2 this can go back to a NoOp
 object CreateRelationInterpreter extends MongoMutactionInterpreter[CreateRelationTable] {
-  override def execute(mutaction: CreateRelationTable)  = NoAction.unit
-  override def rollback(mutaction: CreateRelationTable) = NoAction.unit
+  override def execute(mutaction: CreateRelationTable)  = IndexhelperForTests.add(mutaction.relation)
+  override def rollback(mutaction: CreateRelationTable) = IndexhelperForTests.remove(mutaction.relation)
 }
 
 object DeleteRelationInterpreter extends MongoMutactionInterpreter[DeleteRelationTable] {
@@ -37,7 +39,7 @@ object UpdateRelationInterpreter extends MongoMutactionInterpreter[UpdateRelatio
 //Fixme add relationindexes for ids on embedded types
 //embedded types need their path upwards to set the relation index
 
-object Indexhelper {
+object IndexhelperForTests {
   def add(relation: Relation) = DeployMongoAction { database =>
     (relation.isInlineRelation, relation.modelAField.relationIsInlinedInParent) match {
       case (true, true) if !relation.modelB.isEmbedded  => addRelationIndex(database, relation.modelAField.model.dbName, relation.modelAField.dbName)
@@ -51,6 +53,24 @@ object Indexhelper {
       case (true, true) if !relation.modelB.isEmbedded  => removeRelationIndex(database, relation.modelAField.model.dbName, relation.modelAField.dbName)
       case (true, false) if !relation.modelA.isEmbedded => removeRelationIndex(database, relation.modelBField.model.dbName, relation.modelBField.dbName)
       case (_, _)                                       => Future.successful(())
+    }
+  }
+}
+
+object Indexhelper {
+  def add(relation: Relation) = DeployMongoAction { database =>
+    (relation.modelA.isEmbedded || relation.modelB.isEmbedded, relation.modelAField.relationIsInlinedInParent) match {
+      case (true, _)      => Future.successful(())
+      case (false, true)  => addRelationIndex(database, relation.modelAField.model.dbName, relation.modelAField.dbName)
+      case (false, false) => addRelationIndex(database, relation.modelBField.model.dbName, relation.modelBField.dbName)
+    }
+  }
+
+  def remove(relation: Relation) = DeployMongoAction { database =>
+    (relation.modelA.isEmbedded || relation.modelB.isEmbedded, relation.modelAField.relationIsInlinedInParent) match {
+      case (true, _)      => Future.successful(())
+      case (false, true)  => removeRelationIndex(database, relation.modelAField.model.dbName, relation.modelAField.dbName)
+      case (false, false) => removeRelationIndex(database, relation.modelBField.model.dbName, relation.modelBField.dbName)
     }
   }
 }
