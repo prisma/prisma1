a553db1625ad8889c1cab162f3e44d0aa87fc36c
Dominic Petrick <dompetrick@gmail.com>
2019-01-15 17:43:28 +0100

    Rc11 fixes.

--- server/images/prisma-native/src/main/java/Target_akka_actor_LightArrayRevolverScheduler$.java
+++ server/images/prisma-native/src/main/java/Target_akka_actor_LightArrayRevolverScheduler$.java
@@ -7,5 +7,5 @@ final class Target_akka_actor_LightArrayRevolverScheduler$ {
     @Alias @RecomputeFieldValue(kind = Kind.FieldOffset, //
             declClassName = "akka.actor.LightArrayRevolverScheduler$TaskHolder", //
             name = "task")
-    public final long akka$actor$LightArrayRevolverScheduler$$taskOffset = 0L;
+    public long akka$actor$LightArrayRevolverScheduler$$taskOffset = 0L;
 }
