02d30c2acb91f59f751355ccae8fa60793eb35fa
Marcus Böhm <wif.mboehm@gmx.de>
2019-01-18 15:06:33 +0100

    bugfix: provide a default sequence when strategy AUTO is used for int id fields

--- server/servers/deploy/src/main/scala/com/prisma/deploy/migration/validation/directives/IdDirective.scala
+++ server/servers/deploy/src/main/scala/com/prisma/deploy/migration/validation/directives/IdDirective.scala
@@ -55,7 +55,12 @@ object IdDirective extends FieldDirective[IdBehaviour] {
   override def value(document: Document, typeDef: ObjectTypeDefinition, fieldDef: FieldDefinition, capabilities: ConnectorCapabilities) = {
     fieldDef.directive(name).map { directive =>
       val strategy = IdStrategyArgument(capabilities).value(directive).getOrElse(FieldBehaviour.IdStrategy.Auto)
-      val sequence = SequenceDirective.value(document, typeDef, fieldDef, capabilities)
+      val sequence = (strategy, fieldDef.typeIdentifier(document)) match {
+        case (FieldBehaviour.IdStrategy.Auto, TypeIdentifier.Int) =>
+          Some(FieldBehaviour.Sequence(s"${typeDef.name}_${fieldDef.name}_seq", initialValue = 1, allocationSize = 100))
+        case _ =>
+          SequenceDirective.value(document, typeDef, fieldDef, capabilities)
+      }
       IdBehaviour(strategy, sequence)
     }
   }
