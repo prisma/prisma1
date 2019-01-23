14d3adb1e7c662b0d4532488f3117fa0c6438f10
Dominic Petrick <dompetrick@gmail.com>
2019-01-23 13:08:43 +0100

    Cleanup

--- server/libs/prisma-rs-build/src/test/scala/com/prisma/cache/AwaitUtil.scala
+++
@@ -1,8 +0,0 @@
-package com.prisma.cache
-
-import scala.concurrent.{Await, Awaitable}
-
-trait AwaitUtil {
-  import scala.concurrent.duration._
-  def await[T](awaitable: Awaitable[T]): T = Await.result(awaitable, 5.seconds)
-}
