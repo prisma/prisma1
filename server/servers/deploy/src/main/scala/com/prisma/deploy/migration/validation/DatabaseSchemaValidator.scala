d626bf583b872c90bf8268c55dfd9856f1af1a7b
Marcus Böhm <wif.mboehm@gmx.de>
2019-01-23 14:59:18 +0100

    a lot more test cases

--- server/servers/deploy/src/main/scala/com/prisma/deploy/migration/validation/DatabaseSchemaValidator.scala
+++ server/servers/deploy/src/main/scala/com/prisma/deploy/migration/validation/DatabaseSchemaValidator.scala
@@ -1,6 +1,7 @@
 package com.prisma.deploy.migration.validation
 import com.prisma.deploy.connector.{Column, DatabaseSchema, Table}
-import com.prisma.shared.models.{Field, Model, Schema}
+import com.prisma.shared.models.Manifestations.{EmbeddedRelationLink, RelationTable}
+import com.prisma.shared.models.{Field, Model, Relation, Schema}
 import com.prisma.utils.boolean.BooleanUtils
 
 trait DatabaseSchemaValidator {
@@ -14,7 +15,7 @@ object DatabaseSchemaValidatorImpl extends DatabaseSchemaValidator {
 }
 
 case class DatabaseSchemaValidatorImpl(schema: Schema, databaseSchema: DatabaseSchema) extends BooleanUtils {
-  def check = modelErrors ++ fieldErrors
+  def check = modelErrors ++ fieldErrors ++ relationErrors
 
   val modelErrors = schema.models.flatMap { model =>
     val missingTableError = table(model).isEmpty.toOption {
@@ -26,7 +27,7 @@ case class DatabaseSchemaValidatorImpl(schema: Schema, databaseSchema: DatabaseS
   val fieldErrors = {
     val tmp = for {
       model <- schema.models
-      field <- model.fields
+      field <- model.scalarFields
       _     <- table(model).toVector // only run the validation if the table exists
     } yield {
       column(field) match {
@@ -45,6 +46,100 @@ case class DatabaseSchemaValidatorImpl(schema: Schema, databaseSchema: DatabaseS
     }
 
     tmp.flatten
+  }
+
+  val relationErrors = schema.relations.flatMap { relation =>
+    relation.manifestation match {
+      case Some(link: EmbeddedRelationLink) =>
+        validateEmbeddedRelationLink(link)
+
+      case Some(relationTable: RelationTable) =>
+        validateRelationTable(relation, relationTable)
+
+      case _ => None // can't happen because the new SchemaInferrer always sets it
+    }
+  }
+
+  private def validateEmbeddedRelationLink(embeddedRelationLink: EmbeddedRelationLink): Option[DeployError] = {
+    val model = schema.getModelByName_!(embeddedRelationLink.inTableOfModelName)
+    val field = model.relationFields.find(_.dbName == embeddedRelationLink.referencingColumn).get
+
+    column(field) match {
+      case None =>
+        Some(DeployError(model.name, field.name, s"Could not find the column for the inline relation field ${field.name} in the database."))
+
+      case Some(Column.withForeignKey(fk)) if fk.table != field.relatedModel_!.dbName =>
+        Some(
+          DeployError(
+            model.name,
+            field.name,
+            s"The column for the inline relation field ${field.name} is not referencing the right table. It should reference the table of model `${field.relatedModel_!.name}` but is referencing the table `${fk.table}`."
+          ))
+
+      case Some(Column.withForeignKey(fk)) if fk.column != field.relatedModel_!.dbNameOfIdField_! =>
+        Some(
+          DeployError(
+            model.name,
+            field.name,
+            s"The column for the inline relation field ${field.name} is not referencing a valid column. Those columns must always reference the column of the id field of related model. So it should reference `${field.relatedModel_!.dbNameOfIdField_!}` instead of `${fk.column}`."
+          ))
+
+      case Some(Column.withForeignKey(_)) =>
+        None // the FK constraint is fine
+
+      case _ =>
+        Some(DeployError(model.name, field.name, s"The column for the inline relation field ${field.name} is missing a foreign key constraint."))
+    }
+  }
+
+  private def validateRelationTable(relation: Relation, manifestation: RelationTable): Vector[DeployError] = {
+    val nameOfLinkTableType = relation.name
+    databaseSchema.table(manifestation.table) match {
+      case None =>
+        Vector(DeployError(nameOfLinkTableType, s"Could not find the table `${manifestation.table}` for the relation `${relation.name}` in the database."))
+      case Some(table) =>
+        def missingColumn(columnName: String) = table.hasNotColumn(columnName).toOption {
+          DeployError(nameOfLinkTableType, columnName, s"The link table ${relation.name} is missing the column `$columnName`.")
+        }
+        def fkReferenceWrong(columnName: String, relatedModel: Model) = {
+          table.column(columnName) match {
+            case Some(Column.withForeignKey(fk)) if fk.table != relatedModel.dbName =>
+              Some(
+                DeployError(
+                  nameOfLinkTableType,
+                  columnName,
+                  s"The column `$columnName` of the relation table `${table.name}` is not referencing the right table. It should reference the table of model `${relatedModel.name}` but is referencing the table `${fk.table}`."
+                ))
+
+            case Some(Column.withForeignKey(fk)) if fk.column != relatedModel.dbNameOfIdField_! =>
+              Some(
+                DeployError(
+                  nameOfLinkTableType,
+                  columnName,
+                  s"The column `$columnName` of the relation table `${table.name}` is not referencing a valid column. Those columns must always reference the column of the id field of the related model. So it should reference `${relatedModel.dbNameOfIdField_!}` instead of `${fk.column}`."
+                ))
+
+            case Some(Column.withForeignKey(_)) =>
+              None
+
+            case Some(_) =>
+              Some(
+                DeployError(
+                  nameOfLinkTableType,
+                  columnName,
+                  s"The column `$columnName` for the relation table `${table.name}` is missing a foreign key constraint."
+                ))
+
+            case None =>
+              None // is already handled by the method above
+          }
+        }
+        val missingIdColumn = manifestation.idColumn.flatMap(missingColumn)
+        val errorsOfAColumn = missingColumn(relation.modelAColumn) ++ fkReferenceWrong(relation.modelAColumn, relation.modelA)
+        val errorsOfBColumn = missingColumn(relation.modelBColumn) ++ fkReferenceWrong(relation.modelBColumn, relation.modelB)
+
+        (missingIdColumn ++ errorsOfAColumn ++ errorsOfBColumn).toVector
+    }
   }
 
   private def table(model: Model): Option[Table]   = databaseSchema.table(model.dbName)
--- server/servers/deploy/src/test/scala/com/prisma/deploy/migration/DatabaseSchemaValidatorSpec.scala
+++ server/servers/deploy/src/test/scala/com/prisma/deploy/migration/DatabaseSchemaValidatorSpec.scala
@@ -1,7 +1,9 @@
 package com.prisma.deploy.migration
+import com.prisma.deploy.connector.DatabaseSchema
+import com.prisma.deploy.migration.validation.DeployError
 import com.prisma.deploy.specutils.PassiveDeploySpecBase
 import com.prisma.shared.models.{ConnectorCapabilities, ConnectorCapability}
-import com.prisma.shared.models.ConnectorCapability.{IntIdCapability, IntrospectionCapability, MigrationsCapability}
+import com.prisma.shared.models.ConnectorCapability.{IntIdCapability, IntrospectionCapability, MigrationsCapability, RelationLinkTableCapability}
 import org.scalatest.{Matchers, WordSpecLike}
 
 class DatabaseSchemaValidatorSpec extends WordSpecLike with Matchers with PassiveDeploySpecBase {
@@ -33,7 +35,7 @@ class DatabaseSchemaValidatorSpec extends WordSpecLike with Matchers with Passiv
          |}
        """.stripMargin
 
-    val errors = deploy(dataModel, ConnectorCapabilities(IntIdCapability))
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability))
     errors should have(size(1))
     val error = errors.head
     error.`type` should be("Blog")
@@ -66,7 +68,7 @@ class DatabaseSchemaValidatorSpec extends WordSpecLike with Matchers with Passiv
          |}
        """.stripMargin
 
-    val errors = deploy(dataModel, ConnectorCapabilities(IntIdCapability))
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability))
     errors should have(size(1))
     val error = errors.head
     error.`type` should be("Blog")
@@ -101,7 +103,7 @@ class DatabaseSchemaValidatorSpec extends WordSpecLike with Matchers with Passiv
          |}
        """.stripMargin
 
-    val errors = deploy(dataModel, ConnectorCapabilities(IntIdCapability))
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability))
     errors should have(size(1))
     val error = errors.head
     error.`type` should be("Blog")
@@ -110,10 +112,439 @@ class DatabaseSchemaValidatorSpec extends WordSpecLike with Matchers with Passiv
       "The underlying column for the field title has an incompatible type. The field has type `Int` and the column has type `String`.")
   }
 
-  private def deploy(dataModel: String, capabilities: ConnectorCapabilities) = {
+  "it should succeed if an inline relation field exists" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(id)
+         |);
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  author: Author! @relation(link: INLINE)
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+       """.stripMargin
+
+    deploy(dataModel, ConnectorCapabilities(IntIdCapability))
+  }
+
+  "it should error if the column for an inline relation field is missing" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY
+         |);
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  author: Author! @relation(link: INLINE)
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability))
+    errors should have(size(1))
+    val error = errors.head
+    error.`type` should be("Blog")
+    error.field should be(Some("author"))
+    error.description should be("Could not find the column for the inline relation field author in the database.")
+  }
+
+  "it should error if the foreign key constraint for an inline relation field is referencing the wrong table" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY
+         | );
+         | CREATE TABLE wrong_table (
+         |   id SERIAL PRIMARY KEY
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references wrong_table(id)
+         |);
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  author: Author! @relation(link: INLINE)
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability))
+    errors should have(size(1))
+    val error = errors.head
+    error.`type` should be("Blog")
+    error.field should be(Some("author"))
+    error.description should be(
+      "The column for the inline relation field author is not referencing the right table. It should reference the table of model `Author` but is referencing the table `wrong_table`.")
+  }
+
+  "it should error if the foreign key constraint for an inline relation field is referencing a column which is not the id" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY,
+         |   nick int UNIQUE
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(nick)
+         |);
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  author: Author! @relation(link: INLINE)
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability))
+    errors should have(size(1))
+    val error = errors.head
+    error.`type` should be("Blog")
+    error.field should be(Some("author"))
+    error.description should be(
+      "The column for the inline relation field author is not referencing a valid column. Those columns must always reference the column of the id field of related model. So it should reference `id` instead of `nick`.")
+  }
+
+  "it should error if a @linkTable is missing in the database" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY,
+         |   nick int UNIQUE
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(nick)
+         |);
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  authors: [Author] @relation(name:"BlogToAuthor")
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+         |
+         |type BlogToAuthor @linkTable @db(name:"blog_to_author"){
+         |  id: ID! @id
+         |  blog: Blog!
+         |  author: Author!
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability, RelationLinkTableCapability))
+    errors should have(size(1))
+    val error = errors.head
+    error.`type` should be("BlogToAuthor")
+    error.field should be(empty)
+    error.description should be("Could not find the table `blog_to_author` for the relation `BlogToAuthor` in the database.")
+  }
+
+  "it should error if the id column of a @linkTable is missing in the database" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY,
+         |   nick int UNIQUE
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(nick)
+         | );
+         | CREATE TABLE blog_to_author (
+         |   blog int references blog(id),
+         |   author int references author(id)
+         | )
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  authors: [Author] @relation(name:"BlogToAuthor")
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+         |
+         |type BlogToAuthor @linkTable @db(name:"blog_to_author"){
+         |  id: ID! @id
+         |  blog: Blog!
+         |  author: Author!
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability, RelationLinkTableCapability))
+    errors should have(size(1))
+    val error = errors.head
+    error.`type` should be("BlogToAuthor")
+    error.field should be(Some("id"))
+    error.description should be("The link table BlogToAuthor is missing the column `id`.")
+  }
+
+  "it should succeed if the @linkTable matches the relation table in database" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY,
+         |   nick int UNIQUE
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(nick)
+         | );
+         | CREATE TABLE blog_to_author (
+         |   blog int references blog(id),
+         |   author int references author(id)
+         | )
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  authors: [Author] @relation(name:"BlogToAuthor")
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+         |
+         |type BlogToAuthor @linkTable @db(name:"blog_to_author"){
+         |  blog: Blog!
+         |  author: Author!
+         |}
+       """.stripMargin
+
+    deploy(dataModel, ConnectorCapabilities(IntIdCapability, RelationLinkTableCapability))
+  }
+
+  "it should error if a column misses a foreign key constraint" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY,
+         |   nick int UNIQUE
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(nick)
+         | );
+         | CREATE TABLE blog_to_author (
+         |   blog int,
+         |   author int
+         | )
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  authors: [Author] @relation(name:"BlogToAuthor")
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+         |
+         |type BlogToAuthor @linkTable @db(name:"blog_to_author"){
+         |  blog: Blog!
+         |  author: Author!
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability, RelationLinkTableCapability))
+    errors should have(size(2))
+    val (error1, error2) = (errors(0), errors(1))
+    error1.`type` should be("BlogToAuthor")
+    error1.field should be(Some("author"))
+    error1.description should be("The column `author` for the relation table `blog_to_author` is missing a foreign key constraint.")
+    error2.`type` should be("BlogToAuthor")
+    error2.field should be(Some("blog"))
+    error2.description should be("The column `blog` for the relation table `blog_to_author` is missing a foreign key constraint.")
+  }
+
+  "it should error if a column of a relation table is referencing the wrong table" in {
+    val postgres =
+      s"""
+         | CREATE TABLE author (
+         |   id SERIAL PRIMARY KEY,
+         |   nick int UNIQUE
+         | );
+         | CREATE TABLE blog (
+         |   id SERIAL PRIMARY KEY,
+         |   author int references author(nick)
+         | );
+         | CREATE TABLE wrong_table(
+         |   id SERIAL PRIMARY KEY
+         | );
+         | CREATE TABLE blog_to_author (
+         |   blog int references wrong_table(id),
+         |   author int references wrong_table(id)
+         | )
+       """.stripMargin
+    val mysql =
+      s"""
+         | CREATE TABLE blog (
+         |   id int NOT NULL,
+         |   PRIMARY KEY(id)
+         | );
+       """.stripMargin
+
+    setup(SQLs(postgres = postgres, mysql = mysql))
+
+    val dataModel =
+      s"""
+         |type Blog @db(name: "blog"){
+         |  id: Int! @id
+         |  authors: [Author] @relation(name:"BlogToAuthor")
+         |}
+         |
+         |type Author @db(name: "author"){
+         |  id: Int! @id
+         |}
+         |
+         |type BlogToAuthor @linkTable @db(name:"blog_to_author"){
+         |  blog: Blog!
+         |  author: Author!
+         |}
+       """.stripMargin
+
+    val errors = deployThatMustError(dataModel, ConnectorCapabilities(IntIdCapability, RelationLinkTableCapability))
+    errors should have(size(2))
+    val (error1, error2) = (errors(0), errors(1))
+    error1.`type` should be("BlogToAuthor")
+    error1.field should be(Some("author"))
+    error1.description should be(
+      "The column `author` of the relation table `blog_to_author` is not referencing the right table. It should reference the table of model `Author` but is referencing the table `wrong_table`.")
+    error2.`type` should be("BlogToAuthor")
+    error2.field should be(Some("blog"))
+    error2.description should be(
+      "The column `blog` of the relation table `blog_to_author` is not referencing the right table. It should reference the table of model `Blog` but is referencing the table `wrong_table`.")
+  }
+
+  private def deployThatMustError(dataModel: String, capabilities: ConnectorCapabilities): Vector[DeployError] = {
     val actualCapabilities = ConnectorCapabilities(capabilities.capabilities ++ Set(IntrospectionCapability))
     val result             = deployThatMustError(dataModel, actualCapabilities, noMigration = true)
     println(result)
+    result
+  }
+
+  private def deploy(dataModel: String, capabilities: ConnectorCapabilities): DatabaseSchema = {
+    val actualCapabilities = ConnectorCapabilities(capabilities.capabilities ++ Set(IntrospectionCapability))
+    val result             = deploy(dataModel, actualCapabilities, noMigration = true)
     result
   }
 }
