4f611fa3cbd15dd51ed5ceb46e074c6ff72496d0
Julius de Bruijn <bruijn@prisma.io>
2019-01-29 13:36:05 +0100

    Cache existing databases

--- server/prisma-rs/src/connector/sqlite.rs
+++ server/prisma-rs/src/connector/sqlite.rs
@@ -1,16 +1,21 @@
 use r2d2;
 use r2d2_sqlite::SqliteConnectionManager;
-
 use rusqlite::{types::ToSql, NO_PARAMS};
+
+use std::{collections::HashSet, sync::Arc};
+use arc_swap::ArcSwap;
 
 use crate::{
     connector::Connector, error::Error, project::Project, querying::NodeSelector, PrismaResult,
 };
 
 type Connection = r2d2::PooledConnection<SqliteConnectionManager>;
+type Databases = ArcSwap<HashSet<String>>;
+type Pool = r2d2::Pool<SqliteConnectionManager>;
 
 pub struct Sqlite {
-    pool: r2d2::Pool<SqliteConnectionManager>,
+    pool: Pool,
+    databases: Databases,
 }
 
 impl Sqlite {
@@ -19,36 +24,59 @@ impl Sqlite {
             .max_size(connection_limit)
             .build(SqliteConnectionManager::memory())?;
 
-        Ok(Sqlite { pool })
+        let mut conn = pool.get()?;
+        let databases = ArcSwap::from(Arc::new(HashSet::new()));
+        let this = Sqlite { pool, databases };
+        
+        this.update_databases(&mut conn)?;
+
+        Ok(this)
     }
 
-    fn find_or_create_database(conn: &mut Connection, db_name: &str) -> PrismaResult<()> {
+    /// Updates the set of existing databases for caching purposes.
+    fn update_databases(&self, conn: &mut Connection) -> PrismaResult<()> {
         let mut stmt = conn.prepare("PRAGMA database_list")?;
 
-        let mut databases = stmt.query_map(NO_PARAMS, |row| {
-            let name: String = row.get(1);
-            name
-        })?;
+        let databases: HashSet<String> = stmt
+            .query_map(NO_PARAMS, |row| {
+                let name: String = row.get(1);
+                name
+            })?
+            .map(|res| res.unwrap())
+            .collect();
 
-        let existing = dbg!(databases.find(|res| match res {
-            Ok(ref name) => name == db_name,
-            _ => false,
-        }));
-
-        if existing.is_none() {
-            let path = format!("db/{}", db_name);
-            dbg!(conn.execute("ATTACH DATABASE ? AS ?", &[path.as_ref(), db_name])?);
-        }
+        self.databases.store(Arc::new(databases));
 
         Ok(())
     }
 
+    fn has_database(&self, db_name: &str) -> bool {
+        self.databases.load().contains(db_name)
+    }
+
+    fn create_database(conn: &mut Connection, db_name: &str) -> PrismaResult<()> {
+        let path = format!("db/{}", db_name);
+        dbg!(conn.execute("ATTACH DATABASE ? AS ?", &[path.as_ref(), db_name])?);
+
+        Ok(())
+    }
+
+    /// Take a new connection from the pool and create the database if it
+    /// doesn't exist yet.
     fn with_connection<F, T>(&self, db_name: &str, mut f: F) -> PrismaResult<T>
     where
         F: FnMut(Connection) -> PrismaResult<T>,
     {
         let mut conn = dbg!(self.pool.get()?);
-        Self::find_or_create_database(&mut conn, db_name)?;
+
+        if !self.has_database(db_name) {
+            // We might have a race if having more clients for the same
+            // databases. Create will silently fail and we'll get the database
+            // name to our bookkeeping.
+            Self::create_database(&mut conn, db_name)?;
+            self.update_databases(&mut conn)?;
+        }
+
         f(conn)
     }
 }
