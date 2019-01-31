fcd9ad3e129a7a71baed6ee3dbc6e614c17f8c1c
Dominic Petrick <dompetrick@gmail.com>
2019-01-31 16:35:34 +0100

    Dead-end experiment

--- server/prisma-rs/src/querying/query_builder.rs
+++ server/prisma-rs/src/querying/query_builder.rs
@@ -9,6 +9,7 @@ pub enum SQLDialect {
 struct QueryBuilder {
   dialect: SQLDialect,
   // parameter binding type?
+  // escaping handler?
 }
 
 impl QueryBuilder {
@@ -16,7 +17,6 @@ impl QueryBuilder {
     match dialect {
       SQLDialect::SQLite => QueryBuilder {
         dialect: dialect,
-        // placeholder: "?",
       },
       _ => panic!("Unsupported dialect: {:?}", dialect),
     }
@@ -25,29 +25,151 @@ impl QueryBuilder {
   fn render(&self) -> String {
     unimplemented!();
   }
-
-  // fn select()
 }
 
-struct Condition {}
+enum Step<'a> {
+  SelectStep(SelectStep<'a>),
+  FromStep(FromStep<'a>),
+  ConditionStep(ConditionStep<'a>),
+}
 
-struct Value {}
+struct SelectStep<'a> {
+  selectors: &'a [Column<'a>],
+  parent_step: Option<Step<'a>>,
+}
 
-struct Field<'a> {
+impl<'a> SelectStep<'a> {
+  // multiple tables possible
+  fn from(self, parts: Vec<&'a str>) -> FromStep {
+    FromStep {
+      table: TableQualifier { parts },
+      parent_step: Some(&self.into_step()),
+    }
+  }
+
+  fn into_step(self) -> Step<'a> {
+    Step::SelectStep(self)
+  }
+}
+
+struct FromStep<'a> {
+  table: TableQualifier<'a>,
+  parent_step: Option<&'a Step<'a>>,
+}
+
+impl<'a> FromStep<'a> {
+  fn where_(self, condition: Condition<'a>) -> ConditionStep<'a> {
+    let expression = ConditionExpression {
+      ty: ConditionExpressionType::AND,
+      condition,
+    };
+
+    ConditionStep { conditions: vec!(expression) }
+  }
+}
+
+struct TableQualifier<'a> {
   parts: Vec<&'a str>,
+  // alias
+}
+
+enum ConditionExpressionType {
+  AND,
+  OR,
+}
+
+struct ConditionExpression<'a> {
+  ty: ConditionExpressionType,
+  condition: Condition<'a>,
+}
+
+struct ConditionStep<'a> {
+  conditions: Vec<ConditionExpression<'a>>
+}
+
+impl<'a> ConditionStep<'a> {
+  fn and(mut self, condition: Condition<'a>) -> Self {
+    self.conditions.push(ConditionExpression {
+      ty: ConditionExpressionType::AND,
+      condition,
+    });
+
+    self
+  }
+}
+
+enum ConditionType {
+  EQ,
+  GT,
+  GTE,
+  LT,
+  LTE,
+  IN,
+  IsNotNull,
+  IsNull
+}
+
+enum Field<'a> {
+  ColumnReference(Column<'a>),
+  SqlBindParam,
+}
+
+struct Condition<'a> {
+  ty: ConditionType,
+  left: Field<'a>,
+  right: Option<Field<'a>>
+}
+
+struct Column<'a> {
+  parts: &'a [&'a str],
   alias: Option<&'a str>,
 }
 
-impl<'a> Field<'a> {
-  fn eq(field: Field) -> Condition {
-    unimplemented!();
+impl<'a> Column<'a> {
+  fn eq(self, col: Column<'a>) -> Condition {
+    Condition {
+      ty: ConditionType::EQ,
+      left: self.into_field(),
+      right: Some(Field::ColumnReference(col))
+    }
   }
 
-  fn equal(val: Value) -> Condition {
-    unimplemented!();
+  fn into_field(self) -> Field<'a> {
+    Field::ColumnReference(self)
   }
+
+  fn equal(self, field: Field<'a>) -> Condition<'a> {
+    Condition {
+      ty: ConditionType::EQ,
+      left: self.into_field(),
+      right: Some(field)
+    }
+  }
+
+  // fn _in<T>(elems: &[T]) -> Condition {
+  //   unimplemented!();
+  // }
 }
 
-fn field<'a>(parts: Vec<&'a str>) -> Field<'a> {
-  Field { parts, alias: None }
+fn col<'a>(parts: &'a [&'a str]) -> Column<'a> {
+  Column { parts, alias: None }
+}
+
+fn select<'a>(columns: &'a[Column<'a>]) -> SelectStep<'a> {
+  SelectStep { selectors: columns, parent_step: None }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test() {
+    let builder = QueryBuilder::new(SQLDialect::SQLite);
+
+    select(&[col(&["testdb", "test"])])
+      .from(vec!("testdb", "test"))
+      .where_(col(&["a"]).equal(Field::SqlBindParam))
+      .and(col(&["b"]).eq(col(&["c"])));
+  }
 }
