4bd1df739d78e22241e7b366ab43dfa4c284e05b
Marcus Böhm <wif.mboehm@gmx.de>
2019-02-04 10:38:42 +0100

    add native sqlite as target in Makefile

--- server/connectors/utils/src/main/scala/com/prisma/connectors/utils/ConnectorLoader.scala
+++ server/connectors/utils/src/main/scala/com/prisma/connectors/utils/ConnectorLoader.scala
@@ -17,17 +17,16 @@ import scala.concurrent.ExecutionContext
 object ConnectorLoader {
   def loadApiConnector(config: PrismaConfig)(implicit ec: ExecutionContext, drivers: SupportedDrivers): ApiConnector = {
     val databaseConfig = config.databases.head
-    val loadNative     = sys.env.getOrElse("USE_NATIVE_DRIVER", "0") == "1"
 
-    (databaseConfig.connector, databaseConfig.active, loadNative) match {
-      case ("mysql", true, _)        => MySqlApiConnector(databaseConfig, drivers(SupportedDrivers.MYSQL), config.isPrototype)
-      case ("mysql", false, _)       => sys.error("There is not passive mysql deploy connector yet!")
-      case ("postgres", isActive, _) => PostgresApiConnector(databaseConfig, drivers(SupportedDrivers.POSTGRES), isActive, config.isPrototype)
-      case ("sqlite", _, true)       => SQLiteApiConnectorNative(databaseConfig, config.isPrototype)
-      case ("sqlite", true, _)       => SQLiteApiConnector(databaseConfig, isPrototype = config.isPrototype)
-      case ("sqlite", false, _)      => sys.error("There is no passive sqlite deploy connector yet!")
-      case ("mongo", _, _)           => MongoApiConnector(databaseConfig)
-      case (conn, _, _)              => sys.error(s"Unknown connector $conn")
+    (databaseConfig.connector, databaseConfig.active) match {
+      case ("mysql", true)                     => MySqlApiConnector(databaseConfig, drivers(SupportedDrivers.MYSQL), config.isPrototype)
+      case ("mysql", false)                    => sys.error("There is not passive mysql deploy connector yet!")
+      case ("postgres", isActive)              => PostgresApiConnector(databaseConfig, drivers(SupportedDrivers.POSTGRES), isActive, config.isPrototype)
+      case ("sqlite", _) if config.isPrototype => SQLiteApiConnectorNative(databaseConfig, config.isPrototype)
+      case ("sqlite", true)                    => SQLiteApiConnector(databaseConfig, isPrototype = config.isPrototype)
+      case ("sqlite", false)                   => sys.error("There is no passive sqlite deploy connector yet!")
+      case ("mongo", _)                        => MongoApiConnector(databaseConfig)
+      case (conn, _)                           => sys.error(s"Unknown connector $conn")
     }
   }
 
