139e8c264b8ff190636d1d7514a54c4ab6261472
Divyendu Singh <divyendu.z@gmail.com>
2019-02-06 23:06:31 +0530

    fix(go-client): handle empty secret env interpolation

--- cli/packages/prisma-client-lib/src/codegen/generators/__tests__/env-interpolation/go-env.test.ts
+++ cli/packages/prisma-client-lib/src/codegen/generators/__tests__/env-interpolation/go-env.test.ts
@@ -1,6 +1,16 @@
 import { GoGenerator } from '../../go-client'
 import { test } from 'ava'
 
+test('go env interpolation - empty', t => {
+  const result = GoGenerator.replaceEnv(``)
+  t.snapshot(result)
+})
+
+test('go env interpolation - null', t => {
+  const result = GoGenerator.replaceEnv(null as any)
+  t.snapshot(result)
+})
+
 test('go env interpolation - plain', t => {
   const result = GoGenerator.replaceEnv(`http://localhost:4466/test/dev`)
   t.snapshot(result)
