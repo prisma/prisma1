6e74a3eaf4687851e56cdb049448050e6eb7f2e9
Julius de Bruijn <bruijn@prisma.io>
2019-02-27 17:57:20 +0100

    Rest of the relation domain logic

--- server/prisma-rs/src/models/relation.rs
+++ server/prisma-rs/src/models/relation.rs
@@ -1,4 +1,4 @@
-use crate::models::prelude::*;
+use crate::{error::Error, models::prelude::*, PrismaResult};
 use once_cell::unsync::OnceCell;
 use std::sync::{Arc, Weak};
 
@@ -11,10 +11,35 @@ pub enum OnDelete {
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
+pub struct InlineRelation {
+    #[serde(rename = "inTableOfModelId")]
+    pub in_table_of_model_name: String,
+    pub referencing_column: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RelationTable {
+    pub table: String,
+    pub model_a_column: String,
+    pub model_b_column: String,
+    pub id_column: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase", tag = "relation_manifestation_type")]
+pub enum RelationLinkManifestation {
+    Inline(InlineRelation),
+    RelationTable(RelationTable),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct RelationTemplate {
     pub name: String,
     pub model_a_on_delete: OnDelete,
     pub model_b_on_delete: OnDelete,
+    pub manifestation: Option<RelationLinkManifestation>,
 
     #[serde(rename = "modelAId")]
     pub model_a_name: String,
@@ -29,12 +54,15 @@ pub struct Relation {
     model_a: OnceCell<RelationAttributes>,
     model_b: OnceCell<RelationAttributes>,
 
+    manifestation: Option<RelationLinkManifestation>,
+
     #[debug_stub = "#SchemaWeakRef#"]
     pub schema: SchemaWeakRef,
 }
 
 #[derive(DebugStub)]
 struct RelationAttributes {
+    pub name: String,
     #[debug_stub = "#ModelWeakRef#"]
     pub model: ModelWeakRef,
     #[debug_stub = "#FieldWeakRef#"]
@@ -43,8 +71,14 @@ struct RelationAttributes {
 }
 
 impl RelationAttributes {
-    pub fn new(model: ModelRef, field: Arc<RelationField>, on_delete: OnDelete) -> Self {
+    pub fn new(
+        name: String,
+        model: ModelRef,
+        field: Arc<RelationField>,
+        on_delete: OnDelete,
+    ) -> Self {
         RelationAttributes {
+            name: name,
             model: Arc::downgrade(&model),
             field: Arc::downgrade(&field),
             on_delete: on_delete,
@@ -59,6 +93,7 @@ impl RelationTemplate {
 
         let relation = Relation {
             name: self.name,
+            manifestation: self.manifestation,
             model_a: OnceCell::new(),
             model_b: OnceCell::new(),
             schema: schema,
@@ -72,7 +107,7 @@ impl RelationTemplate {
                 .find_from_relation(&relation.name, RelationSide::A)
                 .unwrap();
 
-            RelationAttributes::new(model, field, self.model_a_on_delete)
+            RelationAttributes::new(model_a_name, model, field, self.model_a_on_delete)
         };
 
         let model_b = {
@@ -83,7 +118,7 @@ impl RelationTemplate {
                 .find_from_relation(&relation.name, RelationSide::B)
                 .unwrap();
 
-            RelationAttributes::new(model, field, self.model_b_on_delete)
+            RelationAttributes::new(model_b_name, model, field, self.model_b_on_delete)
         };
 
         relation.model_a.set(model_a).unwrap();
@@ -94,7 +129,10 @@ impl RelationTemplate {
 }
 
 impl Relation {
-    pub fn with_schema<F, T>(&self, f: F) -> T
+    const MODEL_A_DEFAULT_COLUMN: &'static str = "A";
+    const MODEL_B_DEFAULT_COLUMN: &'static str = "B";
+
+    fn with_schema<F, T>(&self, f: F) -> T
     where
         F: FnOnce(SchemaRef) -> T,
     {
@@ -104,6 +142,24 @@ impl Relation {
                 "Schema does not exist anymore. Parent schema is deleted without deleting the child schema."
             )
         }
+    }
+
+    pub fn is_inline_relation(&self) -> bool {
+        self.manifestation
+            .as_ref()
+            .map(|manifestation| match manifestation {
+                RelationLinkManifestation::RelationTable(_) => true,
+                _ => false,
+            })
+            .unwrap_or(false)
+    }
+
+    pub fn is_relation_table(&self) -> bool {
+        !self.is_inline_relation()
+    }
+
+    pub fn is_self_relation(&self) -> bool {
+        self.model_a().name == self.model_b().name
     }
 
     pub fn both_sides_cascade(&self) -> bool {
@@ -122,12 +178,124 @@ impl Relation {
             .expect("Model A deleted without deleting the relations in schama.")
     }
 
+    pub fn field_a(&self) -> Arc<RelationField> {
+        self.model_a
+            .get()
+            .unwrap()
+            .field
+            .upgrade()
+            .expect("Field A deleted without deleting the relations in schama.")
+    }
+
     pub fn model_b(&self) -> ModelRef {
         self.model_b
             .get()
             .unwrap()
             .model
             .upgrade()
-            .expect("Model A deleted without deleting the relations in schama.")
+            .expect("Model B deleted without deleting the relations in schama.")
+    }
+
+    pub fn field_b(&self) -> Arc<RelationField> {
+        self.model_b
+            .get()
+            .unwrap()
+            .field
+            .upgrade()
+            .expect("Field A deleted without deleting the relations in schama.")
+    }
+
+    pub fn relation_table_name(&self) -> String {
+        use RelationLinkManifestation::*;
+
+        match self.manifestation {
+            Some(RelationTable(ref m)) => m.table.clone(),
+            Some(Inline(ref m)) => self.with_schema(|schema| {
+                schema
+                    .find_model(&m.in_table_of_model_name)
+                    .unwrap()
+                    .db_name()
+                    .to_string()
+            }),
+            None => format!("_{}", self.name),
+        }
+    }
+
+    pub fn model_a_column(&self) -> String {
+        use RelationLinkManifestation::*;
+
+        match self.manifestation {
+            Some(RelationTable(ref m)) => m.model_a_column.clone(),
+            Some(Inline(ref m)) => {
+                let model = self.model_a();
+
+                if m.in_table_of_model_name == model.name && !self.is_self_relation() {
+                    model.fields().id().db_name().to_string()
+                } else {
+                    m.referencing_column.to_string()
+                }
+            }
+            None => Self::MODEL_A_DEFAULT_COLUMN.to_string(),
+        }
+    }
+
+    pub fn model_b_column(&self) -> String {
+        use RelationLinkManifestation::*;
+
+        match self.manifestation {
+            Some(RelationTable(ref m)) => m.model_b_column.clone(),
+            Some(Inline(ref m)) => {
+                let model = self.model_b();
+
+                if m.in_table_of_model_name == model.name && !self.is_self_relation() {
+                    model.fields().id().db_name().to_string()
+                } else {
+                    m.referencing_column.to_string()
+                }
+            }
+            None => Self::MODEL_B_DEFAULT_COLUMN.to_string(),
+        }
+    }
+
+    pub fn is_many_to_many(&self) -> bool {
+        self.field_a().is_list && self.field_b().is_list
+    }
+
+    pub fn id_column(&self) -> Option<&str> {
+        use RelationLinkManifestation::*;
+
+        match self.manifestation {
+            None => Some("id"),
+            Some(RelationTable(ref m)) => Some(m.id_column.as_ref()),
+            _ => None,
+        }
+    }
+
+    pub fn relation_table_has_3_columns(&self) -> bool {
+        self.id_column().is_some()
+    }
+
+    pub fn column_for_relation_side(&self, side: RelationSide) -> String {
+        match side {
+            RelationSide::A => self.model_a_column(),
+            RelationSide::B => self.model_b_column(),
+        }
+    }
+
+    pub fn contains_the_model(&self, model: ModelRef) -> bool {
+        self.model_a().name == model.name || self.model_b().name == model.name
+    }
+
+    pub fn get_field_on_model(&self, model_id: &str) -> PrismaResult<Arc<RelationField>> {
+        if model_id == self.model_a().name {
+            Ok(self.field_a())
+        } else if model_id == self.model_b().name {
+            Ok(self.field_b())
+        } else {
+            Err(Error::InvalidInputError(format!(
+                "The model id {} is not part of the relation {}",
+                model_id, self.name
+            )))
+        }
     }
 }
