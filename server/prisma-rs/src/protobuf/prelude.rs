de964627ee46f40a30b131f9246d5653fad4ec60
Dominic Petrick <dompetrick@gmail.com>
2019-03-04 11:31:50 +0100

    Being get_related_nodes boilerplate up to date.

--- server/prisma-rs/src/protobuf/prelude.rs
+++ server/prisma-rs/src/protobuf/prelude.rs
@@ -3,9 +3,9 @@ pub use super::{
         self, filter, graphql_id, graphql_id::IdValue, order_by, order_by::SortOrder,
         relation_filter, result, rpc_response as rpc, scalar_filter, selected_field,
         value_container::PrismaValue, AndFilter, Error as ProtoError, Filter, GetNodeByWhereInput,
-        GetNodesInput, GraphqlId, Header, MultiContainer, Node, NodesResult, NotFilter, OrFilter,
-        OrderBy, QueryArguments, RelationFilter, RpcResponse, ScalarFilter, SelectedField,
-        ValueContainer,
+        GetNodesInput, GetRelatedNodesInput, GraphqlId, Header, MultiContainer, Node, NodesResult,
+        NotFilter, OrFilter, OrderBy, QueryArguments, RelationFilter, RpcResponse, ScalarFilter,
+        SelectedField, ValueContainer,
     },
     query_arguments::IntoConditionTree,
 };
