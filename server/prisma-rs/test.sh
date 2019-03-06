7e1461df6344b0b79af28b8ee93b0832d81d3b18
Marcus Böhm <boehm@prisma.io>
2019-03-06 11:58:24 +0100

    fail on error

--- server/prisma-rs/test.sh
+++ server/prisma-rs/test.sh
@@ -1,6 +1,10 @@
-# tests all rust projects. 
+#!/bin/bash
+set -e
+
+# This script tests all rust projects. 
 # We are not using cargo workspaces because this is causing troubles with JNA. 
 # With workspaces the dylibs for query-engine are not bitwise identical to the ones without workspaces.
+
 function test() {
     folder_to_test=$1
     echo "will run cargo test in $folder_to_test"
