0a9ff855a1b797ea39b18e0b8748dc2ea6754b65
Marcus Böhm <boehm@prisma.io>
2019-03-06 11:53:57 +0100

    refactor test.sh

--- server/prisma-rs/test.sh
+++ server/prisma-rs/test.sh
@@ -1,8 +1,17 @@
 # tests all rust projects. 
 # We are not using cargo workspaces because this is causing troubles with JNA. 
 # With workspaces the dylibs for query-engine are not bitwise identical to the ones without workspaces.
+function test() {
+    folder_to_test=$1
+    echo "will run cargo test in $folder_to_test"
+    current_dir=$PWD
+    cd $folder_to_test
+    cargo test
+    cd $current_dir
+}
+
 for lib in libs/*/ ; do
-    cd $lib && cargo test && cd -
+    test $lib
 done
-cd migration-engine && cargo test && cd -
-cd query-engine && cargo test && cd -
+test "migration-engine"
+test "query-engine"
