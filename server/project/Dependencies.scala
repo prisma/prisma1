b2b3a872230c4a8f050053eed56dc74099c26650
do4gr <matthias.oertel@gmx.net>
2019-03-09 19:15:45 +0100

    add explicit dependency for netty

--- server/project/Dependencies.scala
+++ server/project/Dependencies.scala
@@ -42,6 +42,10 @@ object Dependencies {
   val playJson    = "com.typesafe.play" %% "play-json"    % v.play
   val playStreams = "com.typesafe.play" %% "play-streams" % v.play
 
+  val nettyTransport = "io.netty" % "netty-transport" % "4.0.55.Final"
+  val nettyHandler   = "io.netty" % "netty-handler" % "4.0.19.Final"
+  val netty          = Seq(nettyTransport, nettyHandler)
+
   val akka            = "com.typesafe.akka" %% "akka-actor"        % v.akka
   val akkaStream      = "com.typesafe.akka" %% "akka-stream"       % v.akka
   val akkaTestKit     = "com.typesafe.akka" %% "akka-testkit"      % v.akka
