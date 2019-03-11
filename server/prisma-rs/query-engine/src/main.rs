66aaef45a6a477a9f24c530c3ddd144013b9dd15
Katharina Fey <kookie@spacekookie.de>
2019-03-11 16:32:46 +0100

    Adding small hello world

--- server/prisma-rs/query-engine/src/main.rs
+++ server/prisma-rs/query-engine/src/main.rs
@@ -1,5 +1,12 @@
+use actix_web::{http, server, App, Path, Responder};
+
+fn index(info: Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.1, info.0)
+}
 
 fn main() {
-
-    
+    server::new(|| App::new().route("/{id}/{name}/index.html", http::Method::GET, index))
+        .bind("127.0.0.1:8080")
+        .unwrap()
+        .run();
 }
