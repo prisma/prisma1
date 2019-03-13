337a3526b7795d268010de56d134df5e2d15da3c
timsuchanek <tim.suchanek@gmail.com>
2019-03-13 19:58:34 +0100

    fix custom cluster deployment

--- cli/packages/prisma-yml/src/PrismaDefinition.ts
+++ cli/packages/prisma-yml/src/PrismaDefinition.ts
@@ -229,9 +229,9 @@ and execute ${chalk.bold.green(
     const {
       clusterName,
       clusterBaseUrl,
-      shared,
       isPrivate,
       local,
+      shared,
       workspaceSlug,
     } = data
 
@@ -253,7 +253,7 @@ and execute ${chalk.bold.green(
       local,
       shared,
       isPrivate,
-      workspaceSlug,
+      workspaceSlug!,
     )
   }
 
