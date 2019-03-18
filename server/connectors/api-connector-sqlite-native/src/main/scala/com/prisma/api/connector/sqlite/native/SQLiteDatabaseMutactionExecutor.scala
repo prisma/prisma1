156ad88592f85e019cb1cb8efc9dfbb5a762776b
Marcus Böhm <boehm@prisma.io>
2019-03-18 13:32:39 +0100

    sqlite native can't support transactional execution from the get go

--- server/connectors/api-connector-sqlite-native/src/main/scala/com/prisma/api/connector/sqlite/native/SQLiteDatabaseMutactionExecutor.scala
+++ server/connectors/api-connector-sqlite-native/src/main/scala/com/prisma/api/connector/sqlite/native/SQLiteDatabaseMutactionExecutor.scala
@@ -1,11 +1,14 @@
 package com.prisma.api.connector.sqlite.native
-import com.prisma.api.connector.{DatabaseMutactionExecutor, MutactionResults, TopLevelDatabaseMutaction}
+import com.prisma.api.connector.jdbc.{NestedDatabaseMutactionInterpreter, TopLevelDatabaseMutactionInterpreter}
+import com.prisma.api.connector.jdbc.impl.JdbcDatabaseMutactionExecutor
+import com.prisma.api.connector.{DatabaseMutactionExecutor, MutactionResults, NestedDatabaseMutaction, TopLevelDatabaseMutaction}
+import com.prisma.connector.shared.jdbc.SlickDatabase
 import com.prisma.rs.NativeBinding
 import com.prisma.shared.models.Project
 import play.api.libs.json.JsValue
 import prisma.protocol
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 case class SQLiteDatabaseMutactionExecutor(delegate: DatabaseMutactionExecutor) extends DatabaseMutactionExecutor {
   override def executeTransactionally(mutaction: TopLevelDatabaseMutaction): Future[MutactionResults] = {
@@ -23,5 +26,24 @@ case class SQLiteDatabaseMutactionExecutor(delegate: DatabaseMutactionExecutor) 
 //    val json = NativeBinding.execute_raw(input);
 //    Future.successful(json)
     delegate.executeRaw(project, query)
+  }
+}
+
+case class SQLiteDatabaseMutactionExecutor2(
+    override val slickDatabase: SlickDatabase,
+    override val manageRelayIds: Boolean
+)(implicit ec: ExecutionContext)
+    extends JdbcDatabaseMutactionExecutor(slickDatabase, manageRelayIds) {
+
+  override def executeTransactionally(mutaction: TopLevelDatabaseMutaction): Future[MutactionResults] = {
+    executeNonTransactionally(mutaction)
+  }
+
+  override def interpreterFor(mutaction: TopLevelDatabaseMutaction): TopLevelDatabaseMutactionInterpreter = {
+    super.interpreterFor(mutaction)
+  }
+
+  override def interpreterFor(mutaction: NestedDatabaseMutaction): NestedDatabaseMutactionInterpreter = {
+    super.interpreterFor(mutaction)
   }
 }
