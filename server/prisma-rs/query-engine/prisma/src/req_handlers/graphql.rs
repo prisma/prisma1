de20d6370ee241d2ce7ca2eb2f3823966d904335
Katharina Fey <kookie@spacekookie.de>
2019-03-21 16:20:36 +0100

    Fixing some compile issues

--- server/prisma-rs/query-engine/prisma/src/req_handlers/graphql.rs
+++ server/prisma-rs/query-engine/prisma/src/req_handlers/graphql.rs
@@ -7,6 +7,11 @@ use prisma_common::{error::Error, PrismaResult};
 use prisma_models::{GraphqlId, PrismaValue, SingleNode};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::error::Error as _;
+
+use serde_json::{Map, Number, Value};
+
+type JsonMap = Map<String, Value>;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -21,20 +26,20 @@ pub struct GraphQlRequestHandler;
 impl RequestHandler for GraphQlRequestHandler {
     type Body = GraphQlBody;
 
-    fn handle<S: Into<PrismaRequest<Self::Body>>>(&self, req: S, ctx: &PrismaContext) -> serde_json::Value {
+    fn handle<S: Into<PrismaRequest<Self::Body>>>(&self, req: S, ctx: &PrismaContext) -> Value {
         // Handle incoming request and deal with errors properly
         match handle_safely(req.into(), ctx) {
             Ok(val) => val,
             Err(err) => {
-                let mut map = serde_json::Map::new();
-                map.insert("reason".into(), serde_json::Value::String(err));
+                let mut map = Map::new();
+                map.insert("reason".into(), Value::String(err.description().into()));
                 json_envelope("error", map)
             }
         }
     }
 }
 
-fn handle_safely(req: PrismaRequest<GraphQlBody>, ctx: &PrismaContext) -> PrismaResult<serde_json::Value> {
+fn handle_safely(req: PrismaRequest<GraphQlBody>, ctx: &PrismaContext) -> PrismaResult<Value> {
     let query_doc = match gql::parse_query(&req.body.query) {
         Ok(doc) => doc,
         Err(e) => return Err(Error::QueryParsingError(format!("{:?}", e))),
@@ -58,30 +63,29 @@ fn handle_safely(req: PrismaRequest<GraphQlBody>, ctx: &PrismaContext) -> Prisma
 
     Ok(json_envelope(
         "data",
-        results.iter().fold(Ok(serde_json::Map::new()), |acc, result| {
-            serialize_tree(&mut acc?, result).map(|_| acc)?
-        })?,
+        results
+            .iter()
+            .fold(Ok(Map::new()), |acc: PrismaResult<JsonMap>, result| {
+                acc.map(|mut a| serialize_tree(&mut a, result).map(|_| a))?
+            })?,
     ))
 }
 
 /// Recursively serialize query results
-fn serialize_tree(
-    map: &mut serde_json::map::Map<String, serde_json::Value>,
-    result: &PrismaQueryResult,
-) -> PrismaResult<()> {
+fn serialize_tree(map: &mut JsonMap, result: &PrismaQueryResult) -> PrismaResult<()> {
     match result {
         PrismaQueryResult::Single(result) => {
-            let json = match result.result {
-                None => serde_json::Value::Null,
+            let json = match &result.result {
+                None => Value::Null,
                 Some(single_node) => {
                     let mut map = serialize_single_node(single_node)?;
-                    for result in result.nested {
+                    for result in &result.nested {
                         serialize_tree(&mut map, &result)?;
                     }
-                    serde_json::Value::Object(map)
+                    Value::Object(map)
                 }
             };
-            map.insert(result.name, json);
+            map.insert(result.name.clone(), json);
         }
         _ => unimplemented!(),
     }
@@ -89,10 +93,10 @@ fn serialize_tree(
     Ok(())
 }
 
-fn serialize_single_node(single_node: SingleNode) -> PrismaResult<serde_json::map::Map<String, serde_json::Value>> {
-    let mut serde_map = serde_json::map::Map::new();
-    let field_names = single_node.field_names;
-    let values = single_node.node.values;
+fn serialize_single_node(single_node: &SingleNode) -> PrismaResult<JsonMap> {
+    let mut serde_map = Map::new();
+    let field_names = &single_node.field_names;
+    let values = &single_node.node.values;
 
     for (field, value) in field_names.into_iter().zip(values) {
         let key = field.to_string();
@@ -102,42 +106,42 @@ fn serialize_single_node(single_node: SingleNode) -> PrismaResult<serde_json::ma
     Ok(serde_map)
 }
 
-fn serialize_prisma_value(value: PrismaValue) -> PrismaResult<serde_json::Value> {
+fn serialize_prisma_value(value: &PrismaValue) -> PrismaResult<Value> {
     Ok(match value {
-        PrismaValue::String(x) => serde_json::Value::String(x),
-        PrismaValue::Float(x) => serde_json::Value::Number(match serde_json::Number::from_f64(x) {
+        PrismaValue::String(x) => Value::String(x.clone()),
+        PrismaValue::Float(x) => Value::Number(match Number::from_f64(*x) {
             Some(num) => num,
             None => return Err(Error::SerialisationError("`f64` number was invalid".into())),
         }),
-        PrismaValue::Boolean(x) => serde_json::Value::Bool(x),
+        PrismaValue::Boolean(x) => Value::Bool(*x),
         PrismaValue::DateTime(_) => unimplemented!(),
-        PrismaValue::Enum(x) => serde_json::Value::String(x),
+        PrismaValue::Enum(x) => Value::String(x.clone()),
         PrismaValue::Json(x) => serde_json::from_str(&x)?,
-        PrismaValue::Int(x) => serde_json::Value::Number(match serde_json::Number::from_f64(x as f64) {
+        PrismaValue::Int(x) => Value::Number(match Number::from_f64(*x as f64) {
             Some(num) => num,
             None => return Err(Error::SerialisationError("`f64` number was invalid".into())),
         }),
         PrismaValue::Relation(_) => unimplemented!(),
-        PrismaValue::Null => serde_json::Value::Null,
-        PrismaValue::Uuid(x) => serde_json::Value::String(x),
+        PrismaValue::Null => Value::Null,
+        PrismaValue::Uuid(x) => Value::String(x.clone()),
         PrismaValue::GraphqlId(x) => serialize_graphql_id(x)?,
     })
 }
 
-fn serialize_graphql_id(id: GraphqlId) -> PrismaResult<serde_json::Value> {
+fn serialize_graphql_id(id: &GraphqlId) -> PrismaResult<Value> {
     Ok(match id {
-        GraphqlId::String(x) => serde_json::Value::String(x),
-        GraphqlId::Int(x) => serde_json::Value::Number(match serde_json::Number::from_f64(x as f64) {
+        GraphqlId::String(x) => Value::String(x.clone()),
+        GraphqlId::Int(x) => Value::Number(match serde_json::Number::from_f64(*x as f64) {
             Some(num) => num,
             None => return Err(Error::SerialisationError("`f64` number was invalid".into())),
         }),
-        GraphqlId::UUID(x) => serde_json::Value::String(x),
+        GraphqlId::UUID(x) => Value::String(x.clone()),
     })
 }
 
 /// Create a json envelope
-fn json_envelope(id: &str, map: serde_json::Map<String, serde_json::Value>) -> serde_json::Value {
-    let mut envelope = serde_json::map::Map::new();
-    envelope.insert(id.to_owned(), serde_json::Value::Object(map));
-    serde_json::Value::Object(envelope)
+fn json_envelope(id: &str, map: serde_json::Map<String, Value>) -> Value {
+    let mut envelope = JsonMap::new();
+    envelope.insert(id.to_owned(), Value::Object(map));
+    Value::Object(envelope)
 }
