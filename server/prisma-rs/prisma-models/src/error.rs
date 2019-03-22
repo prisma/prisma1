b3cf928a0f61e642f6a286804948af1189ebc972
Katharina Fey <kookie@spacekookie.de>
2019-03-22 14:18:03 +0100

    Restructuring DomainErrors

--- server/prisma-rs/prisma-models/src/error.rs
+++ server/prisma-rs/prisma-models/src/error.rs
@@ -1,18 +1,26 @@
 use failure::Fail;
 
-#[derive(Debug)]
-pub enum Missing {
-    Model { name: String },
-    Field { name: String, model: String },
-    Relation { name: String },
-    ScalarField { name: String, model: String },
-    RelationField { name: String, model: String },
-    FieldForRelation { relation: String, model: String },
-    ModelForRelation { model_id: String, relation: String },
-}
-
 #[derive(Debug, Fail)]
 pub enum DomainError {
-    #[fail(display = "Couldn't find the requested resource.")]
-    NotFound(Missing),
+    #[fail(display = "Model `{}` not found", name)]
+    ModelNotFound { name: String },
+
+    #[fail(display = "Field `{}` on model `{}` not found", name, model)]
+    FieldNotFound { name: String, model: String },
+
+    #[fail(display = "Relation `{}` not found", name)]
+    RelationNotFound { name: String },
+
+    #[fail(display = "ScalarField `{}` on model `{}` not found", name, model)]
+    ScalarFieldNotFound { name: String, model: String },
+
+    #[fail(display = "RelationField `{}` on model `{}` not found", name, model)]
+    RelationFieldNotFound { name: String, model: String },
+
+    #[fail(display = "Relation field `{}` on model `{}` not found", relation, model)]
+    FieldForRelationNotFound { relation: String, model: String },
+
+    #[fail(display = "Model id `{}` for relation `{}` not found", model_id, relation)]
+    ModelForRelationNotFound { model_id: String, relation: String },
+
 }
