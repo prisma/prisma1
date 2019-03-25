ee1f332da15ab714f9ddfc65b869af3c9689d215
Julius de Bruijn <bruijn@prisma.io>
2019-03-25 10:51:27 +0100

    Execute is generic for all connectors

--- server/prisma-rs/query-engine/connectors/connector/src/database_mutaction_executor.rs
+++ server/prisma-rs/query-engine/connectors/connector/src/database_mutaction_executor.rs
@@ -3,8 +3,28 @@ use prisma_models::GraphqlId;
 use serde_json::Value;
 
 pub trait DatabaseMutactionExecutor {
-    fn execute_raw(&self, query: String) -> ConnectorResult<Value>;
-    fn execute(&self, db_name: String, mutaction: DatabaseMutaction) -> ConnectorResult<DatabaseMutactionResults>;
+    fn execute(&self, db_name: String, mutaction: DatabaseMutaction) -> ConnectorResult<DatabaseMutactionResults> {
+        let mut results = DatabaseMutactionResults::default();
 
+        match mutaction {
+            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::CreateNode(ref cn)) => {
+                let result = DatabaseMutactionResult {
+                    id: self.execute_create(db_name, cn)?,
+                    typ: DatabaseMutactionResultType::Create,
+                    mutaction: mutaction,
+                };
+
+                results.push(result);
+            }
+            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::UpdateNode(_)) => unimplemented!(),
+            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::UpsertNode(_)) => unimplemented!(),
+            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::DeleteNode(_)) => unimplemented!(),
+            DatabaseMutaction::Nested(_) => panic!("nested mutactions are not supported yet!"),
+        };
+
+        Ok(results)
+    }
+
+    fn execute_raw(&self, query: String) -> ConnectorResult<Value>;
     fn execute_create(&self, db_name: String, mutaction: &CreateNode) -> ConnectorResult<GraphqlId>;
 }
--- server/prisma-rs/query-engine/connectors/sqlite-connector/src/sqlite.rs
+++ server/prisma-rs/query-engine/connectors/sqlite-connector/src/sqlite.rs
@@ -80,28 +80,6 @@ impl DatabaseMutactionExecutor for Sqlite {
             Ok(id)
         })
     }
-
-    fn execute(&self, db_name: String, mutaction: DatabaseMutaction) -> ConnectorResult<DatabaseMutactionResults> {
-        let mut results = DatabaseMutactionResults::default();
-
-        match mutaction {
-            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::CreateNode(ref cn)) => {
-                let result = DatabaseMutactionResult {
-                    id: self.execute_create(db_name, cn)?,
-                    typ: DatabaseMutactionResultType::Create,
-                    mutaction: mutaction,
-                };
-
-                results.push(result);
-            }
-            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::UpdateNode(_)) => unimplemented!(),
-            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::UpsertNode(_)) => unimplemented!(),
-            DatabaseMutaction::TopLevel(TopLevelDatabaseMutaction::DeleteNode(_)) => unimplemented!(),
-            DatabaseMutaction::Nested(_) => panic!("nested mutactions are not supported yet!"),
-        };
-
-        Ok(results)
-    }
 }
 
 impl Sqlite {
