43735902eb928d5c85ba7d57d29ce8127386c535
Dominic Petrick <dompetrick@gmail.com>
2019-03-29 18:49:41 +0100

    First venture into native tests.

--- server/servers/api/src/test/scala/com/prisma/api/ApiTestServer.scala
+++ server/servers/api/src/test/scala/com/prisma/api/ApiTestServer.scala
@@ -1,6 +1,7 @@
 package com.prisma.api
 
 import com.prisma.api.schema.{ApiUserContext, PrivateSchemaBuilder, SchemaBuilder}
+import com.prisma.graphql.GraphQlClient
 import com.prisma.shared.models.Project
 import com.prisma.utils.json.PlayJsonExtensions
 import play.api.libs.json._
@@ -11,15 +12,10 @@ import sangria.schema.Schema
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Awaitable, Future}
 import scala.reflect.io.File
+import scala.sys.process.{Process, ProcessLogger}
 
-case class ApiTestServer()(implicit dependencies: ApiDependencies) extends PlayJsonExtensions {
-  import dependencies.system.dispatcher
-
-  def writeSchemaIntoFile(schema: String): Unit = File("schema").writeAll(schema)
-
-  def printSchema: Boolean = false
-  def writeSchemaToFile    = false
-  def logSimple: Boolean   = false
+trait ApiTestServer extends PlayJsonExtensions {
+  val dependencies: ApiDependencies
 
   /**
     * Execute a Query that must succeed.
@@ -33,6 +29,177 @@ case class ApiTestServer()(implicit dependencies: ApiDependencies) extends PlayJ
   ): JsValue = awaitInfinitely {
     queryAsync(query, project, dataContains, variables, requestId)
   }
+
+  def queryAsync(
+      query: String,
+      project: Project,
+      dataContains: String = "",
+      variables: JsValue = JsObject.empty,
+      requestId: String = "CombinedTestDatabase.requestId"
+  ): Future[JsValue]
+
+  /**
+    * Execute a Query that must fail.
+    */
+  def queryThatMustFail(
+      query: String,
+      project: Project,
+      errorCode: Int,
+      errorCount: Int = 1,
+      errorContains: String = "",
+      userId: Option[String] = None,
+      variables: JsValue = JsObject.empty,
+      requestId: String = "CombinedTestDatabase.requestId"
+  ): JsValue
+
+  protected def querySchemaAsync(
+      query: String,
+      project: Project,
+      schema: Schema[ApiUserContext, Unit],
+      variables: JsValue,
+      requestId: String
+  ): Future[JsValue]
+
+  def queryPrivateSchema(query: String, project: Project, variables: JsObject = JsObject.empty): JsValue = {
+    val schemaBuilder = PrivateSchemaBuilder(project)(dependencies)
+    awaitInfinitely {
+      querySchemaAsync(
+        query = query,
+        project = project,
+        schema = schemaBuilder.build(),
+        variables = variables,
+        requestId = "private-api-request"
+      )
+    }
+  }
+
+  protected def awaitInfinitely[T](awaitable: Awaitable[T]): T = Await.result(awaitable, Duration.Inf)
+}
+
+case class PrismaLogger() extends ProcessLogger {
+  private val stdout = new StringBuffer()
+  private val stderr = new StringBuffer()
+
+  override def out(s: => String): Unit = stdout.append(s)
+  override def err(s: => String): Unit = stderr.append(s)
+
+  override def buffer[T](f: => T): T = ???
+
+  def getStdout: String = stdout.toString
+  def getStderr: String = stderr.toString
+}
+
+case class ProcessHandle(process: Process, logger: PrismaLogger) {
+  def kill = process.destroy()
+}
+
+/*
+ * Wip
+ * - Underlying forwarder for mutations
+ */
+case class ExternalApiTestServer()(implicit val dependencies: ApiDependencies) extends ApiTestServer {
+  import dependencies.system.dispatcher
+
+  import sys.process._
+
+  implicit val system       = dependencies.system
+  implicit val materializer = dependencies.materializer
+
+  val prismaBinaryPath: String       = sys.env.getOrElse("PRISMA_BINARY_PATH", sys.error("Required PRISMA_BINARY_PATH env var not found"))
+  val prismaBinaryConfigPath: String = sys.env.getOrElse("PRISMA_BINARY_CONFIG_PATH", sys.error("Required PRISMA_BINARY_CONFIG_PATH env var not found"))
+  val gqlClient                      = GraphQlClient("http://127.0.0.1:8000") // todo rust code currently ignores port in config
+
+  def startPrismaProcess(): ProcessHandle = {
+    val logger     = PrismaLogger()
+    val workingDir = new java.io.File(".")
+    val process    = Process(prismaBinaryPath, workingDir, "PRISMA_CONFIG_PATH" -> prismaBinaryConfigPath).run(logger)
+
+    ProcessHandle(process, logger)
+  }
+
+  override def queryAsync(query: String, project: Project, dataContains: String, variables: JsValue, requestId: String): Future[JsValue] = {
+    val schemaBuilder = SchemaBuilder()(dependencies)
+    val result = querySchemaAsync(
+      query = query.stripMargin,
+      project = project,
+      schema = schemaBuilder(project),
+      variables = variables,
+      requestId = requestId
+    )
+
+    result.map { r =>
+      r.assertSuccessfulResponse(dataContains)
+      r
+    }
+  }
+
+  override protected def querySchemaAsync(query: String,
+                                          project: Project,
+                                          schema: Schema[ApiUserContext, Unit],
+                                          variables: JsValue,
+                                          requestId: String): Future[JsValue] = {
+    // Decide whether to go through the external server or internal resolver
+    if (query.startsWith("mutation")) {
+      val queryAst = QueryParser.parse(query.stripMargin).get
+      val result = dependencies.queryExecutor.execute(
+        requestId = requestId,
+        queryString = query,
+        queryAst = queryAst,
+        variables = variables,
+        operationName = None,
+        project = project,
+        schema = schema
+      )
+
+      result.foreach(x => println(s"""Request Result:
+          |$x
+      """.stripMargin))
+      result
+    } else {
+      val prismaProcess = startPrismaProcess()
+      val res           = gqlClient.sendQuery(query).map(r => r.jsonBody.get) // todo don't unwrap
+
+      res.onComplete(_ => {
+        println(prismaProcess.logger.getStdout)
+        println(prismaProcess.logger.getStderr)
+        prismaProcess.kill
+      })
+      res
+    }
+  }
+
+  override def queryThatMustFail(query: String,
+                                 project: Project,
+                                 errorCode: Int,
+                                 errorCount: Int,
+                                 errorContains: String,
+                                 userId: Option[String],
+                                 variables: JsValue,
+                                 requestId: String): JsValue = {
+    val schemaBuilder = SchemaBuilder()(dependencies)
+    val result = awaitInfinitely {
+      querySchemaAsync(
+        query = query,
+        project = project,
+        schema = schemaBuilder(project),
+        variables = variables,
+        requestId = requestId
+      )
+    }
+
+    result.assertFailingResponse(errorCode, errorCount, errorContains)
+    result
+  }
+}
+
+case class InternalApiTestServer()(implicit val dependencies: ApiDependencies) extends ApiTestServer {
+  import dependencies.system.dispatcher
+
+  def writeSchemaIntoFile(schema: String): Unit = File("schema").writeAll(schema)
+
+  def printSchema: Boolean = false
+  def writeSchemaToFile    = false
+  def logSimple: Boolean   = false
 
   def queryAsync(
       query: String,
@@ -56,9 +223,6 @@ case class ApiTestServer()(implicit dependencies: ApiDependencies) extends PlayJ
     }
   }
 
-  /**
-    * Execute a Query that must fail.
-    */
   def queryThatMustFail(
       query: String,
       project: Project,
@@ -79,24 +243,12 @@ case class ApiTestServer()(implicit dependencies: ApiDependencies) extends PlayJ
         requestId = requestId
       )
     }
+
     result.assertFailingResponse(errorCode, errorCount, errorContains)
     result
   }
 
-  def queryPrivateSchema(query: String, project: Project, variables: JsObject = JsObject.empty): JsValue = {
-    val schemaBuilder = PrivateSchemaBuilder(project)(dependencies)
-    awaitInfinitely {
-      querySchemaAsync(
-        query = query,
-        project = project,
-        schema = schemaBuilder.build(),
-        variables = variables,
-        requestId = "private-api-request"
-      )
-    }
-  }
-
-  private def querySchemaAsync(
+  def querySchemaAsync(
       query: String,
       project: Project,
       schema: Schema[ApiUserContext, Unit],
@@ -125,5 +277,4 @@ case class ApiTestServer()(implicit dependencies: ApiDependencies) extends PlayJ
     result
   }
 
-  private def awaitInfinitely[T](awaitable: Awaitable[T]): T = Await.result(awaitable, Duration.Inf)
 }
