6852e8f8760fcef71f2a02b39add16a0db4e060f
Marcus Böhm <boehm@prisma.io>
2019-04-03 15:59:41 +0200

    migrate first test case based on new syntax

--- server/servers/api/src/test/scala/com/prisma/api/queries/nonEmbedded/NonEmbeddedScalarListsQuerySpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/queries/nonEmbedded/NonEmbeddedScalarListsQuerySpec.scala
@@ -36,11 +36,7 @@ class NonEmbeddedScalarListsQuerySpec extends FlatSpec with Matchers with ApiSpe
       TestDataModels(mongo = Vector(s1), sql = Vector(s2))
     }
 
-    testDataModels.test { dm =>
-      val project = SchemaDsl.fromStringV11()(dm)
-
-      database.setup(project)
-
+    testDataModels.testV11 { project =>
       server.query(
         s"""mutation{createList(data: {listInts: {set: [1, 2]}, todos: {create: {todoInts: {set: [3, 4]}}}}) {id}}""".stripMargin,
         project
@@ -77,10 +73,7 @@ class NonEmbeddedScalarListsQuerySpec extends FlatSpec with Matchers with ApiSpe
       TestDataModels(mongo = Vector(s1), sql = Vector(s1))
     }
 
-    testDataModels.test { dm =>
-      val project = SchemaDsl.fromStringV11()(dm)
-      database.setup(project)
-
+    testDataModels.testV11 { project =>
       server.query(
         s"""mutation{createList(data: {listInts: {set: [1, 2]}, todo: {create: {todoInts: {set: [3, 4]}, tag: {create: {tagInts: {set: [5, 6]}}}}}}) {id}}""".stripMargin,
         project
@@ -98,11 +91,13 @@ class NonEmbeddedScalarListsQuerySpec extends FlatSpec with Matchers with ApiSpe
       val s1 = s"""type List{
                   |   id: ID! @id
                   |   todo: Todo @relation(link: INLINE)
+                  |   uList: String! @unique
                   |   listInts: [Int]
                   |}
                   |
                   |type Todo{
                   |   id: ID! @id
+                  |   uTodo: String! @unique
                   |   list: List
                   |   tag: Tag @relation(link: INLINE)
                   |   todoInts: [Int]
@@ -110,6 +105,7 @@ class NonEmbeddedScalarListsQuerySpec extends FlatSpec with Matchers with ApiSpe
                   |
                   |type Tag{
                   |   id: ID! @id
+                  |   uTag: String! @unique
                   |   todo: Todo
                   |   tagInts: [Int]
                   |}
@@ -117,92 +113,69 @@ class NonEmbeddedScalarListsQuerySpec extends FlatSpec with Matchers with ApiSpe
       TestDataModels(mongo = Vector(s1), sql = Vector(s1))
     }
 
-    val project = SchemaDsl.fromStringV11() {
-      s"""type List{
-         |   id: ID! @id
-         |   todo: Todo
-         |   uList: String! @unique
-         |   listInts: [Int]
-         |}
-         |
-         |type Todo{
-         |   id: ID! @id
-         |   uTodo: String! @unique
-         |   list: List
-         |   tag: Tag
-         |   todoInts: [Int]
-         |}
-         |
-         |type Tag{
-         |   id: ID! @id
-         |   uTag: String! @unique
-         |   todo: Todo
-         |   tagInts: [Int]
-         |}
-         |"""
-    }
+    testDataModels.testV11 { project =>
+      server
+        .query(
+          s"""mutation{createList(data: {uList: "A", listInts: {set: [1, 2]}, todo: {create: {uTodo: "B", todoInts: {set: [3, 4]}, tag: {create: {uTag: "C",tagInts: {set: [5, 6]}}}}}}) {id}}""".stripMargin,
+          project
+        )
 
-    database.setup(project)
-
-    server
-      .query(
-        s"""mutation{createList(data: {uList: "A", listInts: {set: [1, 2]}, todo: {create: {uTodo: "B", todoInts: {set: [3, 4]}, tag: {create: {uTag: "C",tagInts: {set: [5, 6]}}}}}}) {id}}""".stripMargin,
-        project
-      )
-
-    server.query(
-      s"""mutation{updateList(where: {uList: "A"}
+      server.query(
+        s"""mutation{updateList(where: {uList: "A"}
          |                    data: {listInts: {set: [7, 8]},
          |                           todo: {update: {todoInts: {set: [9, 10]},
          |                                           tag: {update: { tagInts: {set: [11, 12]}}}}}}) {id}}""".stripMargin,
-      project
-    )
-    val result = server.query(s"""query{lists {listInts, todo {todoInts, tag {tagInts}}}}""".stripMargin, project)
+        project
+      )
+      val result = server.query(s"""query{lists {listInts, todo {todoInts, tag {tagInts}}}}""".stripMargin, project)
 
-    result.toString should equal("""{"data":{"lists":[{"listInts":[7,8],"todo":{"todoInts":[9,10],"tag":{"tagInts":[11,12]}}}]}}""")
+      result.toString should equal("""{"data":{"lists":[{"listInts":[7,8],"todo":{"todoInts":[9,10],"tag":{"tagInts":[11,12]}}}]}}""")
+    }
+
   }
 
   "Nested scalar lists" should "work in upserts and only execute one branch of the upsert" in {
-
-    val project = SchemaDsl.fromStringV11() {
-      s"""type List{
-         |   id: ID! @id
-         |   todo: Todo
-         |   uList: String! @unique
-         |   listInts: [Int]
-         |}
-         |
-         |type Todo{
-         |   id: ID! @id
-         |   uTodo: String! @unique
-         |   list: List
-         |   todoInts: [Int]
-         |}
-         |"""
+    val testDataModels = {
+      val s1 = s"""type List{
+                  |   id: ID! @id
+                  |   todo: Todo @relation(link: INLINE)
+                  |   uList: String! @unique
+                  |   listInts: [Int]
+                  |}
+                  |
+                  |type Todo{
+                  |   id: ID! @id
+                  |   uTodo: String! @unique
+                  |   list: List
+                  |   todoInts: [Int]
+                  |}
+                  |"""
+      TestDataModels(mongo = Vector(s1), sql = Vector(s1))
     }
 
-    database.setup(project)
+    testDataModels.testV11 { project =>
+      server
+        .query(
+          s"""mutation{createList(data: {uList: "A", listInts: {set: [1, 2]}, todo: {create: {uTodo: "B", todoInts: {set: [3, 4]}}}}) {id}}""".stripMargin,
+          project
+        )
+        .pathAsString("data.createList.id")
 
-    server
-      .query(
-        s"""mutation{createList(data: {uList: "A", listInts: {set: [1, 2]}, todo: {create: {uTodo: "B", todoInts: {set: [3, 4]}}}}) {id}}""".stripMargin,
-        project
-      )
-      .pathAsString("data.createList.id")
+      server
+        .query(
+          s"""mutation upsertListValues {upsertList(
+          |                             where:{uList: "A"}
+          |                             create:{uList:"Should Not Matter" listInts:{set: [75, 85]}}
+          |                             update:{listInts:{set: [70, 80]} }
+          |){id}}""".stripMargin,
+          project
+        )
+        .pathAsString("data.upsertList.id")
 
-    server
-      .query(
-        s"""mutation upsertListValues {upsertList(
-        |                             where:{uList: "A"}
-	      |                             create:{uList:"Should Not Matter" listInts:{set: [75, 85]}}
-	      |                             update:{listInts:{set: [70, 80]} }
-        |){id}}""".stripMargin,
-        project
-      )
-      .pathAsString("data.upsertList.id")
+      val result = server.query(s"""query{lists {uList, listInts}}""".stripMargin, project)
 
-    val result = server.query(s"""query{lists {uList, listInts}}""".stripMargin, project)
+      result.toString should equal("""{"data":{"lists":[{"uList":"A","listInts":[70,80]}]}}""")
+    }
 
-    result.toString should equal("""{"data":{"lists":[{"uList":"A","listInts":[70,80]}]}}""")
   }
 }
