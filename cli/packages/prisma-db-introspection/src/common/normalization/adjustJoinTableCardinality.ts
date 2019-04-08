6d498c82d44799b18a0efaa8c0354a58557004a6
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2019-04-08 19:13:14 +0200

    CLI/DB-Introspection: WIP/Code Cleanup

--- cli/packages/prisma-db-introspection/src/common/normalization/adjustJoinTableCardinality.ts
+++ cli/packages/prisma-db-introspection/src/common/normalization/adjustJoinTableCardinality.ts
@@ -5,57 +5,40 @@ import {
   IGQLType,
   DirectiveKeys,
 } from 'prisma-datamodel'
-import { INormalizer } from './normalizer'
+import { INormalizer, Normalizer } from './normalizer'
 
-export class AdjustJoinTableCardinality implements INormalizer {
-  protected baseModel: ISDL
-
-  constructor(baseModel: ISDL) {
-    this.baseModel = baseModel
-  }
-
-  public normalizeType(type: IGQLType, ref: IGQLType) {
-    for (const field of type.fields) {
-      // Only look at n:n relations.
-      if (typeof field.type !== 'string' && field.isList) {
-        // TODO: Subclass
-        // Fid the reference field.
-        const refField = ref.fields.find(x => x.name === field.name)
-
-        if (refField === undefined || typeof refField.type === 'string')
-          continue
-
-        if (refField.type.name !== field.type.name) continue
-
-        // If the reference is not a list. We restrict it to a non-list and add
-        // a link: TABLE directive (compatability mode).
-        if (!refField.isList) {
-          field.isList = false
-          field.isRequired = refField.isRequired
-          if (
-            field.relatedField &&
-            field.relatedField.directives.some(
-              d =>
-                d.name === DirectiveKeys.relation && Boolean(d.arguments.link),
-            )
-          ) {
-            continue
-          }
-          field.directives.push(this.createLinkTableDirective(field))
-        }
-      }
+export class AdjustJoinTableCardinality extends Normalizer {
+  protected normalizeField(
+    field: IGQLField,
+    parentType: IGQLType,
+    parentModel: ISDL,
+  ) {
+    if (
+      typeof field.type === 'string' ||
+      !field.isList ||
+      this.baseField === null ||
+      typeof this.baseField.type === 'string' ||
+      this.baseField.type.name !== field.type.name
+    ) {
+      return
     }
-  }
 
-  public normalize(model: ISDL) {
-    for (const type of model.types) {
-      // TODO: We should move all tooling for finding types or fields into some common class.
-      const ref = this.baseModel.types.find(
-        x => x.name === type.name || x.databaseName === type.name,
-      )
-      if (ref !== undefined) {
-        this.normalizeType(type, ref)
+    // If the reference is not a list. We restrict it to a non-list and add
+    // a link: TABLE directive (compatability mode).
+    if (!this.baseField.isList) {
+      field.isList = false
+      field.isRequired = this.baseField.isRequired
+
+      const alreadyHasLinkDirective =
+        field.relatedField &&
+        field.relatedField.directives.some(
+          d => d.name === DirectiveKeys.relation && Boolean(d.arguments.link),
+        )
+
+      if (alreadyHasLinkDirective) {
+        return
       }
+      field.directives.push(this.createLinkTableDirective(field))
     }
   }
 
