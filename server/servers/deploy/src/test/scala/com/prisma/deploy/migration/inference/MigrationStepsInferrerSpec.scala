37fde9f18353f14542dc3f38cdf3c42f6a7982a4
Marcus Böhm <boehm@prisma.io>
2019-04-09 17:09:30 +0200

    replace last usage of SchemaBuilder

--- server/servers/deploy/src/test/scala/com/prisma/deploy/migration/inference/MigrationStepsInferrerSpec.scala
+++ server/servers/deploy/src/test/scala/com/prisma/deploy/migration/inference/MigrationStepsInferrerSpec.scala
@@ -1,12 +1,14 @@
 package com.prisma.deploy.migration.inference
 
 import com.prisma.deploy.specutils.DeploySpecBase
+import com.prisma.shared.models.ConnectorCapability.EmbeddedScalarListsCapability
 import com.prisma.shared.models._
 import com.prisma.shared.schema_dsl.SchemaDsl
-import com.prisma.shared.schema_dsl.SchemaDsl.SchemaBuilder
 import org.scalatest.{FlatSpec, Matchers}
 
 class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecBase {
+
+  val capas: Set[ConnectorCapability] = Set(EmbeddedScalarListsCapability)
 
   /**
     * Basic tests
@@ -16,17 +18,20 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   "No changes" should "create no migration steps" in {
     val renames = SchemaMapping.empty
 
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
+    val previousProject = SchemaDsl.fromStringV11() {
+      """type Test {
+        |  id: ID! @id
+        |  a: String
+        |  b: Int
+        |}
+      """.stripMargin
     }
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
-    }
+    val nextProject = previousProject
 
     val inferrer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames)
     val steps    = inferrer.evaluate()
 
-    steps shouldBe Vector(UpdateModel("Test", "Test"))
+    steps should be(empty)
   }
 
   "Creating models" should "create CreateModel and CreateField migration steps" in {
@@ -74,13 +79,23 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   "Deleting models" should "create DeleteModel migration steps" in {
     val renames = SchemaMapping.empty
 
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
-      schema.model("Test2").field("c", _.String).field("d", _.Int)
+    val previousProject = SchemaDsl.fromStringV11() {
+      """type Test {
+        |  id: ID! @id
+        |}
+        |
+        |type Test2 {
+        |  id: ID! @id
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
+    val nextProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames)
@@ -94,12 +109,23 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
     val renames = SchemaMapping(
       models = Vector(Mapping(previous = "Test", next = "Test2"))
     )
-
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
+    val previousProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |  a: String
+        |  b: Int
+        |}
+      """.stripMargin
     }
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Test2").field("a", _.String).field("b", _.Int)
+    val nextProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test2 {
+        |  id: ID! @id
+        |  a: String
+        |  b: Int
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames)
@@ -112,11 +138,20 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   "Creating fields" should "create CreateField migration steps" in {
     val renames = SchemaMapping.empty
 
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String)
+    val previousProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |}
+      """.stripMargin
     }
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
+    val nextProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |  b: Int
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames)
@@ -129,11 +164,20 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   "Deleting fields" should "create DeleteField migration steps" in {
     val renames = SchemaMapping.empty
 
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String).field("b", _.Int)
+    val previousProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |  b: Int
+        |}
+      """.stripMargin
     }
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Test").field("a", _.String)
+    val nextProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames)
@@ -150,28 +194,31 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
       )
     )
 
-    val previousProject = SchemaBuilder() { schema =>
-      schema
-        .model("Test")
-        .field_!("id", _.Cuid, isUnique = true)
-        .field("a", _.String)
-        .field("b", _.String)
-        .field("c", _.String)
-        .field("d", _.String)
-        .field("e", _.String)
-        .field("f", _.String)
+    val previousProject = SchemaDsl.fromStringV11() {
+      """
+        |type Test {
+        |  id: ID! @id
+        |  a: String
+        |  b: String
+        |  c: String
+        |  d: String
+        |  e: String
+        |  f: String
+        |}
+      """.stripMargin
     }
-
-    val nextProject = SchemaBuilder() { schema =>
-      schema
-        .model("Test")
-        .field_!("id", _.Cuid, isUnique = true)
-        .field("a2", _.String) // Rename
-        .field("b", _.Int) // Type change
-        .field_!("c", _.String) // Now required
-        .field("d", _.String, isList = true) // Now a list
-        .field("e", _.String, isUnique = true) // Now unique
-        .field("f", _.String) // no change
+    val nextProject = SchemaDsl.fromStringV11Capabilities(capas) {
+      """
+        |type Test {
+        |  id: ID! @id
+        |  a2: String         # rename
+        |  b: Int             # type change
+        |  c: String!         # now required
+        |  d: [String]        # now a list
+        |  e: String @unique  # now unique
+        |  f: String          # no change
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames)
@@ -189,19 +236,35 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   }
 
   "Creating Relations" should "create CreateRelation and CreateField migration steps" in {
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Comment").field("text", _.String)
-      schema
-        .model("Todo")
-        .field("title", _.String)
+
+    val previousProject = SchemaDsl.fromStringV11Capabilities(capas) {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  title: String
+        |}
+        |
+        |type Comment {
+        |  id: ID! @id
+        |  text: String
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      val comment = schema.model("Comment").field("text", _.String)
-      schema
-        .model("Todo")
-        .field("title", _.String)
-        .oneToManyRelation_!("comments", "todo", comment)
+    val nextProject = SchemaDsl.fromStringV11Capabilities(capas) {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  title: String
+        |  comments: [Comment]
+        |}
+        |
+        |type Comment {
+        |  id: ID! @id
+        |  text: String
+        |  todo: Todo
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, SchemaMapping.empty)
@@ -227,19 +290,34 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   }
 
   "Deleting Relations" should "create DeleteRelation and DeleteField migration steps" in {
-    val previousProject = SchemaBuilder() { schema =>
-      val comment = schema.model("Comment").field("text", _.String)
-      schema
-        .model("Todo")
-        .field("title", _.String)
-        .oneToManyRelation_!("comments", "todo", comment)
+    val previousProject = SchemaDsl.fromStringV11Capabilities(capas) {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  title: String
+        |  comments: [Comment]
+        |}
+        |
+        |type Comment {
+        |  id: ID! @id
+        |  text: String
+        |  todo: Todo
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Comment").field("text", _.String)
-      schema
-        .model("Todo")
-        .field("title", _.String)
+    val nextProject = SchemaDsl.fromStringV11Capabilities(capas) {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  title: String
+        |}
+        |
+        |type Comment {
+        |  id: ID! @id
+        |  text: String
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, SchemaMapping.empty)
@@ -256,14 +334,30 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   }
 
   "Updating Relations" should "create UpdateRelation steps (even when there are lots of renames)" in {
-    val previousProject = SchemaBuilder() { schema =>
-      val comment = schema.model("Comment")
-      schema.model("Todo").oneToManyRelation("comments", "todo", comment, relationName = Some("CommentToTodo"))
+    val previousProject = SchemaDsl.fromStringV11Capabilities() {
+      """type Todo {
+        |  id: ID! @id
+        |  comments: [Comment] @relation(name: "CommentToTodo")
+        |}
+        |
+        |type Comment {
+        |  id: ID! @id
+        |  todo: Todo @relation(name: "CommentToTodo")
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      val comment = schema.model("CommentNew")
-      schema.model("TodoNew").oneToManyRelation("commentsNew", "todoNew", comment, relationName = Some("CommentNewToTodoNew"))
+    val nextProject = SchemaDsl.fromStringV11Capabilities() {
+      """type TodoNew {
+        |  id: ID! @id
+        |  commentsNew: [CommentNew] @relation(name: "CommentNewToTodoNew")
+        |}
+        |
+        |type CommentNew {
+        |  id: ID! @id
+        |  todoNew: TodoNew @relation(name: "CommentNewToTodoNew")
+        |}
+      """.stripMargin
     }
 
     val mappings = SchemaMapping(
@@ -290,35 +384,46 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
 
   // TODO: this spec probably cannot be fulfilled. And it probably does need to because the NextProjectInferer guarantees that those swaps cannot occur. Though this must be verified by extensive testing.
   "Switching modelA and modelB in a Relation" should "not generate any migration step" ignore {
-    val relationName = "TodoToComments"
-    val previousProject = SchemaBuilder() { schema =>
-      val comment = schema.model("Comment")
-      val todo    = schema.model("Todo")
-      todo.oneToManyRelation("comments", "todo", comment, relationName = Some(relationName))
-    }
-
-    val nextProject = SchemaBuilder() { schema =>
-      val comment = schema.model("Comment")
-      val todo    = schema.model("Todo")
-      comment.manyToOneRelation("todo", "comments", todo, relationName = Some(relationName))
-    }
-
-    val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, SchemaMapping.empty)
-    val steps    = proposer.evaluate()
-
-    steps should have(size(0))
+//    val relationName = "TodoToComments"
+//    val previousProject = SchemaBuilder() { schema =>
+//      val comment = schema.model("Comment")
+//      val todo    = schema.model("Todo")
+//      todo.oneToManyRelation("comments", "todo", comment, relationName = Some(relationName))
+//    }
+//
+//    val nextProject = SchemaBuilder() { schema =>
+//      val comment = schema.model("Comment")
+//      val todo    = schema.model("Todo")
+//      comment.manyToOneRelation("todo", "comments", todo, relationName = Some(relationName))
+//    }
+//
+//    val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, SchemaMapping.empty)
+//    val steps    = proposer.evaluate()
+//
+//    steps should have(size(0))
   }
 
   "Creating and using Enums" should "create CreateEnum and CreateField migration steps" in {
-    val previousProject = SchemaBuilder() { schema =>
-      schema.model("Todo")
+    val previousProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatus", Vector("Active", "Done"))
-      schema
-        .model("Todo")
-        .field("status", _.Enum, enum = Some(enum))
+    val nextProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatus
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
     val proposer = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, SchemaMapping.empty)
@@ -338,18 +443,32 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
   "Updating an Enum Name" should "create one UpdateEnum and one UpdateField for each field using that Enum" in {
     val renames = SchemaMapping(enums = Vector(Mapping(previous = "TodoStatus", next = "TodoStatusNew")))
 
-    val previousProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatus", Vector("Active", "Done"))
-      schema
-        .model("Todo")
-        .field("status", _.Enum, enum = Some(enum))
+    val previousProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatus
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatusNew", Vector("Active", "Done"))
-      schema
-        .model("Todo")
-        .field("status", _.Enum, enum = Some(enum))
+    val nextProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatusNew
+        |}
+        |
+        |enum TodoStatusNew {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
     val steps = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames).evaluate()
@@ -364,23 +483,38 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
 
   "Updating the values of an Enum" should "create one UpdateEnum step" in {
     val renames = SchemaMapping.empty
-    val previousProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatus", Vector("Active", "Done"))
-      schema
-        .model("Todo")
-        .field("status", _.Enum, enum = Some(enum))
-        .field("status2", _.Enum, enum = Some(enum))
+
+    val previousProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatus
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      schema
-        .model("Todo")
-        .field("status", _.Enum, enum = Some(schema.enum("TodoStatus", Vector("Active", "AbsolutelyDone")))) // one value changed
-        .field("status2", _.Enum, enum = Some(schema.enum("TodoStatus", Vector("Active", "Done")))) // no change
+    val nextProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatus
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  AbsolutelyDone
+        |}
+      """.stripMargin
     }
 
     val steps = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames).evaluate()
 
+    println(steps)
     steps.length should be(2)
     steps should contain allOf (
       UpdateModel("Todo", "Todo"),
@@ -397,19 +531,33 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
       enums = Vector()
     )
 
-    val previousProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatus", Vector("Active", "Done"))
-      schema
-        .model("Todo")
-        .field("status", _.Enum, enum = Some(enum))
+    val previousProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatus
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatus", Vector("Active", "Done"))
-      schema
-        .model("Todo")
-        .field("someField", _.String)
-        .field("status", _.Enum, enum = Some(enum))
+    val nextProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |  status: TodoStatus
+        |  someField: String
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
     val steps = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames).evaluate()
@@ -425,14 +573,26 @@ class MigrationStepsInferrerSpec extends FlatSpec with Matchers with DeploySpecB
 
   "Removing Enums" should "create an DeleteEnum step" in {
     val renames = SchemaMapping.empty
-    val previousProject = SchemaBuilder() { schema =>
-      val enum = schema.enum("TodoStatus", Vector("Active", "Done"))
-      schema
-        .model("Todo")
+
+    val previousProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |}
+        |
+        |enum TodoStatus {
+        |  Active
+        |  Done
+        |}
+      """.stripMargin
     }
 
-    val nextProject = SchemaBuilder() { schema =>
-      schema.model("Todo")
+    val nextProject = SchemaDsl.fromStringV11Capabilities() {
+      """
+        |type Todo {
+        |  id: ID! @id
+        |}
+      """.stripMargin
     }
 
     val steps = MigrationStepsInferrerImpl(previousProject.schema, nextProject.schema, renames).evaluate()
