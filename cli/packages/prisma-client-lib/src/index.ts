82be5be01671de893a7c26310227699c3da3707f
Medet Tleukabiluly ✎ <m.diddy_11@mail.ru>
2019-05-10 23:17:45 +0600

    Remove generators
    Because relies on node modules

--- cli/packages/prisma-client-lib/src/index.ts
+++ cli/packages/prisma-client-lib/src/index.ts
@@ -1,11 +1,11 @@
 export { Client } from './Client'
 export { ClientOptions, BaseClientOptions, Model } from './types'
-export { Generator } from './codegen/Generator'
-export { JavascriptGenerator } from './codegen/generators/javascript-client'
-export { TypescriptGenerator } from './codegen/generators/typescript-client'
-export {
-  TypescriptDefinitionsGenerator,
-} from './codegen/generators/typescript-definitions'
-export { GoGenerator } from './codegen/generators/go-client'
-export { FlowGenerator } from './codegen/generators/flow-client'
+// export { Generator } from './codegen/Generator'
+// export { JavascriptGenerator } from './codegen/generators/javascript-client'
+// export { TypescriptGenerator } from './codegen/generators/typescript-client'
+// export {
+//   TypescriptDefinitionsGenerator,
+// } from './codegen/generators/typescript-definitions'
+// export { GoGenerator } from './codegen/generators/go-client'
+// export { FlowGenerator } from './codegen/generators/flow-client'
 export { makePrismaClientClass } from './makePrismaClientClass'
