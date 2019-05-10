df475ccc0b822e4e11373a3b3140b9c7843746cd
Dominic Petrick <dompetrick@gmail.com>
2019-05-10 19:17:56 +0200

    WIP

--- server/prisma-rs/query-engine/core/src/schema/schema.rs
+++ server/prisma-rs/query-engine/core/src/schema/schema.rs
@@ -1,9 +1,51 @@
-pub struct Schema {
-//    models: Vec<Model>,
+pub struct QuerySchema {
+    query: ObjectType, // read(s)?
+    mutation: ObjectType, // write(s)?
 }
 
-pub struct Model {
-//    fields: Vec<Field>,
+// enum for Optional input types, list types?
+// Could also be a flag on the structs
+
+
+impl QuerySchema {
+
+}
+
+struct ObjectType {
+
+}
+
+struct InputType {
+
+}
+
+struct OutputType {
+
 }
 
 pub struct Field {}
+
+// On schema construction checks:
+// - field name uniqueness
+// - val NameRegexp = """^[_a-zA-Z][_a-zA-Z0-9]*$""".r match
+// -
+
+enum InputType {
+    EnumType,
+    InputObjectType,
+    ListInputType,
+    OptionInputType,
+    ScalarType,
+}
+
+enum OutputType {
+    EnumType,
+    ListType(OutputType),
+    ObjectType,
+    OptionType(OutputType),
+    ScalarType,
+}
+
+// Possible:
+// InputType(OptionType(StringType))
+
