662888a78505d66693b78fc9464ec2b884dedea2
Marcus Böhm <boehm@prisma.io>
2019-05-17 17:55:46 +0200

    god damn it's fixed

--- server/prisma-rs/migration-engine/core/tests/test_harness.rs
+++ server/prisma-rs/migration-engine/core/tests/test_harness.rs
@@ -1,10 +1,7 @@
 use datamodel;
 #[allow(dead_code)]
-use migration_connector::*;
 use migration_core::MigrationEngine;
-use sql_migration_connector::SqlMigrationConnector;
 use std::panic;
-use std::path::Path;
 
 pub fn parse(datamodel_string: &str) -> datamodel::Schema {
     match datamodel::parse(datamodel_string) {
@@ -19,13 +16,6 @@ pub fn parse(datamodel_string: &str) -> datamodel::Schema {
     }
 }
 
-pub fn run_test<T>(test: T) -> ()
-where
-    T: FnOnce() -> () + panic::UnwindSafe,
-{
-    run_test_with_engine(|_| test());
-}
-
 pub fn run_test_with_engine<T>(test: T) -> ()
 where
     T: FnOnce(Box<MigrationEngine>) -> () + panic::UnwindSafe,
@@ -33,17 +23,10 @@ where
     // SETUP
     let engine = MigrationEngine::new();
     let connector = engine.connector();
-    connector.initialize();
     connector.reset();
+    connector.initialize();
 
     // TEST
     let result = panic::catch_unwind(|| test(engine));
     assert!(result.is_ok())
-}
-
-// TODO: swap this out with connector loader and do not hard code associated type
-pub fn connector() -> Box<MigrationConnector<DatabaseMigrationStep = impl DatabaseMigrationStepExt>> {
-    let file_path = dbg!(file!());
-    let file_name = dbg!(Path::new(file_path).file_stem().unwrap().to_str().unwrap());
-    Box::new(SqlMigrationConnector::new(file_name.to_string()))
 }
