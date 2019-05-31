0623370d5ac2ea510371ce6ba0d8b75dd690775e
Dominic Petrick <dompetrick@gmail.com>
2019-05-31 17:08:32 +0200

    Some test fixes

--- server/prisma-rs/prisma-models/tests/datamodel_converter_tests.rs
+++ server/prisma-rs/prisma-models/tests/datamodel_converter_tests.rs
@@ -186,7 +186,7 @@ fn explicit_relation_fields() {
             }
 
             model Post {
-                id: Int @id                
+                id: Int @id
                 blog: Blog? @db(name:"blog_id")
             }
         "#,
@@ -223,7 +223,7 @@ fn many_to_many_relations() {
     let datamodel = convert(
         r#"
             model Post {
-                id: Int @id                
+                id: Int @id
                 blogs: Blog[]
             }
 
@@ -274,7 +274,7 @@ fn implicit_relation_fields() {
             }
 
             model Post {
-                id: Int @id                
+                id: Int @id
             }
         "#,
     );
@@ -305,7 +305,7 @@ fn explicit_relation_names() {
             }
 
             model Post {
-                id: Int @id                
+                id: Int @id
                 blog: Blog? @relation(name: "MyRelationName")
             }
         "#,
@@ -398,7 +398,7 @@ impl FieldAssertions for ScalarField {
     }
 
     fn assert_unique(&self) -> &Self {
-        assert!(self.is_unique);
+        assert!(self.is_unique());
         self
     }
 }
@@ -442,7 +442,7 @@ impl FieldAssertions for RelationField {
     }
 
     fn assert_unique(&self) -> &Self {
-        assert!(self.is_unique);
+        assert!(self.is_unique());
         self
     }
 }
