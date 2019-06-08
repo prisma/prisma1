ceccf797992887626aaae74de4f8d30b7f9367b2
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2019-06-08 21:34:30 +0200

    RS/datamodel: * Rename source to json conversion methods * added serde convenience interface * renamed connectors to postgres, mysql, sqlite

--- server/prisma-rs/libs/datamodel/src/dmmf/mod.rs
+++ server/prisma-rs/libs/datamodel/src/dmmf/mod.rs
@@ -3,8 +3,5 @@ mod from_dmmf;
 mod to_dmmf;
 
 pub use from_dmmf::parse_from_dmmf;
-pub use from_dmmf::sources_from_dmmf;
-pub use from_dmmf::sources_from_dmmf_with_plugins;
-pub use to_dmmf::render_config_to_dmmf;
 pub use to_dmmf::render_to_dmmf;
 pub use to_dmmf::render_to_dmmf_value;
