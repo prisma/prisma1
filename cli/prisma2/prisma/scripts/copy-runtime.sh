8c49032d1a8ad6a40db15b70fd84d7ba84bdca99
timsuchanek <tim.suchanek@gmail.com>
2019-06-15 11:19:12 +0200

    introspection dep fix

--- cli/prisma2/prisma/scripts/copy-runtime.sh
+++ cli/prisma2/prisma/scripts/copy-runtime.sh
@@ -5,3 +5,6 @@ mkdir -p photogen_ncc_build
 cp -R node_modules/@prisma/photon/runtime/* runtime
 cp -R node_modules/photogen/photogen_ncc_build/* photogen_ncc_build
 cp node_modules/@prisma/lift/dist/GeneratorWorker.js build/GeneratorWorker.js
+cp -R node_modules/@prisma/studio-server/public/* build/public
+rm -rf build/public/static/js/*.map
+rm -rf build/public1/static/js/*.map
