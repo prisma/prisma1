9e30cfade577ec1aabbe0aa552986a2cc6c7b0d3
Matthias Oertel <matthias.oertel@gmx.net>
2019-07-05 15:33:46 +0200

    do not run test for SQLite and Mongo, leave comment why this test was necessary for later changes to native code

--- server/servers/api/src/test/scala/com/prisma/api/mutations/CreateMutationSequenceSpec.scala
+++ server/servers/api/src/test/scala/com/prisma/api/mutations/CreateMutationSequenceSpec.scala
@@ -1,5 +1,7 @@
 package com.prisma.api.mutations
 
+import com.prisma.ConnectorTag
+import com.prisma.ConnectorTag.{MongoConnectorTag, SQLiteConnectorTag}
 import com.prisma.api.ApiSpecBase
 import com.prisma.shared.models.ConnectorCapability.ScalarListsCapability
 import com.prisma.shared.schema_dsl.SchemaDsl
@@ -8,6 +10,8 @@ import org.scalatest.{FlatSpec, Matchers}
 class CreateMutationSequenceSpec extends FlatSpec with Matchers with ApiSpecBase {
 
   override def runOnlyForCapabilities = Set(ScalarListsCapability)
+
+  override def doNotRunForConnectors: Set[ConnectorTag] = Set(SQLiteConnectorTag, MongoConnectorTag)
 
   val project = SchemaDsl.fromStringV11() {
     s"""
@@ -24,6 +28,7 @@ class CreateMutationSequenceSpec extends FlatSpec with Matchers with ApiSpecBase
     database.setup(project)
   }
 
+  //This was producing invalid SQL due to no field values being inserted into the table. I special cased this in NodeActions -> Create
   "A Create Mutation for a Type with only an autoincrementing Id" should "work" in {
 
     val res = server.query(
