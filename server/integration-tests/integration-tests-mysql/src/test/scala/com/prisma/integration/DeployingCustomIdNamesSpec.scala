558efbd29a3f7265df50c3f50c864a0faa87d711
Matthias Oertel <matthias.oertel@gmx.net>
2019-07-19 14:28:58 +0200

    use the correct column reference in relationtable create statements for Postgres and SQLite

---
+++ server/integration-tests/integration-tests-mysql/src/test/scala/com/prisma/integration/DeployingCustomIdNamesSpec.scala
@@ -0,0 +1,36 @@
+package com.prisma.integration
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class DeployingCustomIdNamesSpec extends FlatSpec with Matchers with IntegrationBaseSpec {
+
+  "Adding a defaultValue of the wrong type" should "provide a proper error" in {
+
+    val schema =
+      """type Person {
+        |  id: ID! @id
+        |  age: Int! @unique
+        |}"""
+
+    val (project, _) = setupProject(schema)
+
+    val schema1 =
+      """type Game {
+        |  gameId: ID! @id
+        |  createdAt: DateTime! @createdAt
+        |  updatedAt: DateTime! @updatedAt
+        |  lastUpdatedOn: String!
+        |  players: [Player!]! @relation(link: TABLE, name: "GamePlayer")
+        |}
+        |
+        |type Player {
+        |  playerId: ID! @id
+        |  createdAt: DateTime! @createdAt
+        |  updatedAt: DateTime! @updatedAt
+        |  lastUpdatedOn: String!
+        |  games: [Game!]! @relation(name: "GamePlayer")
+        |}"""
+
+    deployServer.deploySchemaThatMustSucceed(project, schema1, 3, true)
+  }
+}
