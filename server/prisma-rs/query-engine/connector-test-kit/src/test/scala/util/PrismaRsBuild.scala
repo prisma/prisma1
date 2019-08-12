a129ccffc10316905de1bddc20706dddd4fed566
Marcus Böhm <boehm@prisma.io>
2019-08-12 16:38:30 +0200

    don't run rust build on buildkite on each test. We run it before anyway. Save time and moneez

--- server/prisma-rs/query-engine/connector-test-kit/src/test/scala/util/PrismaRsBuild.scala
+++ server/prisma-rs/query-engine/connector-test-kit/src/test/scala/util/PrismaRsBuild.scala
@@ -4,12 +4,11 @@ import java.io.ByteArrayInputStream
 
 object PrismaRsBuild {
   def apply(): Unit = {
-    val workingDirectory = new java.io.File(EnvVars.serverRoot + "/prisma-rs")
-    val command          = Seq("cargo", "build", "--release")
-    val env              = ("RUST_LOG", "error")
-//    val begin = System.currentTimeMillis()
-    sys.process.Process(command, workingDirectory, env).!!
-//    val end = System.currentTimeMillis()
-//    println(s"took ${end - begin}ms")
+    if (!EnvVars.isBuildkite) {
+      val workingDirectory = new java.io.File(EnvVars.serverRoot + "/prisma-rs")
+      val command          = Seq("cargo", "build", "--release")
+      val env              = ("RUST_LOG", "error")
+      sys.process.Process(command, workingDirectory, env).!!
+    }
   }
 }
